b9ff85800f311d5f7b0f2334c19f86aa9fd7dc68
Hubert Touvet <htouvet@tranquil.it>
2012-04-16 16:59:44 +0000

    Appli de test installation intégrée pour Windows
    Subversion-branch: /trunk
    Subversion-revision: 3

---
+++ uvistestwapt.pas
@@ -0,0 +1,36 @@
+unit uvistestwapt;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  Form1: TForm1; 
+
+implementation
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+Begin
+End;
+
+
+end.
+
