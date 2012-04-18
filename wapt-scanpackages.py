904b4629afddd429e93dfd2db2bd54bbcbe74812
Hubert Touvet <htouvet@tranquil.it>
2012-04-18 07:25:38 +0000

    indent
    Subversion-branch: /trunk
    Subversion-revision: 16

--- wapt-scanpackages.py
+++ wapt-scanpackages.py
@@ -6,14 +6,14 @@ import hashlib
 import tempfile
 import common
 def md5_for_file(fname, block_size=2**20):
-    f = open(fname,'rb')
-    md5 = hashlib.md5()
-    while True:
-        data = f.read(block_size)
-        if not data:
-            break
-        md5.update(data)
-    return md5.hexdigest()
+  f = open(fname,'rb')
+  md5 = hashlib.md5()
+  while True:
+    data = f.read(block_size)
+    if not data:
+      break
+    md5.update(data)
+  return md5.hexdigest()
 
 
 
@@ -23,18 +23,17 @@ print packagefilename
 packagefile = open(packagefilename,"wb")
 for fname in waptlist:
 
-    print fname
-    entry = Package_Entry()
-    entry.register_package(fname)
-    packagefile.write(entry.printobj())
+  print fname
+  entry = Package_Entry()
+  entry.register_package(fname)
+  packagefile.write(entry.printobj())
 
-    packagefile.write('\n')
+  packagefile.write('\n')
 
 packagefile.close()
 myzipfile = zipfile.ZipFile("Packages", "w")
 myzipfile.write(filename=packagefilename,arcname= "Packages")
 myzipfile.close()
 os.remove(packagefilename)
-
 
 
