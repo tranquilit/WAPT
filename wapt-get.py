9942d18d46f769542268e07695d44eb5eea34bd6
Hubert Touvet <htouvet@tranquil.it>
2012-06-01 10:41:15 +0000

    changement test URL wapt
    Subversion-branch: /trunk
    Subversion-revision: 57

--- wapt-get.py
+++ wapt-get.py
@@ -48,192 +48,206 @@ logger.addHandler(hdlr)
 
 # set loglevel
 if loglevel in ('debug','warning','info','error','critical'):
-  numeric_level = getattr(logging, loglevel.upper(), None)
-  if not isinstance(numeric_level, int):
-    raise ValueError('Invalid log level: %s' % loglevel)
-  logger.setLevel(numeric_level)
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    logger.setLevel(numeric_level)
 
 if len(args) == 0:
-  print "ERROR : You must provide one action to perform"
-  parser.print_usage()
-  sys.exit(2)
+    print "ERROR : You must provide one action to perform"
+    parser.print_usage()
+    sys.exit(2)
 
 def psource(module):
-  file = os.path.basename( module )
-  dir = os.path.dirname( module )
-  toks = file.split( '.' )
-  modname = toks[0]
+    file = os.path.basename( module )
+    dir = os.path.dirname( module )
+    toks = file.split( '.' )
+    modname = toks[0]
 
-  # Check if the file directory already exists in the sys.path array
-  if os.path.exists( dir ) and not dir in sys.path:
-    sys.path.append( dir )
+    # Check if the file directory already exists in the sys.path array
+    if os.path.exists( dir ) and not dir in sys.path:
+        sys.path.append( dir )
 
-  exec ('import ' + modname) in globals()
-  exec( 'reload( ' + modname + ' )' ) in globals()
+    exec ('import ' + modname) in globals()
+    exec( 'reload( ' + modname + ' )' ) in globals()
 
-  # This returns the namespace of the file imported
-  return modname
+    # This returns the namespace of the file imported
+    return modname
 
 def find_wapt_server(configparser):
+    def tryurl(url):
+        try:
+            logger.debug('Trying %s' % s)
+            urllib2.urlopen(s+'/')
+            logger.debug('OK')
+            return True
+        except:
+            logger.debug('Not available')
+            return False
+    
     local_ips = socket.gethostbyname_ex(socket.gethostname())[2]
-    servers = []
-    if configparser.get('global','repo_url'):
-        servers.append(configparser.get('global','repo_url'))
+    url = configparser.get('global','repo_url')
+    if  url and tryurl(url):
+        return url
     # find by dns SRV _wapt._tcp
     try:
         answers = dns.resolver.query('_wapt._tcp','SRV')
         for a in answers:
             if a.port == 80:
-                servers.append('http://%s/wapt' % (a.target.canonicalize().to_text()[0:-1]))
+                url = 'http://%s/wapt' % (a.target.canonicalize().to_text()[0:-1])
+                if tryurl(url):
+                    return url
             else:
-                servers.append('http://%s:%i/wapt' % (a.target.canonicalize().to_text()[0:-1],a.port))
+                url = 'http://%s:%i/wapt' % (a.target.canonicalize().to_text()[0:-1],a.port)
+                if tryurl(url):
+                    return url
     except dns.resolver.NXDOMAIN:
         pass
     # find by dns CNAME
     try:
         answers = dns.resolver.query('wapt','CNAME')
         for a in answers:
-            servers.append('http://%s/wapt' % (a.target.canonicalize().to_text()[0:-1]))
+            url = 'http://%s/wapt' % (a.target.canonicalize().to_text()[0:-1])
+            if tryurl(url):
+                return url
+            
     except dns.resolver.NXDOMAIN:
         pass
+
     # hardcoded wapt
-    servers.append('http://wapt/tiswapt')
-    servers.append('http://wapt/wapt')
-    for s in servers:
-        try:
-            logger.debug('Trying %s' % s)
-            urllib2.urlopen(s+'/')
-            logger.debug('OK')
-            return s
-        except:
-            logger.debug('Not available')
-            pass
+    url = 'http://wapt/wapt'
+    if tryurl(url):
+        return url
+
+    url = 'http://wapt/tiswapt'
+    if tryurl(url):
+        return url
 
     return None
 
 
 class wapt:
-  wapt_repourl=""
-  packagecachedir = ""
-  dry_run = False
-  dbpath = 'c:\\wapt\\db\\waptdb.sqlite'
+    wapt_repourl=""
+    packagecachedir = ""
+    dry_run = False
+    dbpath = 'c:\\wapt\\db\\waptdb.sqlite'
 
 
-  def install(self,package):
-    print ("starting installation")
-    sys.stdout.flush()
-    print ("installing package " + package)
+    def install(self,package):
+        print ("starting installation")
+        sys.stdout.flush()
+        print ("installing package " + package)
 
-    waptdb = WaptDB(dbpath=self.dbpath)
-    q = waptdb.query("select * from wapt_repo where Package=?",(package,))
-    if not q:
-        print "ERROR : Package %s not found in local DB, try update" % package
-        sys.exit(1)
-    mydict = q[0]
-    pprint.pprint (mydict)
-    packagename = mydict['Filename'].strip('./')
-    download_url = mydict['repo_url'] + '/' + packagename
-    print download_url
+        waptdb = WaptDB(dbpath=self.dbpath)
+        q = waptdb.query("select * from wapt_repo where Package=?",(package,))
+        if not q:
+            print "ERROR : Package %s not found in local DB, try update" % package
+            sys.exit(1)
+        mydict = q[0]
+        pprint.pprint (mydict)
+        packagename = mydict['Filename'].strip('./')
+        download_url = mydict['repo_url'] + '/' + packagename
+        print download_url
 
-    print ("download package from " + mydict['repo_url'])
-    sys.stdout.flush()
-    wget( download_url, self.packagecachedir)
+        print ("download package from " + mydict['repo_url'])
+        sys.stdout.flush()
+        wget( download_url, self.packagecachedir)
 
-    # When you import a file you must give it the full path
-    global packagetempdir
-    packagetempdir = tempfile.mkdtemp(dir=tempdir)
-    print ('unziping %s ' % (os.path.join(self.packagecachedir,packagename)))
-    sys.stdout.flush()
-    zip = zipfile.ZipFile( os.path.join(self.packagecachedir , packagename))
-    zip.extractall(path=packagetempdir)
+        # When you import a file you must give it the full path
+        global packagetempdir
+        packagetempdir = tempfile.mkdtemp(dir=tempdir)
+        print ('unziping %s ' % (os.path.join(self.packagecachedir,packagename)))
+        sys.stdout.flush()
+        zip = zipfile.ZipFile( os.path.join(self.packagecachedir , packagename))
+        zip.extractall(path=packagetempdir)
 
-    print ("sourcing install file")
-    sys.stdout.flush()
-    psource(os.path.join( packagetempdir,'setup.py'))
+        print ("sourcing install file")
+        sys.stdout.flush()
+        psource(os.path.join( packagetempdir,'setup.py'))
 
-    if not self.dry_run:
-      print ("executing install script")
-      sys.stdout.flush()
-      setup.install()
+        if not self.dry_run:
+            print ("executing install script")
+            sys.stdout.flush()
+            setup.install()
 
-    print ("install script finished")
-    print ("cleaning package tmp dir")
-    sys.stdout.flush()
-    shutil.rmtree(packagetempdir)
+        print ("install script finished")
+        print ("cleaning package tmp dir")
+        sys.stdout.flush()
+        shutil.rmtree(packagetempdir)
 
-  def update(self):
-    print tempdir
-    packageListFile = zipfile.ZipFile(cStringIO.StringIO(urllib2.urlopen(self.wapt_repourl + '/Packages').read())).read(name='Packages').splitlines()
+    def update(self):
+        print tempdir
+        packageListFile = zipfile.ZipFile(cStringIO.StringIO(urllib2.urlopen(self.wapt_repourl + '/Packages').read())).read(name='Packages').splitlines()
 
-    waptdb = WaptDB(dbpath=self.dbpath)
+        waptdb = WaptDB(dbpath=self.dbpath)
 
-    package = Package_Entry()
-    for line in packageListFile:
-      if line.strip()=='':
-        print package
-        package.repo_url = self.wapt_repourl
-        waptdb.add_package_entry(package)
         package = Package_Entry()
-        continue
-      splitline= line.split(':')
-      setattr(package,splitline[0].strip(),splitline[1].strip())
+        for line in packageListFile:
+            if line.strip()=='':
+                print package
+                package.repo_url = self.wapt_repourl
+                waptdb.add_package_entry(package)
+                package = Package_Entry()
+                continue
+            splitline= line.split(':')
+            setattr(package,splitline[0].strip(),splitline[1].strip())
 
-  def list_repo(self):
-    waptdb = WaptDB(dbpath=self.dbpath)
-    print waptdb.list_repo()
+    def list_repo(self):
+        waptdb = WaptDB(dbpath=self.dbpath)
+        print waptdb.list_repo()
 
 def main(argv):
-  wapt_start_date = datetime.datetime.now().strftime('%Y%m%d-%Hh%Mm%S')
-  action = args[0]
+    wapt_start_date = datetime.datetime.now().strftime('%Y%m%d-%Hh%Mm%S')
+    action = args[0]
 
-  # Config file
-  if not os.path.isfile(config_file):
-    logger.error("Error : could not find file : " + config_file + ", please check the path")
-  logger.info("Using " + config_file + " config file")
+    # Config file
+    if not os.path.isfile(config_file):
+        logger.error("Error : could not find file : " + config_file + ", please check the path")
+    logger.info("Using " + config_file + " config file")
 
-  print config_file
-  cp = ConfigParser( )
-  cp.read(config_file)
+    print config_file
+    cp = ConfigParser( )
+    cp.read(config_file)
 
-  wapt_repourl = find_wapt_server(cp)
-  if not wapt_repourl:
-    print "No valid accessible repository found... aborting"
-    sys.exit(2)
-  print "Using wapt Repository %s" % wapt_repourl
-  wapt_base_dir = cp.get('global','base_dir')
+    wapt_repourl = find_wapt_server(cp)
+    if not wapt_repourl:
+        print "No valid accessible repository found... aborting"
+        sys.exit(2)
+    print "Using wapt Repository %s" % wapt_repourl
+    wapt_base_dir = cp.get('global','base_dir')
 
-  log_dir = os.path.join(wapt_base_dir,'log')
-  if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+    log_dir = os.path.join(wapt_base_dir,'log')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-  packagecachedir = os.path.join(wapt_base_dir,'cache')
-  if not os.path.exists(packagecachedir):
-    os.makedirs(packagecachedir)
-  print packagecachedir
+    packagecachedir = os.path.join(wapt_base_dir,'cache')
+    if not os.path.exists(packagecachedir):
+        os.makedirs(packagecachedir)
+    print packagecachedir
 
-  mywapt = wapt()
-  mywapt.packagecachedir = packagecachedir
-  mywapt.wapt_repourl = wapt_repourl
-  mywapt.dry_run = options.dry_run
+    mywapt = wapt()
+    mywapt.packagecachedir = packagecachedir
+    mywapt.wapt_repourl = wapt_repourl
+    mywapt.dry_run = options.dry_run
 
-  if action=='install':
-    if len(args)<2:
-        print "You must provide the package name"
-        sys.exit(1)
-    packagename=args[1]
-    mywapt.install(packagename)
+    if action=='install':
+        if len(args)<2:
+            print "You must provide the package name"
+            sys.exit(1)
+        packagename=args[1]
+        mywapt.install(packagename)
 
-  if action=='update':
-    mywapt.update()
+    if action=='update':
+        mywapt.update()
 
-  if action=='upgrade':
-    mywapt.upgrade()
+    if action=='upgrade':
+        mywapt.upgrade()
 
-  if action=='remove':
-    mywapt.remove()
+    if action=='remove':
+        mywapt.remove()
 
-  if action=='list':
-    mywapt.list_repo()
+    if action=='list':
+        mywapt.list_repo()
 
 if __name__ == "__main__":
     logger.debug('Python path %s' % sys.path)
