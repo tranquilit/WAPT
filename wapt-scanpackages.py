525f8ef3d1a2fa3921be1b6d6fe9dfdd99952e53
Denis Cardon <dcardon@tranquil.it>
2012-10-15 13:12:36 +0000

    - rajout de check pour verification du repertoire source de wapt - correction de bug suite a integration dans common.py - creation d'un methode main
    Subversion-branch: /trunk
    Subversion-revision: 101

--- wapt-scanpackages.py
+++ wapt-scanpackages.py
@@ -4,25 +4,44 @@ import glob
 import tempfile
 import common
 import codecs
+import sys
 
 from optparse import OptionParser
 import logging
 
-waptlist = glob.glob('./*.wapt')
-packagefilename = tempfile.mkstemp(prefix="wapt-packages-")[1]
-packagefile = codecs.open(packagefilename,"wb",encoding='UTF-8')
-for fname in waptlist:
-    print "Processing %s" % fname
-    entry = common.Package_Entry()
-    entry.register_package(fname)
-    packagefile.write(entry.printobj())
-    packagefile.write('\n')
+sys.path.append("/opt/wapt") 
 
-packagefile.close()
-myzipfile = zipfile.ZipFile("Packages", "w")
-myzipfile.write(filename=packagefilename,arcname= "Packages")
-myzipfile.close()
-os.remove(packagefilename)
+def main():
+    if len(sys.argv) < 2:
+        sys.stderr.write('Usage: wapt-scanpackage <wapt_directory>\n')
+        sys.exit(1)
+    wapt_path = sys.argv[1]
+    print wapt_path
+    if os.path.exists(wapt_path)==False:
+        print "Directory does not exists : %s " % wapt_path
+        sys.exit(1)
+    if os.path.isdir(wapt_path)==False:
+        print "%s does not exists " % wapt_path
+        sys.exit(1)
+
+    waptlist = glob.glob(os.path.abspath(wapt_path) + '/*.wapt')
 
 
+    packagefilename = tempfile.mkstemp(prefix="wapt-packages-")[1]
+    packagefile = codecs.open(packagefilename,"wb",encoding='UTF-8')
+    for fname in waptlist:
+        print "Processing %s" % fname
+        entry = common.Package_Entry()
+        entry.load_control_from_wapt(fname)
+        packagefile.write(entry.ascontrol())
+        packagefile.write('\n')
 
+    packagefile.close()
+    myzipfile = zipfile.ZipFile(os.path.join(wapt_path,"Packages"), "w")
+    myzipfile.write(filename=packagefilename,arcname= "Packages")
+    myzipfile.close()
+    os.remove(packagefilename)
+
+
+if __name__ == "__main__":
+    main()
