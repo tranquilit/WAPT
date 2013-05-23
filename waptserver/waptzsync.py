dede3fd70165e8061f092f5a0b94eb6af3e1d1aa
Steven Samson <ssamson@tranquil.it>
2013-05-23 13:01:18 +0000

    Ajout fichier de config
    Subversion-branch: /trunk
    Subversion-revision: 298

--- waptserver/waptzsync.py
+++ waptserver/waptzsync.py
@@ -29,6 +29,7 @@ import tempfile
 import shutil
 import sys 
 import logging
+import ConfigParser
 
 
 logger = logging.getLogger()
@@ -37,29 +38,43 @@ hdlr.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
 logger.addHandler(hdlr)
 logger.setLevel(logging.CRITICAL)
 
-
-#Remote WAPT Repository 
-#mainRepo = "http://wapt/wapt-test/"
-mainRepo = "http://srvinstallation.tranquil.it/wapt/"
-
-#Local WAPT Repository
-waptDir = "/var/www/wapt/"
-#waptDir = "/home/homes/ssamson/wapt/"
+config_file = './config-waptzsync.ini'
+if not os.path.exists(config_file):
+    raise Exception("No config file found")
 
 
-# download new packages if present
-donwloadNewPackages = True
+config = ConfigParser.ConfigParser()
+config.read(config_file)
 
-os.chdir(waptDir)
-os.system('cd '+waptDir)
+section = config.sections()[0]
 
-urllib.urlretrieve(mainRepo+"wapt-scanpackages.py", waptDir+'wapt-scanpackages.py')
-urllib.urlretrieve(mainRepo+"waptpackage.py", waptDir+'waptpackage.py')
-os.system("/usr/bin/zsync  %s.zsync -o %s" % (mainRepo+"waptsetup.exe", waptDir+"waptsetup.exe"))
+if config.has_option(section, 'main_repo'):
+    main_repo = config.get(section, 'main_repo')
+else:
+    main_repo = "http://srvinstallation.tranquil.it/wapt/" 
+    
+if config.has_option(section, 'wapt_dir'):
+    wapt_dir = config.get(section, 'wapt_dir')
+else:
+    wapt_dir = "/var/www/wapt/"
+    
+if config.has_option(section, 'donwload_new_packages'):
+    donwload_new_packages = config.get(section, 'donwload_new_packages')
+else:
+    donwload_new_packages = True 
+    
 
-os.system('cd '+waptDir+'&& python wapt-scanpackages.py .')
 
-sys.path.append(waptDir)
+os.chdir(wapt_dir)
+os.system('cd '+wapt_dir)
+
+urllib.urlretrieve(main_repo+"wapt-scanpackages.py", wapt_dir+'wapt-scanpackages.py')
+urllib.urlretrieve(main_repo+"waptpackage.py", wapt_dir+'waptpackage.py')
+os.system("/usr/bin/zsync  %s.zsync -o %s" % (main_repo+"waptsetup.exe", wapt_dir+"waptsetup.exe"))
+
+os.system('cd '+wapt_dir+'&& python wapt-scanpackages.py .')
+
+sys.path.append(wapt_dir)
 from waptpackage import PackageEntry
 
 packagesToUpgrade = []
@@ -113,12 +128,12 @@ def downloadRepoPackages(mainRepo):
 def downloadPackages(packages):
     for package in packages:
         print "Downloading : %s version => %s" % ( package.package, package.version )
-        os.system("/usr/bin/zsync  %s.zsync -o %s" % (mainRepo+package.filename, waptDir+package.filename))
+        os.system("/usr/bin/zsync  %s.zsync -o %s" % (main_repo+package.filename, wapt_dir+package.filename))
 
 
 
-repoPackages = downloadRepoPackages(mainRepo)
-localPackages = packagesFileToList(waptDir+'Packages') 
+repoPackages = downloadRepoPackages(main_repo)
+localPackages = packagesFileToList(wapt_dir+'Packages') 
 
 for repoPackage in repoPackages:
     if not repoPackage.section != "host":
@@ -132,7 +147,7 @@ for repoPackage in repoPackages:
     elif len(matchPackage) > 1: 
         if not [ x for x in matchPackage if repoPackage.version in x.version ]:
             packagesToUpgrade.append(repoPackage)
-    elif donwloadNewPackages:
+    elif donwload_new_packages:
         newPackages.append(repoPackage)
     else:
         print "New package: "+repoPackage.package
@@ -146,8 +161,8 @@ if newPackages:
     downloadPackages(newPackages)
 
 if packagesToUpgrade or newPackages:
-    os.system('cd '+waptDir+'&& python wapt-scanpackages.py .')
+    os.system('cd '+wapt_dir+'&& python wapt-scanpackages.py .')
     
     
 
-os.system('chmod 644 %s*'%waptDir)
+os.system('chmod 644 %s*'%wapt_dir)
