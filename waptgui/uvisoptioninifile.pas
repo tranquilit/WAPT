fcd7ca7e01ed8b7ac25f9cbc9c64d118a14e73bb
Denis Cardon <dcardon@tranquil.it>
2013-06-26 08:30:29 +0000

    Subversion-branch: /trunk Subversion-revision: 350

---
+++ waptgui/uvisoptioninifile.pas
@@ -0,0 +1,96 @@
+unit uvisOptionIniFile;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Buttons, StdCtrls, ActnList, EditBtn,fpJson, jsonparser, superobject,vte_json;
+
+type
+
+  { TVisOptionIniFile }
+
+  TVisOptionIniFile = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    tCheckTestRepo: TCheckBox;
+    tCheckPrefix: TCheckBox;
+    tCheckPrivateKey: TCheckBox;
+    tCheckUpload: TCheckBox;
+    tCheckAfterUpload: TCheckBox;
+    Edit1: TEdit;
+    FileNameEdit1: TFileNameEdit;
+    Panel1: TPanel;
+    jsonlog: TVirtualJSONInspector;
+    procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
+    procedure FormCreate(Sender: TObject);
+    procedure tCheckTestRepoChange(Sender: TObject);
+    procedure FileNameEdit1Change(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  VisOptionIniFile: TVisOptionIniFile;
+
+implementation
+uses dmwaptpython;
+
+{$R *.lfm}
+
+{ TVisOptionIniFile }
+
+
+procedure TVisOptionIniFile.tCheckTestRepoChange(Sender: TObject);
+var
+  params:String;
+  result:ISuperObject;
+  done : Boolean;
+  choice : Boolean;
+begin
+  if tCheckTestRepo.Checked then
+  begin
+    params :='';
+    choice := True;
+    params := params+format('"%s",',['global']);
+    params := params+format('"%s",',['repo_url']);
+    params := params+format('"%s",',['http://wapt/wapt-sid']);
+    result := DMPython.RunJSON(format('waptdevutils.add_remove_option_inifile(mywapt,True,%s)',[params]),jsonlog);
+  end
+  else
+  begin
+    params :='';
+    choice := True;
+    params := params+format('"%s",',['global']);
+    params := params+format('"%s",',['repo_url']);
+    params := params+format('"%s",',['']);
+    result := DMPython.RunJSON(format('waptdevutils.add_remove_option_inifile(mywapt,True,%s)',[params]),jsonlog);
+  end;
+
+end;
+
+procedure TVisOptionIniFile.FormCreate(Sender: TObject);
+begin
+   DMPython.PythonEng.ExecString('import waptdevutils');
+end;
+
+procedure TVisOptionIniFile.FormCloseQuery(Sender: TObject;
+  var CanClose: boolean);
+begin
+  if ModalResult=mrOk then
+  begin
+
+  end;
+end;
+
+procedure TVisOptionIniFile.FileNameEdit1Change(Sender: TObject);
+begin
+
+end;
+
+end.
+
