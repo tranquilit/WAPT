a5ec837e81b94c3516f783dcda39e161c6728afa
Yvan Karmouta <ykarmouta@tranquil.it>
2013-08-28 13:28:21 +0000

    postonf deb waptserver
    Subversion-branch: /trunk
    Subversion-revision: 516

---
+++ waptserver/scripts/postconf.py
@@ -0,0 +1,59 @@
+import dialog
+from iniparse import RawConfigParser
+import shutil
+import fileinput
+import os,glob,sys
+
+
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+        
+postconf = dialog.Dialog(dialog="dialog")
+
+if postconf.yesno("Do you wan't to launch post configuration tool ?") == postconf.DIALOG_OK:
+    shutil.copyfile('/opt/wapt/waptserver/waptserver.ini.template','/opt/wapt/waptserver/waptserver.ini')
+    waptserver_ini = RawConfigParser()
+    
+    replaceAll('/opt/wapt/waptserver/waptserver.ini','#','')
+    waptserver_ini.read('/opt/wapt/waptserver/waptserver.ini')
+    
+    code, tag = postconf.menu("Where mongodb is install ?",
+                              choices=[("1", "Localhost (default)"),
+                                    ("2", "other serveur")])
+    if code == postconf.DIALOG_OK:
+        if tag == "1":
+            waptserver_ini.set('options','mongodb_ip','127.0.0.1')
+        elif tag == "2":
+            (code,mongodb_ip) = postconf.inputbox("IP address of the mongodb serveur:")
+            if code<>postconf.DIALOG_OK:
+                exit(0)
+            else:
+                waptserver_ini.set('options','mongodb_ip',mongodb_ip)
+        elif code<>postconf.DIALOG_OK:
+            exit(0)
+            
+            
+    code, tag = postconf.menu("Mongodb serveur port: ",
+                                  choices=[("1", "27017 (default)"),
+                                        ("2", "other")])
+    if code == postconf.DIALOG_OK:
+        if tag == "1":
+            waptserver_ini.set('options','mongodb_port','27017')
+        elif tag == "2":
+            (code,mongodb_port) = postconf.inputbox("mongodb serveur port: ")
+            if code<>postconf.DIALOG_OK:
+                exit(0)
+            else:
+                waptserver_ini.set('options','mongodb_port',mongodb_port)
+        elif code<>postconf.DIALOG_OK:
+            exit(0)    
+
+        
+            
+    with open('/opt/wapt/waptserver/waptserver.ini','w') as inifile:
+        waptserver_ini.write(inifile)    
+else:
+    exit(0)
