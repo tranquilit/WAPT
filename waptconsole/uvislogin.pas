d930cf24a829e73e1932fcb5c518680870423797
Steven Samson <ssamson@tranquil.it>
2013-11-15 11:23:50 +0000

    possibilité de changer la configuration de la console depuis l'écran de login
    Subversion-branch: /trunk
    Subversion-revision: 770

--- waptconsole/uvislogin.pas
+++ waptconsole/uvislogin.pas
@@ -6,13 +6,14 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, Buttons, ButtonPanel, LCLType;
+  ExtCtrls, Buttons, ButtonPanel, LCLType,uwaptconsole,waptcommon ;
 
 type
 
   { TVisLogin }
 
   TVisLogin = class(TForm)
+    BitBtn1: TBitBtn;
     ButtonPanel1: TButtonPanel;
     edPassword: TEdit;
     edUser: TEdit;
@@ -20,6 +21,7 @@ type
     Label1: TLabel;
     laPassword: TLabel;
     laPassword1: TLabel;
+    procedure BitBtn1Click(Sender: TObject);
     procedure edPasswordKeyDown(Sender: TObject; var Key: Word;
       Shift: TShiftState);
   private
@@ -44,6 +46,12 @@ begin
   begin
     edPassword.SelectAll;
   end;
+end;
+
+procedure TVisLogin.BitBtn1Click(Sender: TObject);
+begin
+    VisWaptGUI.ActWAPTLocalConfig.Execute;
+    edWaptServerName.Text:=waptcommon.GetWaptServerURL;
 end;
 
 end.
