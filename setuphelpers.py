03b9ec14c85f4e6d37c065b25dd33288372c91af
Hubert Touvet <htouvet@tranquil.it>
2014-04-30 15:13:23 +0000

    remove replace \ by \n\r in local gpt.ini / scripts.ini files
    Subversion-branch: /trunk
    Subversion-revision: 1070

--- setuphelpers.py
+++ setuphelpers.py
@@ -1067,14 +1067,14 @@ def add_shutdown_script(cmd,parameters):
                 set_file_visible(scriptsini_path)
             try:
                 with codecs.open(scriptsini_path,'w',encoding='utf16') as f:
-                    f.write(str(scriptsini.data).replace('\n','\r\n'))
+                    f.write(str(scriptsini.data))
             finally:
                 set_file_hidden(scriptsini_path)
 
             if not os.path.isdir(os.path.dirname(gptini_path)):
                 os.makedirs(os.path.dirname(gptini_path))
             with codecs.open(gptini_path,'w',encoding='utf8') as f:
-                f.write(str(gptini.data).replace('\n','\r\n'))
+                f.write(str(gptini.data))
             run('GPUPDATE /Target:Computer /Force /Wait:30')
             return script_index
         else:
@@ -1151,14 +1151,14 @@ def remove_shutdown_script(cmd,parameters):
                 set_file_visible(scriptsini_path)
             try:
                 with codecs.open(scriptsini_path,'w',encoding='utf16') as f:
-                    f.write(str(scriptsini.data).replace('\n','\r\n'))
+                    f.write(str(scriptsini.data))
             finally:
                 set_file_hidden(scriptsini_path)
 
             if not os.path.isdir(os.path.dirname(gptini_path)):
                 os.makedirs(os.path.dirname(gptini_path))
             with codecs.open(gptini_path,'w',encoding='utf8') as f:
-                f.write(str(gptini.data).replace('\n','\r\n'))
+                f.write(str(gptini.data))
             run('GPUPDATE /Target:Computer /Force /Wait:30')
             return script_index
         else:
