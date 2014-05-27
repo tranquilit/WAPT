5b31407b6f1cc25f852bb1e08b364cd6e530f5bb
Hubert Touvet <htouvet@tranquil.it>
2014-05-27 15:41:47 +0000

    put revision in Version restart waptservice in background
    Subversion-branch: /trunk
    Subversion-revision: 1133

--- waptupgrade/setup.py
+++ waptupgrade/setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuphelpers import *
 import os
+import _winreg
+import tempfile
+
 
 # registry key(s) where WAPT will find how to remove the application(s)
 uninstallkey = []
@@ -54,14 +57,15 @@ def update_control(entry):
     """Update package control file before build-upload"""
     update_sources()
     waptget = get_file_properties(r'patchs\wapt-get.exe')
-    entry.version = waptget['FileVersion']+'-0'
+    rev = open('version').read().strip()
+    entry.version = '%s-%s' % (waptget['FileVersion'],rev)
 
 def oncopy(msg,src,dst):
     print(u'%s : "%s" to "%s"' % (ensure_unicode(msg),ensure_unicode(src),ensure_unicode(dst)))
     return True
 
 def update_registry_version(version):
-    import _winreg
+    # updatethe registry
     with _winreg.CreateKeyEx(HKEY_LOCAL_MACHINE,r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\WAPT_is1',\
             0, _winreg.KEY_READ| _winreg.KEY_WRITE ) as waptis:
         reg_setvalue(waptis,"DisplayName","WAPT %s" % version)
@@ -73,11 +77,15 @@ def install():
     print('Mise a jour partielle du client Wapt')
     killalltasks('wapttray.exe')
     copytree2('patchs',WAPT.wapt_base_dir, onreplace = default_overwrite,oncopy=oncopy)
-    if service_installed('waptservice') and service_is_running('waptservice'):
-        print('Restart waptservice')
-        service_stop('waptservice')
-        service_start('waptservice')
-        print('Waptservice restarted')
     update_registry_version(control.version.split('-')[0])
+    # restart of service can not be done by service...
+    if service_installed('waptservice') and service_is_running('waptservice'):
+        tmp_bat = tempfile.NamedTemporaryFile(prefix='waptrestart',suffix='.cmd',mode='wt',delete=False)
+        tmp_bat.write('timeout 5')
+        tmp_bat.write('net stop waptservice')
+        tmp_bat.write('net start waptservice')
+        tmp_bat.close()
+        shell_launch(tmp_bat.name)
+
     print('Upgrade done')
 
