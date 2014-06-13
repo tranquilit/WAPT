1dc726d23d189cf0e7fe9a1fe838e49fa85fc3c4
Hubert Touvet <htouvet@tranquil.it>
2014-06-13 14:55:36 +0000

    mot de passe obligatoire dans postconf waptserver
    Subversion-branch: /trunk
    Subversion-revision: 1152

--- waptserver/scripts/postconf.py
+++ waptserver/scripts/postconf.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------
+#    This file is part of WAPT
+#    Copyright (C) 2013  Tranquil IT Systems http://www.tranquil.it
+#    WAPT aims to help Windows systems administrators to deploy
+#    setup and update applications on users PC.
+#
+#    WAPT is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    WAPT is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------
 import os,sys
 try:
     wapt_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
@@ -56,12 +78,15 @@ if postconf.yesno("Do you want to launch post configuration tool ?") == postconf
         elif code != postconf.DIALOG_OK:
             exit(0)
 
-    (code,wapt_password) = postconf.passwordbox("wapt server password:  ")
-    if code != postconf.DIALOG_OK:
-        exit(0)
-    else:
-        password = hashlib.sha512(wapt_password).hexdigest()
-        waptserver_ini.set('options','wapt_password',password)
+    while 1:
+        (code,wapt_password) = postconf.passwordbox("wapt server password:  ")
+        if code != postconf.DIALOG_OK:
+            exit(0)
+        else:
+            password = hashlib.sha512(wapt_password).hexdigest()
+            waptserver_ini.set('options','wapt_password',password)
+        if password != '':
+            break
 
     with open('/opt/wapt/waptserver/waptserver.ini','w') as inifile:
         waptserver_ini.write(inifile)
