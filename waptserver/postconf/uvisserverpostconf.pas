526a7dd34f103391abccbb55d149b1ba9234a7f1
Hubert Touvet <htouvet@tranquil.it>
2014-08-01 13:53:44 +0000

    https://roundup.tranquil.it/wapt/issue270 créer le waptsetup personalisé à la fin du wizard https://roundup.tranquil.it/wapt/issue272 pour le waptservepostconf https://roundup.tranquil.it/wapt/issue255 demander le nom de la structure dans le wizard pour le mettre dans le waptsetup personalisé requires latest pltis_utils package
    Subversion-branch: /trunk
    Subversion-revision: 1210

--- waptserver/postconf/uvisserverpostconf.pas
+++ waptserver/postconf/uvisserverpostconf.pas
@@ -7,7 +7,8 @@ interface
 uses
   Classes, SysUtils, FileUtil, IpHtml, Ipfilebroker, RichView, vsVisualSynapse,
   RLRichText, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls, ExtCtrls,
-  Buttons, ActnList, EditBtn, htmlview, Readhtml, Htmlsubs;
+  Buttons, ActnList, EditBtn, htmlview, Readhtml, Htmlsubs, IdHTTP,
+  IdComponent,uvisLoading;
 
 type
 
@@ -17,32 +18,40 @@ type
     ActCheckDNS: TAction;
     ActCreateKey: TAction;
     ActCancel: TAction;
+    ActBuildWaptsetup: TAction;
     actWriteConfStartServe: TAction;
     ActManual: TAction;
     ActNext: TAction;
     actPrevious: TAction;
     ActionList1: TActionList;
+    BitBtn1: TBitBtn;
     ButPrevious: TBitBtn;
     ButNext: TBitBtn;
     BitBtn3: TBitBtn;
     BitBtn4: TBitBtn;
     BitBtn5: TBitBtn;
     BitBtn6: TBitBtn;
+    Button1: TButton;
     cbLaunchWaptConsoleOnExit: TCheckBox;
     cbManualURL: TCheckBox;
     CBOpenFirewall: TCheckBox;
     DirectoryCert: TDirectoryEdit;
     edCommonName: TEdit;
     edCountry: TEdit;
+    edOrgName: TEdit;
+    edRepoUrl: TEdit;
     EdSourcesRoot: TLabeledEdit;
     edEmail: TEdit;
     EdPwd1: TEdit;
     EdPrivateKeyFN: TEdit;
     edLocality: TEdit;
     edOrganization: TEdit;
-    EdOrgName: TEdit;
+    EdKeyName: TEdit;
     EdPwd2: TEdit;
     edUnit: TEdit;
+    edWaptServerUrl1: TEdit;
+    fnPublicCert: TFileNameEdit;
+    fnWaptDirectory: TDirectoryEdit;
     HTMLViewer1: THTMLViewer;
     Label1: TLabel;
     Label10: TLabel;
@@ -56,6 +65,11 @@ type
     edWAPTRepoURL: TLabeledEdit;
     edWAPTServerURL: TLabeledEdit;
     Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
     Label9: TLabel;
     EdWaptServerIP: TLabeledEdit;
     EdWaptInifile: TMemo;
@@ -66,6 +80,7 @@ type
     Memo3: TMemo;
     Memo4: TMemo;
     Memo5: TMemo;
+    Memo7: TMemo;
     Memo6: TMemo;
     PagesControl: TPageControl;
     Panel1: TPanel;
@@ -73,12 +88,15 @@ type
     panFinish: TPanel;
     pgParameters: TTabSheet;
     pgPassword: TTabSheet;
+    ProgressBar1: TProgressBar;
     Shape1: TShape;
     StaticText1: TStaticText;
     pgStartServices: TTabSheet;
     pgDevparam: TTabSheet;
     pgPrivateKey: TTabSheet;
     pgFinish: TTabSheet;
+    pgCreateWaptSetup: TTabSheet;
+    procedure ActBuildWaptsetupExecute(Sender: TObject);
     procedure ActCheckDNSExecute(Sender: TObject);
     procedure ActCreateKeyExecute(Sender: TObject);
     procedure ActCreateKeyUpdate(Sender: TObject);
@@ -91,13 +109,19 @@ type
     procedure actWriteConfStartServeExecute(Sender: TObject);
     procedure BitBtn3Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
-    procedure EdOrgNameExit(Sender: TObject);
+    procedure EdKeyNameExit(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure HTMLViewer1HotSpotClick(Sender: TObject; const SRC: string;
       var Handled: boolean);
+    procedure IdHTTPWork(ASender: TObject; AWorkMode: TWorkMode;
+      AWorkCount: Int64);
     procedure PagesControlChange(Sender: TObject);
   private
+    CurrentVisLoading:TVisLoading;
+    function CreateWaptSetup(default_public_cert: String='';
+      default_repo_url: String=''; default_wapt_server: String='';
+      destination: String=''; company: String='';OnProgress:TNotifyEvent = Nil): String;
     procedure OpenFirewall;
     { private declarations }
   public
@@ -108,8 +132,8 @@ var
   VisWAPTServerPostConf: TVisWAPTServerPostConf;
 
 implementation
-uses LCLIntf, Windows,WaptCommon,tisinifiles,superobject,
-    tiscommon,tisstrings,IniFiles,UnitRedirect,uvisLoading,sha1;
+uses LCLIntf, Windows,WaptCommon,tisinifiles,superobject,tisutils,soutils,
+    tiscommon,tisstrings,IniFiles,UnitRedirect,sha1,Regex,IdMultipartFormData,IdException;
 {$R *.lfm}
 
 { TVisWAPTServerPostConf }
@@ -122,6 +146,39 @@ begin
   Result := template;
   for key in params.AsObject.GetNames do
     Result := StringReplace(Result,'%('+key.AsString+')s',params.S[key.AsString],[rfReplaceAll]);
+end;
+
+function WAPTServerJsonMultipartFilePost(waptserver,action: String;args:Array of const;
+    FileArg,FileName:String; enableProxy:Boolean= False;
+    user:AnsiString='';password:AnsiString='';OnHTTPWork:TWorkEvent=Nil):ISuperObject;
+var
+  res:String;
+  http:TIdHTTP;
+  St:TIdMultiPartFormDataStream;
+begin
+  if StrLeft(action,1)<>'/' then
+    action := '/'+action;
+  if length(args)>0 then
+    action := format(action,args);
+  HTTP := TIdHTTP.Create;
+  St := TIdMultiPartFormDataStream.Create;
+  try
+    http.Request.BasicAuthentication:=True;
+    http.Request.Username:=user;
+    http.Request.Password:=password;
+    http.OnWork:=OnHTTPWork;
+
+    St.AddFile(FileArg,FileName);
+    try
+      res := HTTP.Post(waptserver+action,St);
+    except
+      on E:EIdException do ShowMessage(E.Message);
+    end;
+    result := SO(res);
+  finally
+    st.Free;
+    HTTP.Free;
+  end;
 end;
 
 function CreateSelfSignedCert(orgname,
@@ -179,7 +236,6 @@ begin
     result := copy(result,1,length(result)-1);
 end;
 
-
 procedure TVisWAPTServerPostConf.FormCreate(Sender: TObject);
 begin
   PagesControl.ShowTabs:=False;
@@ -188,7 +244,7 @@ end;
 
 procedure TVisWAPTServerPostConf.FormShow(Sender: TObject);
 begin
-  if GetServiceStatusByName('','WAPTServer') = ssRunning then
+  if GetServiceStatusByName('','WAPTServer') in  [ssRunning,ssPaused,ssPausePending,ssStartPending] then
     Sto_RedirectedExecute('cmd /C net stop waptserver');
 
   EdWAPTServerName.Text:=LowerCase(GetComputerName)+'.'+GetDNSDomain;
@@ -201,7 +257,7 @@ begin
   if IniHasKey(WaptIniFilename,'global','private_key') then
   begin
     EdPrivateKeyFN.Text:=IniReadString(WaptIniFilename,'global','private_key');
-    EdOrgName.Text := ChangeFileExt(ExtractFileName(EdPrivateKeyFN.Text),'');
+    EdKeyName.Text := ChangeFileExt(ExtractFileName(EdPrivateKeyFN.Text),'');
   end;
   PagesControlChange(Self);
 end;
@@ -213,9 +269,24 @@ begin
   Handled:=True;
 end;
 
+procedure TVisWAPTServerPostConf.IdHTTPWork(ASender: TObject;
+  AWorkMode: TWorkMode; AWorkCount: Int64);
+begin
+  if CurrentVisLoading<>Nil then
+    CurrentVisLoading.DoProgress(ASender)
+  else
+  begin
+    if ProgressBar1.Position>=ProgressBar1.Max then
+      ProgressBar1.Position:=0
+    else
+      ProgressBar1.Position := ProgressBar1.Position+1;
+    Application.ProcessMessages;
+  end;
+end;
+
 procedure TVisWAPTServerPostConf.PagesControlChange(Sender: TObject);
 var
-  ini:TMemIniFile;
+  ini:TIniFile;
 
 begin
   HTMLViewer1.LoadStrings(TMemo(FindComponent('Memo'+IntToStr(PagesControl.ActivePageIndex+1))).Lines);
@@ -230,14 +301,31 @@ begin
     ini.WriteString('global','default_package_prefix',EdDefaultPrefix.Text);
     ini.WriteString('global','loglevel','warning');
     EdWaptInifile.Lines.Clear;
-    ini.GetStrings(EdWaptInifile.Lines);
+    TMemIniFile(ini).GetStrings(EdWaptInifile.Lines);
   finally
     ini.Free;
-  end;
+  end
+  else
   if PagesControl.ActivePage = pgFinish then
   begin
     HTMLViewer1.Parent := panFinish;
     HTMLViewer1.Align:=alClient;
+  end
+  else
+  if PagesControl.ActivePage = pgCreateWaptSetup then
+  begin
+    ini := TIniFile.Create(WaptIniFilename);
+    try
+      fnPublicCert.Text:=ChangeFileExt(ini.ReadString('global', 'private_key', ''),'.crt');
+      if not FileExists(fnPublicCert.Text) then
+        fnPublicCert.Clear;
+      edWaptServerUrl1.Text := ini.ReadString('global', 'wapt_server', '');
+      edRepoUrl.Text := ini.ReadString('global', 'repo_url', '');
+      edOrgName.Text := edOrganization.Text;
+      fnWaptDirectory.Directory := GetTempDir(False);
+    finally
+      ini.Free;
+    end;
   end;
 end;
 
@@ -262,6 +350,7 @@ begin
       output := Sto_RedirectedExecute(format('netsh advfirewall firewall add rule name="waptserver %d" dir=in localport=%d protocol=TCP action=allow',[waptserver_port,waptserver_port]));
   end;
 end;
+
 
 procedure TVisWAPTServerPostConf.ActManualUpdate(Sender: TObject);
 begin
@@ -330,8 +419,10 @@ procedure TVisWAPTServerPostConf.actWriteConfStartServeExecute(Sender: TObject);
 var
   ini:TMemIniFile;
 begin
-  with TVisLoading.Create(Self) do
+  CurrentVisLoading := TVisLoading.Create(Self);
+  with CurrentVisLoading do
   try
+    ExceptionOnStop:=True;
     ini := TMemIniFile.Create(WaptIniFilename);
     ini.SetStrings(EdWaptInifile.Lines);
     ini.UpdateFile;
@@ -347,31 +438,28 @@ begin
     Fileutil.CopyFile(ChangeFileExt(EdPrivateKeyFN.Text,'.crt'),WaptBaseDir+'\ssl\'+ChangeFileExt(ExtractFileNameOnly(EdPrivateKeyFN.Text),'.crt'),True);
     runwapt('{app}\wapt-get.exe update-packages "{app}\waptserver\repository\wapt"');
 
-
-    ProgressTitle('Mise en place mot de passe server');
+    ProgressTitle('Mise en place mot de passe du serveur');
     ProgressStep(3,8);
     IniWriteString(WaptBaseDir+'\waptserver\waptserver.ini' ,'Options','wapt_password',sha1.SHA1Print(sha1.SHA1String(EdPwd1.Text)));
 
-    ProgressTitle('Ouverture firewall pour WaptServer');
-    ProgressStep(4,8);
-    OpenFirewall;
-
-    ProgressTitle('Redémarrage waptserver');
-    ProgressStep(5,8);
-    try
-      if GetServiceStatusByName('','WAPTServer') = ssRunning then
-        Sto_RedirectedExecute('cmd /C net stop waptserver');
-    except
+    if CBOpenFirewall.Checked then
+    begin
+      ProgressTitle('Ouverture firewall pour WaptServer');
+      ProgressStep(4,8);
+      OpenFirewall;
     end;
+
+    ProgressTitle('Redémarrage service waptserver');
+    ProgressStep(5,8);
+    if GetServiceStatusByName('','WAPTServer') in [ssRunning,ssPaused,ssPausePending,ssStartPending] then
+      Sto_RedirectedExecute('cmd /C net stop waptserver');
+    ProgressStep(5,8);
     Sto_RedirectedExecute('cmd /C net start waptserver');
 
     ProgressTitle('Redémarrage waptservice');
     ProgressStep(6,8);
-    try
-      if GetServiceStatusByName('','WAPTService') = ssRunning then
-        Sto_RedirectedExecute('cmd /C net stop waptservice');
-    except
-    end;
+    if GetServiceStatusByName('','WAPTService') in [ssRunning,ssPaused,ssPausePending,ssStartPending]  then
+      Sto_RedirectedExecute('cmd /C net stop waptservice');
     Sto_RedirectedExecute('cmd /C net start waptservice');
 
     ProgressTitle('Enregistrement machine sur serveur');
@@ -386,7 +474,7 @@ begin
 
   finally
     ini.Free;
-    Free;
+    FreeAndNil(CurrentVisLoading);
   end;
 end;
 
@@ -397,9 +485,19 @@ begin
 end;
 
 procedure TVisWAPTServerPostConf.Button1Click(Sender: TObject);
+var
+  params : ISuperObject;
+  waptsetupPath: string;
+  done: boolean;
+  ini: TIniFile;
+  SORes:ISuperObject;
 begin
-  showmessage(Sto_RedirectedExecute('netsh firewall show portopening'));
-  showmessage(Sto_RedirectedExecute(format('netsh.exe firewall add portopening name="waptserver %d" port=%d protocol=TCP',[waptserver_port,waptserver_port])));
+  waptsetupPath := 'C:\Users\htouvet\AppData\Local\Temp\waptsetup.exe';
+  SORes := WAPTServerJsonMultipartFilePost(edWAPTServerURL.text,'upload_waptsetup',[],'file',waptsetupPath,False,'admin','fakepassword',@IdHTTPWork);
+  if SORes.S['status'] = 'OK' then
+    ShowMessage('Waptsetup déposé avec succès')
+  else
+    ShowMessage('Erreur lors du dépôt de waptsetup: ' + SORes.S['message']);
 end;
 
 procedure TVisWAPTServerPostConf.ActManualExecute(Sender: TObject);
@@ -409,7 +507,7 @@ end;
 
 procedure TVisWAPTServerPostConf.ActCreateKeyExecute(Sender: TObject);
 begin
-  EdPrivateKeyFN.Text := CreateSelfSignedCert( EdOrgName.Text,WaptBaseDir,DirectoryCert.Text,
+  EdPrivateKeyFN.Text := CreateSelfSignedCert( EdKeyName.Text,WaptBaseDir,DirectoryCert.Text,
     edCountry.Text,edLocality.Text,edOrganization.Text,edUnit.Text,edCommonName.Text,edEmail.Text);
 end;
 
@@ -442,12 +540,104 @@ begin
 
 end;
 
+
+function startswith(st:ISuperObject;subst:String):Boolean;
+begin
+  result := (st <>Nil) and (st.DataType = stString) and (pos(subst,trim(st.AsString))=1)
+end;
+
+function TVisWAPTServerPostConf.CreateWaptSetup(default_public_cert:String='';default_repo_url:String='';
+          default_wapt_server:String='';destination:String='';company:String='';OnProgress:TNotifyEvent = Nil):String;
+var
+  OutputFile,iss_template,custom_iss,source,target : String;
+  iss,new_iss,line : ISuperObject;
+  wapt_base_dir,inno_fn: String;
+  re : TRegexEngine;
+  exitstatus:integer;
+begin
+    wapt_base_dir:= WaptBaseDir;
+    OutputFile := '';
+    iss_template := makepath([wapt_base_dir,'waptsetup','waptsetup.iss']);
+    custom_iss := makepath([wapt_base_dir,'waptsetup','custom_waptsetup.iss']);
+    iss := SplitLines(FileToString(iss_template));
+    new_iss := TSuperObject.Create(stArray);
+    for line in iss do
+    begin
+        if startswith(line,'#define default_repo_url') then
+            new_iss.AsArray.Add(format('#define default_repo_url "%s"',[default_repo_url]))
+        else if startswith(line,'#define default_wapt_server') then
+            new_iss.AsArray.Add(format('#define default_wapt_server "%s"',[default_wapt_server]))
+        else if startswith(line,'#define output_dir') then
+            new_iss.AsArray.Add(format('#define output_dir "%s"' ,[destination]))
+        else if startswith(line,'WizardImageFile=') then
+
+        else if not startswith(line,'#define signtool') then
+            new_iss.AsArray.Add(line);
+
+        if startswith(line,'OutputBaseFilename') then
+            outputfile := makepath([wapt_base_dir,'waptsetup',format('%s.exe',[Split(line.AsString,'=')[1]])]);
+    end;
+    source := default_public_cert;
+    target := makepath([ExtractFileDir(iss_template),'..','ssl',ExtractFileName(source)]);
+    if not FileUtil.CopyFile(source,target,True) then
+      raise Exception.CreateFmt('Copie du certificat de %s vers %s impossible',[source,target]);
+    StringToFile(custom_iss,SOUtils.Join(#13#10,new_iss));
+
+    inno_fn :=  makepath([wapt_base_dir,'waptsetup','innosetup','ISCC.exe']);
+    if not FileExists(inno_fn) then
+        raise Exception.CreateFmt('Innosetup n''est pas disponible (emplacement %s), veuillez l''installer',[inno_fn]);
+    Sto_RedirectedExecute(format('"%s"  %s',[inno_fn,custom_iss]),'',3600000,'','','',OnProgress);
+    Result := makepath([destination,ExtractFileName(outputfile)]);
+end;
+
+
+procedure TVisWAPTServerPostConf.ActBuildWaptsetupExecute(Sender: TObject);
+var
+  params : ISuperObject;
+  waptsetupPath: string;
+  ini: TIniFile;
+  SORes:ISuperObject;
+begin
+  CurrentVisLoading := TVisLoading.Create(Self);
+  with CurrentVisLoading do
+  try
+    ExceptionOnStop:=True;
+    Screen.Cursor := crHourGlass;
+    ProgressTitle('Création en cours');
+    Start;
+    Application.ProcessMessages;
+    waptsetupPath := CreateWaptSetup(fnPublicCert.FileName, edRepoUrl.Text, edWaptServerUrl1.Text,fnWaptDirectory.Directory,edOrganization.Text,@DoProgress);
+    Finish;
+    if FileExists(waptsetupPath) then
+    try
+      Start;
+      ProgressTitle('Dépôt sur le serveur WAPT en cours');
+      SORes := WAPTServerJsonMultipartFilePost(edWAPTServerURL1.Text,'upload_waptsetup',[],'file',waptsetupPath,False,'admin',EdPwd1.Text,@IdHTTPWork);
+      Finish;
+      if SORes.S['status'] = 'OK' then
+        ShowMessage('waptsetup.exe créé et déposé avec succès: ' + waptsetupPath)
+      else
+        ShowMessage('Erreur lors du dépôt de waptsetup: ' + SORes.S['message']);
+    except
+      on e: Exception do
+      begin
+        ShowMessage('Erreur à la création du waptsetup.exe: ' + e.Message);
+        Finish;
+      end;
+    end;
+  finally
+    Finish;
+    Screen.Cursor := crDefault;
+    FreeAndNil(CurrentVisLoading);
+  end;
+end;
+
 procedure TVisWAPTServerPostConf.ActCreateKeyUpdate(Sender: TObject);
 var
    TargetKeyFN:String;
 begin
-  TargetKeyFN := AppendPathDelim(DirectoryCert.Text)+EdOrgName.Text+'.pem';
-  ActCreateKey.Enabled := (DirectoryCert.Text<>'') and (EdOrgName.Text<>'') and not FileExists(TargetKeyFN);
+  TargetKeyFN := AppendPathDelim(DirectoryCert.Text)+EdKeyName.Text+'.pem';
+  ActCreateKey.Enabled := (DirectoryCert.Text<>'') and (EdKeyName.Text<>'') and not FileExists(TargetKeyFN);
   if FileExists(TargetKeyFN) then
     EdPrivateKeyFN.Text := TargetKeyFN;
 end;
@@ -462,9 +652,9 @@ begin
       result := Result+st[i];
 end;
 
-procedure TVisWAPTServerPostConf.EdOrgNameExit(Sender: TObject);
+procedure TVisWAPTServerPostConf.EdKeyNameExit(Sender: TObject);
 begin
-  EdOrgName.Text:= MakeIdent(EdOrgName.Text);
+  EdKeyName.Text:= MakeIdent(EdKeyName.Text);
 end;
 
 end.
