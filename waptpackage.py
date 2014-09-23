c24abb1243c3fc4faa976223c53c153651ddfcbf
Hubert Touvet <htouvet@tranquil.it>
2014-09-23 16:21:32 +0000

    updated version to 0.9.3
    Subversion-branch: /trunk
    Subversion-revision: 1344

--- waptpackage.py
+++ waptpackage.py
@@ -20,7 +20,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 
 import os
 import zipfile
