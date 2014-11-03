b654fc64008b68473d7855042934b830152a10b9
Jérémie Courrèges-Anglas <jcourreges@tranquil.it>
2014-11-03 15:22:56 +0000

    Toujours plus de remplacements "waptsetup" -> "waptagent".
    Subversion-branch: /trunk
    Subversion-revision: 1463

--- waptserver/postconf/uvisserverpostconf.pas
+++ waptserver/postconf/uvisserverpostconf.pas
@@ -476,13 +476,13 @@ begin
       SORes := WAPTServerJsonMultipartFilePost(edWAPTServerURL1.Text,'upload_waptsetup',[],'file',waptsetupPath,False,'admin',EdPwd1.Text,@IdHTTPWork);
       Finish;
       if SORes.S['status'] = 'OK' then
-        ShowMessage('waptsetup.exe créé et déposé avec succès: ' + waptsetupPath)
+        ShowMessage('waptagent.exe créé et déposé avec succès: ' + waptsetupPath)
       else
-        ShowMessage('Erreur lors du dépôt de waptsetup: ' + SORes.S['message']);
+        ShowMessage('Erreur lors du dépôt du waptagent: ' + SORes.S['message']);
     except
       on e: Exception do
       begin
-        ShowMessage('Erreur à la création du waptsetup.exe: ' + e.Message);
+        ShowMessage('Erreur à la création du waptagent.exe: ' + e.Message);
         Finish;
       end;
     end;
