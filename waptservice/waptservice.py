9b42883c1dc58b7e28626bd8f1c78fa041253ca1
htouvet <htouvet-github@tranquil.it>
2014-11-14 12:39:05 +0100

    bump version to 0.9.7 in python files

--- waptservice/waptservice.py
+++ waptservice/waptservice.py
@@ -19,7 +19,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 
 import time
 import sys
