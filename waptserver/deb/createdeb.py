779b88d74ffd4f4801e576f8396514ceaaaf68bb
Jérémie Courrèges-Anglas <jca@tranquil.it>
2014-11-28 18:14:19 +0100

    Repair previous...

--- waptserver/deb/createdeb.py
+++ waptserver/deb/createdeb.py
@@ -141,7 +141,7 @@ os.chmod('./builddir/DEBIAN/postinst',stat.S_IRWXU| stat.S_IXGRP | stat.S_IRGRP 
 os.chmod('./builddir/DEBIAN/preinst',stat.S_IRWXU| stat.S_IXGRP | stat.S_IRGRP | stat.S_IROTH | stat.S_IXOTH)
 
 print >> sys.stderr, 'creating the Debian package'
-dpkg_command = 'dpkg-deb --build builddir tis-waptserver.deb'% (wapt_version, )
+dpkg_command = 'dpkg-deb --build builddir tis-waptserver.deb'
 ret = os.system(dpkg_command)
 status = ret >> 8
 if status == 0:
