93dfa6870dd76c91594239ed140c93e65b3fa679
Mael Cadorel <mcadorel@tranquil.it>
2015-01-07 11:51:18 +0100

    Sketched an English translation of wapt-console messages (in progress)

--- wapt-get/uwaptres.pas
+++ wapt-get/uwaptres.pas
@@ -10,124 +10,124 @@ uses
 resourcestring
 
   { Messages dans uwaptconsole.pas }
-  rsFatalError = '... Impossible de récupérer l''action.';
+  rsFatalError = 'Failed to recover action.'; // '... Impossible de récupérer l''action.';
   rsInstalling = 'Installing %s...';
-  rsDefineWaptdevPath = 'Veuillez définir un répertoire de développement pour pouvoir éditer un paquet groupe.';
+  rsDefineWaptdevPath = 'Please choose a development directory to edit group package.'; //'Veuillez définir un répertoire de développement pour pouvoir éditer un paquet groupe.'
 
   rsPublicKeyGenSuccess = 'Key %s successfully created.';
   rsPublicKeyGenFailure = 'Failed to generate public key.';
-  rsPublicKeyGenError = 'Erreur à la création de la clé : %s';
+  rsPublicKeyGenError = 'Error during key generation : %s';
 
-  rsCreationInProgress = 'Création en cours';
-  rsProgressTitle = 'Dépôt sur le serveur WAPT en cours';
-  rsWaptSetupUploadSuccess = 'Agent WAPT créé et déposé avec succès : %s';
-  rsWaptUploadError = 'Erreur lors du dépôt de l''agent WAPT : %s';
-  rsWaptSetupError = 'Erreur à la création de l''agent WAPT : %s';
+  rsCreationInProgress = 'Creating...'; //'Création en cours'
+  rsProgressTitle = 'Uploading to WAPT server...';
+  rsWaptSetupUploadSuccess = 'WAPT agent successfully created and uploaded.'; // 'Agent WAPT créé et déposé avec succès : %s';
+  rsWaptUploadError = 'Error while uploading agent'; // 'Erreur lors du dépôt de l''agent WAPT : %s';
+  rsWaptSetupError = 'Error while creating agent'; // 'Erreur à la création de l''agent WAPT : %s';
 
-  rsForcedUninstallPackages = 'Choix des paquets à forcer à désintaller sur les postes sélectionnés';
-  rsDependencies = 'Choix des paquets à ajouter en dépendance aux postes sélectionnés';
-  rsNbModifiedHosts = '%s postes modifiés.';
+  rsForcedUninstallPackages = 'Mark packages for forced uninstall'; // 'Choix des paquets à forcer à désintaller sur les postes sélectionnés'; TODO
+  rsDependencies = 'Mark packages for adding as dependencies for selected hosts'; // 'Choix des paquets à ajouter en dépendance aux postes sélectionnés';
+  rsNbModifiedHosts = '%s hosts affected.';
   rsTaskCancelled = 'Task canceled.';
-  rsFailedToCancel = 'Impossible d''annuler : %s.';
+  rsFailedToCancel = 'Could not cancel : %s.';
 
-  rsIncorrectPassword = 'Mauvais mot de passe';
-  rsPasswordChangeSuccess = 'Le mot de passe a été changé avec succès !';
-  rsPasswordChangeError = 'Erreur : %s';
+  rsIncorrectPassword = 'Password incorrect.';
+  rsPasswordChangeSuccess = 'Password successfully updated !';
+  rsPasswordChangeError = 'Error : %s';
 
-  rsWaptAgentUploadSuccess = 'Waptagent déposé avec succès';
-  rsWaptAgentUploadError = 'Erreur lors du dépôt de waptagent: %s';
-  rsWaptAgentSetupSuccess = 'waptagent.exe créé avec succès: ';
-  rsWaptAgentSetupError = 'Erreur à la création du waptagent.exe: %s';
+  rsWaptAgentUploadSuccess = 'Successfully uploaded WAPT agent !';
+  rsWaptAgentUploadError = 'Error while uploading WAPT agent : %s';
+  rsWaptAgentSetupSuccess = 'waptagent.exe successfully created : %s';
+  rsWaptAgentSetupError = 'Error while creating waptagent.exe: %s';
 
-  rsConfirmRmOnePackage = 'Etes-vous sûr de vouloir supprimer ce paquet du serveur ?';
-  rsConfirmRmMultiplePackages = 'Etes-vous sûr de vouloir supprimer ces paquets du serveur ?'; // Maybe consider merging the last two into a single formatted str.
-  rsConfirmRmPackageCaption = 'Confirmer la suppression';
-  rsDeletionInProgress = 'Suppression des paquets...';
-  rsDeletingElement = 'Suppression de %s';
-  rsUpdatingPackageList = 'Mise à jour de la liste des paquets';
-  rsDisplaying = 'Affichage';
-  rsConfirmDeletion = 'Confirmer la suppression';
+  rsConfirmRmOnePackage = 'Are you sure you want to remove this package from server ?';
+  rsConfirmRmMultiplePackages = 'Are you sure you want to remove these packages from server ?'; // Maybe consider merging the last two into a single formatted str.
+  rsConfirmRmPackageCaption = 'Confirm removal';
+  rsDeletionInProgress = 'Removing packages...';
+  rsDeletingElement = 'Removing %s';
+  rsUpdatingPackageList = 'Updating package list';
+  rsDisplaying = 'Displaying';
+  rsConfirmDeletion = 'Confirm removal';  // Duplicate of rsConfirmRmPackageCaption
 
-  rsConfirmCaption = 'Confirmer';
+  rsConfirmCaption = 'Confirm';
 
-  rsConfirmHostForgetsPackages = 'Confirmez-vous l''oubli de %s packages du poste %s ?'; // Possible issue with multiple formatting parameters
-  rsForgetPackageError = 'Erreur pour le package %s: %s';
+  rsConfirmHostForgetsPackages = 'Are you sure you want to forget %s packages from host %s ?'; // Possible issue with multiple formatting parameters
+  rsForgetPackageError = 'Error while forgetting package %s: %s';
 
-  rsPrivateKeyDoesntExist = 'La clé privée n''existe pas : %s';
+  rsPrivateKeyDoesntExist = 'Private key doesn''t exist : %s';
 
-  rsConfirmImportCaption = 'Confirmer l''import';
-  rsConfirmImport = 'Etes vous sûr de vouloir importer'#13#10'%s'#13#10' dans votre dépôt ?';
+  rsConfirmImportCaption = 'Confirm import';
+  rsConfirmImport = 'Are you sure you want to import'#13#10'%s'#13#10' to your repository ?';
   rsImportingFile = 'Importing %s';
-  rsUploadingPackagesToWaptSrv = 'Dépôt sur le serveur WAPT en cours pour %s paquets...';
-  rsSuccessfullyImported = '%s importé(s) avec succès.';
-  rsFailedImport = 'Erreur lors de l''import.';
+  rsUploadingPackagesToWaptSrv = 'Uploading %s packages to WAPT server...';
+  rsSuccessfullyImported = '%s successfully imported.';
+  rsFailedImport = 'Error during import.';
 
-  rsConfirmRmPackagesFromHost = 'Confirmez-vous la désinstallation de %s packages du poste %s ?';
-  rsPackageRemoveError = 'Erreur pour le package %s: %s';
+  rsConfirmRmPackagesFromHost = 'Are you sure you want to remove %s packages from host %s ?';
+  rsPackageRemoveError = 'Error while removing package %s: %s';
 
-  rsReallowPackagesOnHost = 'Choix des paquets à réautoriser sur les postes sélectionnés';
-  rsRmGroupFromHosts = 'Choix des groupes à enlever des postes sélectionnés';
-  rsNoGroup = 'Il n''y a aucun groupe.';
+  rsReallowPackagesOnHost = 'Mark packages for reallowing on selected hosts'; // 'Choix des paquets à réautoriser sur les postes sélectionnés';
+  rsRmGroupFromHosts = 'Mark groups for removing from selected hosts'; // 'Choix des groupes à enlever des postes sélectionnés';
+  rsNoGroup = 'There is no group.'; // 'Il n''y a aucun groupe.'; TODO : pas assez explicite ?
 
-  rsWaptClientUpdateOnHosts = 'Mise à jour du client WAPT sur les postes';
+  rsWaptClientUpdateOnHosts = 'Updating WAPT client on hosts'; //'Mise à jour du client WAPT sur les postes';
 
-  rsConfirmRmHostsFromList = 'Confirmez-vous la suppression de %s postes de la liste ?';
+  rsConfirmRmHostsFromList = 'Are you sure you want to remove %s hsots from the list ?';
 
-  rsUninstallingPackage = 'Désinstallation de %s en cours ...';
+  rsUninstallingPackage = 'Uninstalling %s...';
 
   rsCanceledByUser = 'Task %s has been canceled by user';
 
   { Messages dans wapt-get/waptcommon.pas }
-  rsInnoSetupUnavailable = 'Innosetup n''est pas disponible (emplacement %s), veuillez l''installer';
+  rsInnoSetupUnavailable = 'Innosetup is unavailable (path : %s), please install it.';
   rsUndefWaptSrvInIni = 'wapt_server is not defined in your %s ini file';
   rsDlStoppedByUser = 'Download stopped by user';
 
   { Messages dans uVisCreateKey }
-  rsInputKeyName = 'Veuillez rentrer un nom de clé';
-  rsKeyAlreadyExists = 'La clé %s existe déjà, choisir un autre nom de clé';
+  rsInputKeyName = 'Please input a key name'; // 'Veuillez rentrer un nom de clé'; // TODO
+  rsKeyAlreadyExists = 'Key %s already exists, please pick another name.';
 
   { Messages dans uVisEditPackage.pas }
-  rsEditGroup = 'Editer le groupe';
-  rsEdPackage = 'Groupe';
-  rsPackagesNeededCaption = 'Paquets devant être présents dans le groupe';
+  rsEditGroup = 'Edit group';
+  rsEdPackage = 'Group';
+  rsPackagesNeededCaption = 'Packages needed in group';
 
-  rsEditHostCaption = 'Editer la machine';
-  rsUpgradingHost = 'Upgrade lancée';
-  rsUpgradeHostError = 'Impossible de lancer l''upgrade sur la machine: ';
+  rsEditHostCaption = 'Edit host';
+  rsUpgradingHost = 'Upgrading';
+  rsUpgradeHostError = 'Failed to upgrade host : ';
 
-  rsSaveMods = 'Sauvegarder les modifications ?';
-  rsUploading = 'Upload en cours';
-  rsPackageCreationError = 'Problème lors de la création du paquet: %s';
-  rsHostConfigEditCaption = 'Modifier la configuration de la machine';
-  rsPackagesNeededOnHostCaption = 'Paquets devant être présents sur la machine';
+  rsSaveMods = 'Save changes ?';
+  rsUploading = 'Uploading';
+  rsPackageCreationError = 'Error while creating package : %s';
+  rsHostConfigEditCaption = 'Edit host configuration';
+  rsPackagesNeededOnHostCaption = 'Packages needed on host';
 
-  rsDownloading = 'Téléchargement en cours';
-  rsGroupConfigEditCaption = 'Modifier la configuration du groupe';
-  rsDlCanceled = 'Téléchargement annulé';
-  rsIgnoredPackages = 'Attention, les paquets %s ont été ignorés car introuvables';
-  rsIgnoredConfictingPackages = 'Attention, les paquets %s ont été ignorés des paquets interdits car introuvables';
+  rsDownloading = 'Downloading';
+  rsGroupConfigEditCaption = 'Edit group configuration';
+  rsDlCanceled = 'Download canceled.';
+  rsIgnoredPackages = 'Warning : couldn''t find packages %s ; ignoring them.';
+  rsIgnoredConfictingPackages = 'Warning : couldn''t find packages %s ; ignored from conflicting packages.'; // TODO
 
   { Messages dans uVisEditPackage.pas }
-  rsInputPubKeyPath = 'Veuillez rentrer le chemin vers la clé publique';
-  rsInvalidWaptSetupDir = 'Le répertoire pour sauvegarder waptsetup n''est pas valide: ';
+  rsInputPubKeyPath = 'Please input path to public key';
+  rsInvalidWaptSetupDir = 'WAPTsetup directory is not valid : %s'; // 'Le répertoire pour sauvegarder waptsetup n''est pas valide: %s';
 
   { Messages dans uVisChangePassword.pas }
-  rsDiffPwError = 'Les mots de passe sont différents';
-  rsEmptyNewPwError = 'Le nouveau mot de passe ne doit pas être vide';
-  rsEmptyOldPwError = 'L''ancien mot de passe ne doit pas être vide';
-  rsIncorrectOldPwError = 'L''ancien mot de passe ne correspond pas';
+  rsDiffPwError = 'Passwords do not match.';
+  rsEmptyNewPwError = 'New password may not be empty.';
+  rsEmptyOldPwError = 'Old password may not be empty.';
+  rsIncorrectOldPwError = 'Old password is incorrect.';
 
   { Messages dans uVisApropos }
   rsVersion = 'Version Waptconsole: %s'#13#10'Version Wapt-get: %s';
 
   { Messages dans uVisApropos }
   rsUrl = 'Url : %s';
-  rsPackageDuplicateConfirmCaption = 'Confirmer la duplication';
-  rsPackageDuplicateConfirm = 'Etes vous sûr de vouloir dupliquer'#13#10'%s'#13#10' dans votre dépôt ?';
-  rsDownloadingPackage = 'Téléchargement en cours de %s';
-  rsDuplicating = 'Duplication de %s';
-  rsDuplicateSuccess = '%s dupliqué(s) avec succès.';
-  rsDuplicateFailure = 'Erreur lors de la duplication.';
+  rsPackageDuplicateConfirmCaption = 'Confirm duplication';
+  rsPackageDuplicateConfirm = 'Are you sure you want to duplicate'#13#10'%s'#13#10' into your repository ?'; // TODO : 'in' vs 'into' ?
+  rsDownloadingPackage = 'Downloading %s';
+  rsDuplicating = 'Duplicating %s';
+  rsDuplicateSuccess = '%s successfully duplicated.';
+  rsDuplicateFailure = 'Error while duplicating.';
 
 
 
