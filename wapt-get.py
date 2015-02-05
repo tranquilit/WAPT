a8a839a81a0f73d17acaedf92ffa7f4ceef6140c
htouvet <htouvet-hithub@tranquil.it>
2015-02-05 11:41:35 +0100

    bumped some python version to 1.0.1

--- wapt-get.py
+++ wapt-get.py
@@ -20,7 +20,8 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "0.9.9"
+__version__ = "1.0.1"
+
 import codecs
 import getpass
 import glob
