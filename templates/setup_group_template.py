88f73e0111b9fea5dcdf16302772ad24d8dab686
htouvet <htouvet-github@tranquil.it>
2015-02-13 17:39:08 +0100

    remove unused stiff in package templates

--- templates/setup_group_template.py
+++ templates/setup_group_template.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuphelpers import *
 
-# registry key(s) where WAPT will find how to remove the application(s)
 uninstallkey = []
 
-
 def install():
-    # if you want to modify the keys depending on environment (win32/win64... params..)
-    global uninstallkey
-    global uninstallstring
     print('installing %(packagename)s')
--- templates/setup_host_template.py
+++ templates/setup_host_template.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuphelpers import *
 
-# registry key(s) where WAPT will find how to remove the application(s)
 uninstallkey = []
-
 
 def install():
     print('installing %(packagename)s')
