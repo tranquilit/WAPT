6dd5c0f0c8879d6d0b273919a362b5c7462067b8
htouvet <htouvet-hithub@tranquil.it>
2015-02-25 08:55:23 +0100

    bump to version 1.1.1

--- waptserver/waptserver.py
+++ waptserver/waptserver.py
@@ -20,7 +20,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__="1.1.0"
+__version__="1.1.1"
 
 import os,sys
 try:
