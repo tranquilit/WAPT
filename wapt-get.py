e62db340beb9e26a4352e3fa9a779dc25094dd7b
htouvet <htouvet-github@tranquil.it>
2015-04-22 19:00:15 +0200

    wapt-get.py version to 1.2.3

--- wapt-get.py
+++ wapt-get.py
@@ -20,7 +20,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 import codecs
 import getpass
