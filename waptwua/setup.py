117859e8a183d1650f085bdc1ad429c3afe38bca
htouvet <htouvet-github@tranquil.it>
2015-05-19 18:03:39 +0200

    waptwua task downloads the critical windows updates

--- waptwua/setup.py
+++ waptwua/setup.py
@@ -12,7 +12,7 @@ def install():
     mkdirs(makepath(waptwua_path,'cache'))
     mkdirs(waptwuabin_path)
     filecopyto('waptwua.py',waptwuabin_path)
-    create_daily_task('waptwua',waptpython_path,'"%s" download' % (makepath(waptwuabin_path,'waptwua.py'),))
+    create_daily_task('waptwua',waptpython_path,'"%s" --critical download' % (makepath(waptwuabin_path,'waptwua.py'),))
 
 def uninstall():
     delete_task('waptwua')
