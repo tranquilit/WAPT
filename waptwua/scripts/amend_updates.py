cdddea7b43b0dda5ccbeabdce1a6f788b76121e0
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-05-20 16:44:00 +0200

    WIP scripts to populate mongodb from wsusscan2.cab

---
+++ waptwua/scripts/amend_updates.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    #wapt_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+    wapt_root_dir = '/opt/wapt'
+except:
+    wapt_root_dir = 'c:/tranquilit/wapt'
+
+sys.path.insert(0,os.path.join(wapt_root_dir))
+sys.path.insert(0,os.path.join(wapt_root_dir,'lib'))
+sys.path.insert(0,os.path.join(wapt_root_dir,'lib','site-packages'))
+
+import collections
+import pymongo
+
+from lxml import etree as ET
+
+PFX = "{http://schemas.microsoft.com/msus/2002/12/Update}"
+
+def qualify(tag):
+    return PFX + tag
+
+def find_cab(rev, cabset):
+    for start, cab in cabset.items():
+        if int(rev) > int(start):
+            return cab
+
+
+def parse_metadata(upd, descr_file):
+
+    debug = False
+    #debug = True
+
+    data = {}
+
+    if not os.path.exists(descr_file):
+        return
+    if os.path.getsize(descr_file) == 0:
+        return
+
+    try:
+        xml_str = file(descr_file, 'r').read()
+        root = ET.fromstring(xml_str)
+
+        if debug:
+            print ""
+
+
+        props = root.find(qualify('Properties'))
+
+        msrc_severity = props.get('MsrcSeverity')
+        if msrc_severity is not None:
+            data['msrc_severity'] = msrc_severity
+            if debug:
+                print 'MsrcSeverity', data['msrc_severity']
+
+        elem = props.find(qualify('KBArticleID'))
+        if elem is not None:
+            data['kb_article_id'] = elem.text
+            if debug:
+                print 'KBArticleID', data['kb_article_id']
+
+        elem = props.find(qualify('SecurityBulletinID'))
+        if elem is not None:
+            data['security_bulletin_id'] = elem.text
+            print 'SecurityBulletinID', data['security_bulletin_id']
+
+        localized_properties_collection = root.find(qualify('LocalizedPropertiesCollection'))
+        for elem in localized_properties_collection.iter():
+            if elem.tag.endswith('LocalizedProperties'):
+
+                lang = elem.find(qualify('Language'))
+                if lang is not None:
+                    if lang.text != 'en':
+                        break
+                else:
+                    continue
+
+                title = elem.find(qualify('Title'))
+                if title is not None and title.text != '':
+                    data['title'] = title.text
+                    if debug:
+                        print 'Title:', data['title']
+
+                descr = elem.find(qualify('Description'))
+                if descr is not None and descr.text != '':
+                    data['description'] = descr.text
+                    if debug:
+                        print 'Description:', data['description']
+
+    except Exception, e:
+        print >> sys.stderr, "Error while using %s: %s" % (descr_file, str(e))
+    return data
+
+
+def main(directory):
+    if not os.path.isdir(directory):
+        print >> sys.stderr, '%s is not a directory' % directory
+
+    xmlindex = os.path.join(directory, 'index.xml')
+    tree = ET.parse(xmlindex)
+    root = tree.getroot()
+    cablist = root.find('CABLIST').findall('CAB')
+
+    cabs = {}
+    for cab in cablist:
+        cabname = cab.get('NAME')
+        rangestart = cab.get('RANGESTART')
+        # 'package.cab' has no rangestart attribute
+        if rangestart is None:
+            continue
+        # strip the extension, keep the directory name
+        cabs[rangestart] = cabname[:-len('.cab')]
+
+    cabs = collections.OrderedDict(
+        sorted(
+            cabs.items(),
+            key=lambda i: int(i[0]),
+            reverse=True
+        )
+    )
+
+    client = pymongo.MongoClient()
+    db = client.wapt
+
+    for update in db.wsus_updates.find():
+
+        rev = update.get('revision_id')
+        # no revision -> no metadata on disk
+        if rev is None:
+            continue
+
+        cab_dir = find_cab(rev, cabs)
+
+        descr_file = os.path.join(directory, cab_dir, 's', rev)
+        metadata = parse_metadata(update, descr_file)
+        if metadata:
+            # XXX add data to MongoDB
+            db.wsus_updates.update(
+                { "_id": update["_id"] },
+                {
+                    "$set": metadata,
+                }
+            )
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print >> sys.stderr, 'Usage: %s <packages_directory>' % sys.argv[0]
+        exit(1)
+    main(sys.argv[1])
---
+++ waptwua/scripts/parse_package.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os,sys
+try:
+    #wapt_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+    wapt_root_dir = '/opt/wapt'
+except:
+    wapt_root_dir = 'c:/tranquilit/wapt'
+
+sys.path.insert(0,os.path.join(wapt_root_dir))
+sys.path.insert(0,os.path.join(wapt_root_dir,'lib'))
+sys.path.insert(0,os.path.join(wapt_root_dir,'lib','site-packages'))
+
+from pymongo import MongoClient
+
+from lxml import etree as ET
+
+OFFLINE_SYNC_NAMESPACE = 'http://schemas.microsoft.com/msus/2004/02/OfflineSync'
+OFFLINE_SYNC = '{' + OFFLINE_SYNC_NAMESPACE + '}'
+
+UpdateClassification = {
+    '5C9376AB-8CE6-464A-B136-22113DD69801'.lower(): 'Application',
+    '434DE588-ED14-48F5-8EED-A15E09A991F6'.lower(): 'Connectors',
+    'E6CF1350-C01B-414D-A61F-263D14D133B4'.lower(): 'CriticalUpdates',
+    'E0789628-CE08-4437-BE74-2495B842F43B'.lower(): 'DefinitionUpdates',
+    'E140075D-8433-45C3-AD87-E72345B36078'.lower(): 'DeveloperKits',
+    'B54E7D24-7ADD-428F-8B75-90A396FA584F'.lower(): 'FeaturePacks',
+    '9511D615-35B2-47BB-927F-F73D8E9260BB'.lower(): 'Guidance',
+    '0FA1201D-4330-4FA8-8AE9-B877473B6441'.lower(): 'SecurityUpdates',
+    '68C5B0A3-D1A6-4553-AE49-01D3A7827828'.lower(): 'ServicePacks',
+    'B4832BD8-E735-4761-8DAF-37F882276DAB'.lower(): 'Tools',
+    '28BC880E-0592-4CBF-8F95-C79B17911D5F'.lower(): 'UpdateRollups',
+    'CD5FFD1E-E932-4E3A-BF74-18BF0B1BBD83'.lower(): 'Updates',
+}
+
+UpdateCategories = [
+    'Company',
+    'Product',
+    'ProductFamily',
+    'UpdateClassification',
+]
+
+updates = 0
+locations = 0
+payload_files_found = 0
+files_found = 0
+
+client = MongoClient()
+db = client.wapt
+
+def qualify(tag):
+    return OFFLINE_SYNC + tag
+
+def parse_update(update):
+    global updates, payload_files_found, files_found
+
+    superseded = update.findall(qualify('SupersededBy'))
+    if superseded:
+        return
+
+    updates += 1
+
+    upd = {}
+
+    upd['update_id'] = update.get('UpdateId')
+
+    if update.get('isBundle', False):
+        upd['isBundle'] = True
+
+    if update.get('isLeaf', False):
+        upd['isLeaf'] = True
+
+    if update.get('RevisionId', False) != False:
+        upd['revision_id'] = update.get('RevisionId')
+
+    categories = update.findall(qualify('Categories'))
+    if categories:
+        upd['categories'] = {}
+        for cat in categories:
+            for subcat in cat.getchildren():
+                type_ = subcat.get('Type')
+                if type_ in UpdateCategories:
+                    upd['categories'][type_] = subcat.get('Id', '').lower()
+
+    prereqs = update.findall(qualify('Prerequisites'))
+    if prereqs:
+        upd['prereqs'] = []
+        for prereq in prereqs:
+            for update in prereq.iterchildren(qualify('UpdateId')):
+                upd['prereqs'].append(update.get('Id').lower())
+
+    # FIXME the following statements don't find all the necessary <PayloadFiles>
+    files = update.findall(qualify('PayloadFiles'))
+    if files:
+        upd['payload_files'] = []
+        for files_ in files:
+            payload_files_found += 1
+            for f in files_.iter(qualify('File')):
+                files_found += 1
+                upd['payload_files'].append(f.get('Id'))
+
+    db.wsus_updates.insert(upd)
+
+
+def parse_file_location(location):
+    global locations
+    locations += 1
+
+    location_id = location.get('Id')
+    location_url = location.get('Url')
+
+    locations_collection = db.wsus_locations
+    locations_collection.insert({
+        'id': location_id,
+        'url': location_url,
+    })
+
+
+def get_dl_urls(package_xml):
+    for _, elem in ET.iterparse(package_xml):
+        if elem.tag == qualify('Update'):
+            parse_update(elem)
+        elif elem.tag == qualify('FileLocation'):
+            parse_file_location(elem)
+
+
+def main(args):
+    global updates, locations, payload_files_found, files_found
+
+    try:
+        get_dl_urls(args[0])
+    except Exception, e:
+        print >> sys.stderr, "Warning:", str(e)    
+    print "Updates inserted: " + str(updates)
+    print "Locations inserted: " + str(locations)
+    print "PayloadFiles/File statements found: " + str(payload_files_found) + "/" + str(files_found)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print >> sys.stderr, 'Usage: %s /path/to/package.xml' % sys.argv[0]
+        exit(1)
+    main(sys.argv[1:])
