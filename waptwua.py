1f57072e80a3a4aea265b83fdfc95a4946902c49
htouvet <htouvet-github@tranquil.it>
2015-05-29 17:16:07 +0200

    filter updates based on waptwua.forbidden_updates, waptwua.allowed_updates waptwua.allowed_severities wapt parameters

--- waptwua.py
+++ waptwua.py
@@ -36,6 +36,7 @@
 
 from setuphelpers import *
 
+import sys
 import os
 import win32com.client
 import json
@@ -45,6 +46,7 @@ from urlparse import urlparse
 
 import datetime
 import requests
+import logging
 
 InstallResult = {
   0: 'NotStarted',
@@ -56,7 +58,7 @@ InstallResult = {
  }
 
 class WaptWUA(object):
-    def __init__(self,wapt,allowed_updates=None, filter="Type='Software'", allow_critical=False):
+    def __init__(self,wapt,allowed_updates=None, forbidden_updates= None, allowed_severities=None, filter="Type='Software'"):
         self.wapt = wapt
         self.cache_path = os.path.abspath(makepath(wapt.wapt_base_dir,'waptwua','cache'))
         self._update_session = None
@@ -64,19 +66,40 @@ class WaptWUA(object):
         self._update_searcher = None
         self._update_service = None
         self.filter = filter
-        self.allow_critical = allow_critical
         if allowed_updates is not None:
             self.allowed_updates = allowed_updates
         else:
             au = self.wapt.read_param('waptwua.allowed_updates')
             if au:
-                self.allowed_updates = json.loads(au)
+                self.allowed_updates = json.loads(au) or '[]'
             else:
                 self.allowed_updates = []
+
+        if forbidden_updates is not None:
+            self.forbidden_updates = forbidden_updates
+        else:
+            fu = self.wapt.read_param('waptwua.forbidden_updates') or '[]'
+            if au:
+                self.forbidden_updates = json.loads(fu)
+            else:
+                self.forbidden_updates = []
+
+        if allowed_severities is not None:
+            self.allowed_severities = allowed_severities
+        else:
+            au = self.wapt.read_param('waptwua.allowed_severities') or '[]'
+            if au:
+                self.allowed_severities = json.loads(au)
+            else:
+                self.allowed_severities = []
 
         self._updates = None
         # to store successful changes in read only properties of _updates after initial scan
         self._cached_updates = {}
+        logger.info('Forbidden updates:%s'%self.forbidden_updates or 'None')
+        logger.info('Allowed severities:%s'%(self.allowed_severities or 'All'))
+        logger.info('Allowed additional specific updates:%s'%self.allowed_updates or 'None')
+
 
     def cached_update_property(self,update,key):
         update_id = update.Identity.UpdateID
@@ -141,17 +164,17 @@ class WaptWUA(object):
                     cab_location,
                     timeout=self.wapt.repositories[0].timeout,
                     proxies=self.wapt.repositories[0].proxies,
-                    verify=False,
+                    verify=self.wapt.repositories[0].verify_cert,
                     ).headers['last-modified'])
                 if not isfile(cab_target) or (cab_new_date > cab_current_date ):
                     wget(cab_location,cab_target,proxies=self.wapt.repositories[0].proxies,connect_timeout=self.wapt.repositories[0].timeout)
                     self.wapt.write_param('waptwua.wsusscn2cab_date',cab_new_date)
-
+                    logger.debug('New wusscn2.cab date : %s'%cab_new_date)
+                self.wapt.write_param('waptwua.status','DBREADY')
                 return cab_new_date
             except requests.RequestException as e:
+                self.wapt.write_param('waptwua.status','ERROR')
                 return None
-            finally:
-                self.wapt.write_param('waptwua.status','READY')
 
     @property
     def update_searcher(self):
@@ -197,40 +220,52 @@ class WaptWUA(object):
 
 
     def is_allowed(self,update):
-        """Check if an update is allowed"""
+        """Check if an update is allowed
+            allowed if
+        """
         kbs = [ "KB%s" % kb for kb in update.KBArticleIDs ]
-        match_kb = False
+        allowed_kb = False
+        forbidden_kb = False
         for kb in kbs:
             if kb in self.allowed_updates:
-                match_kb = True
+                allowed_kb = True
                 break
-        return update.Identity.UpdateID in self.allowed_updates or match_kb or (self.allow_critical and update.MsrcSeverity == 'Critical')
+        for kb in kbs:
+            if kb in self.forbidden_updates:
+                forbidden_kb = True
+                break
+        return not forbidden_kb and not update.Identity.UpdateID in self.forbidden_updates and \
+            (update.Identity.UpdateID in self.allowed_updates or allowed_kb or update.MsrcSeverity in self.allowed_severities or (not update.MsrcSeverity and self.allowed_severities is None))
 
     def scan_updates_status(self):
         """Check all updates and filter out which one should be installed"""
         installed,pending,discarded = 0,0,0
+        logger.debug('Scanning installed / not installed Updates')
         for update in self.updates:
             if not self.cached_update_property(update,'IsInstalled'):
                 if self.is_allowed(update):
                     # IUpdate : https://msdn.microsoft.com/en-us/library/windows/desktop/aa386099(v=vs.85).aspx
                     # IUpdate2 : https://msdn.microsoft.com/en-us/library/windows/desktop/aa386100(v=vs.85).aspx
-                    print('Adding %s : %s' % (update.Identity.UpdateID,update.Title ))
+                    logger.info('Adding %s : %s' % (update.Identity.UpdateID,update.Title ))
                     update.IsHidden = False
                     pending += 1
                 else:
-                    print('Skipping %s : %s' % (update.Identity.UpdateID,update.Title ))
+                    logger.info('Skipping %s : %s' % (update.Identity.UpdateID,update.Title ))
                     update.IsHidden = True
                     discarded += 1
             else:
+                logger.debug('Already installed %s : %s' % (update.Identity.UpdateID,update.Title ))
                 installed += 1
 
+        logger.debug('Writing status in local wapt DB')
         self.wapt.write_param('waptwua.updates',json.dumps([ self.update_as_dict(u) for u in self.updates]))
         self.wapt.write_param('waptwua.last_scan_date',datetime2isodate())
         if not pending:
             self.wapt.write_param('waptwua.status','OK')
         else:
-            self.wapt.write_param('waptwua.status','PENDING')
+            self.wapt.write_param('waptwua.status','PENDING_UPDATES')
         # send status to wapt server
+        logger.debug('Updating workstation status on remote wapt server')
         self.wapt.update_server_status()
         return (installed,pending,discarded)
 
@@ -359,17 +394,50 @@ class WaptWUA(object):
 
 if __name__ == '__main__':
     parser=OptionParser(usage=__doc__)
-    parser.add_option("-a","--allowed", dest="allowed", default='', help="List of updates uuid or KB to apply (default: %default)")
+    parser.add_option("-S","--severity", dest="severity", default='All', help="Allow updates by severity. csv list of  Critical,Important,Moderate,Low (default: %default)")
+    parser.add_option("-a","--allowed", dest="allowed", default=None, help="Allow updates by update-id or KB. csv list of id to allow (default: %default)")
+    parser.add_option("-b","--forbidden", dest="forbidden", default=None, help="Forbid updates by update-id or KB. csv list (default: %default)")
     parser.add_option("-c","--config", dest="config", default=None, help="Config file full path (default: %default)")
-    parser.add_option("-C","--critical", dest="allow_critical", default=False, action='store_true', help="Allows all 'Critical' updates too (default: %default)")
+    parser.add_option("-l","--loglevel", dest="loglevel", default=None, type='choice',  choices=['debug','warning','info','error','critical'], metavar='LOGLEVEL',help="Loglevel (default: warning)")
     #parser.add_option("-d","--dry-run", dest="dry_run",    default=False, action='store_true', help="Dry run (default: %default)")
+
     (options,args) = parser.parse_args()
+
+    logger = logging.getLogger()
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+
+    def setloglevel(logger,loglevel):
+        """set loglevel as string"""
+        if loglevel in ('debug','warning','info','error','critical'):
+            numeric_level = getattr(logging, loglevel.upper(), None)
+            if not isinstance(numeric_level, int):
+                raise ValueError(_('Invalid log level: {}').format(loglevel))
+            logger.setLevel(numeric_level)
+
+    # force loglevel
+    if options.loglevel is not None:
+        setloglevel(logger,options.loglevel)
+
 
     from common import Wapt
     wapt = Wapt(config_filename=options.config)
 
-    allowed_updates = ensure_list(options.allowed) or None
-    wua = WaptWUA(wapt,allowed_updates=allowed_updates,allow_critical=options.allow_critical)
+    if options.allowed is not None:
+        allowed_updates = ensure_list(options.allowed)
+    else:
+        allowed_updates = None
+
+    if options.forbidden is not None:
+        forbidden_updates = ensure_list(options.forbidden)
+    else:
+        forbidden_updates = None
+
+    if options.severity is not None:
+        allowed_severities = ensure_list(options.severity)
+    else:
+        allowed_severities = None
+
+    wua = WaptWUA(wapt,allowed_updates=allowed_updates,forbidden_updates=forbidden_updates,allowed_severities=allowed_severities)
     if len(args) <1:
         print parser.usage
         sys.exit(1)
