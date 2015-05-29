76892ee83cb5bbdc862fe17666523b0d454e3554
htouvet <htouvet-github@tranquil.it>
2015-05-29 18:59:11 +0200

    use allowed_updates, forbidden_updates, allowed_severities

--- waptwua/setup.py
+++ waptwua/setup.py
@@ -1,47 +1,57 @@
 # -*- coding: utf-8 -*-
 from setuphelpers import *
 import json
+import random
+import datetime
 
 uninstallkey = []
 
 def update_control(control):
     restrictions = WAPT.waptserver.get('api/v2/windows_updates_restrictions?group=default')['result']
     if restrictions:
-        enabled = restrictions[0]['enabled']
-        if enabled:
-            open('enabled_updates.lst','w').write('\n'.join(enabled))
+        allowed = restrictions[0]['allowed']
+        if allowed:
+            open('allowed_updates.lst','w').write('\n'.join(allowed))
         else:
-            remove_file('enabled_updates.lst')
+            remove_file('allowed_updates.lst')
 
-        discarded = restrictions[0]['discarded']
-        if discarded:
-            open('discarded_updates.lst','w').write('\n'.join(discarded))
+        forbidden = restrictions[0]['forbidden']
+        if forbidden:
+            open('forbidden_updates.lst','w').write('\n'.join(forbidden))
         else:
-            remove_file('discarded_updates.lst')
+            remove_file('forbidden_updates.lst')
 
 def install():
-    print('installing tis-waptwsus')
     waptpython_path = makepath(WAPT.wapt_base_dir,'waptpython.exe')
     waptwua_path = makepath(WAPT.wapt_base_dir,'waptwua')
     waptwuabin_path = WAPT.wapt_base_dir
 
-    if isfile('enabled_updates.lst'):
-        enabled = open('enabled_updates.lst','r').read().splitlines()
-        WAPT.write_param('waptwua.allowed_updates',json.dumps(enabled))
+    if isfile('allowed_updates.lst'):
+        allowed = open('allowed_updates.lst','r').read().splitlines()
+        WAPT.write_param('waptwua.allowed_updates',json.dumps(allowed))
+
+    if isfile('forbidden_updates.lst'):
+        forbidden = open('forbidden_updates.lst','r').read().splitlines()
+        WAPT.write_param('waptwua.forbidden_updates',json.dumps(forbidden))
+
+    if isfile('allowed_severities.lst'):
+        allowed_severities = open('allowed_severities.lst','r').read().splitlines()
+        WAPT.write_param('waptwua.allowed_severities',json.dumps(allowed_severities))
 
     mkdirs(makepath(waptwua_path,'cache'))
     mkdirs(waptwuabin_path)
     if task_exists('waptwua'):
         delete_task('waptwua')
 
-    if isfile('enabled_updates.lst'):
-        create_daily_task('waptwua',waptpython_path,'"%s" download' % (makepath(waptwuabin_path,'waptwua.py'),))
-    else:
-        create_daily_task('waptwua',waptpython_path,'"%s" --critical download' % (makepath(waptwuabin_path,'waptwua.py'),))
-    run_task('waptwua')
+    dt = datetime.datetime.now()+datetime.timedelta(hours=random.randrange(0,5),minutes=random.randrange(0,59))
+    create_daily_task('waptwua',waptpython_path,'"%s" download' % (makepath(waptwuabin_path,'waptwua.py'),),start_hour=dt.hour,start_minute=dt.minute)
 
 def uninstall():
     delete_task('waptwua')
     waptwua_path = makepath(WAPT.wapt_base_dir,'waptwua')
+    WAPT.delete_param('waptwua.allowed_updates')
+    WAPT.delete_param('waptwua.forbidden_updates')
+    WAPT.delete_param('waptwua.allowed_severities')
+    WAPT.delete_param('waptwua.allowed_severities')
     remove_tree(waptwua_path)
 
