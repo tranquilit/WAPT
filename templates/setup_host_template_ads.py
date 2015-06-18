c97c509ecc5791ff92b317879b8dc1939fc219a0
htouvet <htouvet-github@tranquil.it>
2015-06-18 11:42:30 +0200

    host package template updates host description to match host package description

--- templates/setup_host_template_ads.py
+++ templates/setup_host_template_ads.py
@@ -5,9 +5,10 @@ import sys
 uninstallkey = []
 
 def update_control(control):
+    # try to update package description to match computer description in active directory
     try:
         import active_directory
-        for pc in active_directory.search(objectCategory='Computer', objectClass='Computer',dNShostname='%(packagename)s'):
+        for pc in active_directory.search(objectCategory='Computer', objectClass='Computer',dNShostname=control.package):
             if pc.description:
                 control.description = pc.description
     except:
@@ -15,5 +16,5 @@ def update_control(control):
 
 def install():
     if control.description:
+        print('Change local computer description to match package description')
         print run_notfatal('WMIC os set description="'+control.description.splitlines()[0].encode(sys.getfilesystemencoding())+'"')
-
