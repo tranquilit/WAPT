1cc79538181c0244979d71630445951ac1c91a19
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-08-17 17:45:41 +0200

    Revert to an almost unmodified huey_consumer, by wrapping its main() method in wapthuey.py
    Discussed in #537

--- waptserver/wapthuey.py
+++ waptserver/wapthuey.py
@@ -1,3 +1,20 @@
-#! python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    wapt_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+except:
+    wapt_root_dir = 'c:/tranquilit/wapt'
+
+sys.path.insert(0,os.path.join(wapt_root_dir))
+sys.path.insert(0,os.path.join(wapt_root_dir,'lib'))
+sys.path.insert(0,os.path.join(wapt_root_dir,'lib','site-packages'))
+
+from huey.bin import huey_consumer
+
+if __name__ == '__main__':
+    huey_consumer.main()
 
