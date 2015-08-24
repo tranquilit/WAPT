f90f16489f8ad0dbd8413dd766eb8d98c2223ad2
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-08-24 10:07:39 +0200

    Always use 127.0.0.1:27017 as mongodb target
    In practice, nobody tweaks this.

--- waptserver/scripts/postconf.py
+++ waptserver/scripts/postconf.py
@@ -157,7 +157,7 @@ def enable_redhat_vhost():
         void = subprocess.check_output('systemctl restart httpd', stderr=subprocess.STDOUT, shell=True)
 
 
-# main program 
+# main program
 def main():
 
     if postconf.yesno("Do you want to launch post configuration tool ?") != postconf.DIALOG_OK:
@@ -186,36 +186,6 @@ def main():
         # for install on windows
         # keep in sync with waptserver.py
         wapt_folder = os.path.join(wapt_root_dir,'waptserver','repository','wapt')
-
-    code, tag = postconf.menu("Mongodb server location",
-                              choices=[("1", "localhost (default)"),
-                                    ("2", "other server")])
-    if code == postconf.DIALOG_OK:
-        if tag == "1":
-            waptserver_ini.set('options','mongodb_ip','127.0.0.1')
-        elif tag == "2":
-            (code,mongodb_ip) = postconf.inputbox("IP address of the mongodb server:")
-            if code != postconf.DIALOG_OK:
-                exit(1)
-            else:
-                waptserver_ini.set('options','mongodb_ip',mongodb_ip)
-        elif code != postconf.DIALOG_OK:
-            exit(1)
-
-    code, tag = postconf.menu("Mongodb server port: ",
-                                  choices=[("1", "27017 (default)"),
-                                        ("2", "other")])
-    if code == postconf.DIALOG_OK:
-        if tag == "1":
-            waptserver_ini.set('options','mongodb_port','27017')
-        elif tag == "2":
-            (code,mongodb_port) = postconf.inputbox("mongodb server port: ")
-            if code != postconf.DIALOG_OK:
-                exit(1)
-            else:
-                waptserver_ini.set('options','mongodb_port',mongodb_port)
-        elif code != postconf.DIALOG_OK:
-            exit(0)
 
     wapt_password_ok = False
     while not wapt_password_ok:
