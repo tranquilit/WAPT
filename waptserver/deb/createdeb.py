a87e56e37295a312bb9e7e16071840625121e2e4
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-11-03 11:48:33 +0100

    Don't copy editors backup files

--- waptserver/deb/createdeb.py
+++ waptserver/deb/createdeb.py
@@ -42,7 +42,7 @@ def replaceAll(file,searchExp,replaceExp):
         sys.stdout.write(line)
 
 def rsync(src,dst,excludes=[]):
-    rsync_option = " --exclude '*.pyc' --exclude '.svn' --exclude 'deb' --exclude '.git' --exclude '.gitignore' -aP"
+    rsync_option = " --exclude '*.pyc' --exclude '*~' --exclude '.svn' --exclude 'deb' --exclude '.git' --exclude '.gitignore' -aP"
     if excludes:
         rsync_option = rsync_option + ' '.join(" --exclude '%s'" % x for x in excludes)
     rsync_source = src
