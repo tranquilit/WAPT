e091f8e5d32362000fa2f1c2db06364acda8e473
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-11-05 11:03:42 +0100

    Pimp my rpmbuild

--- waptrepo/rpm/build.sh
+++ waptrepo/rpm/build.sh
@@ -1,1 +1,9 @@
-rpmbuild -bb --buildroot /root/wapt/waptrepo/rpm/builddir  -v  --clean ./waptrepo.spec
+#!/usr/bin/env bash
+
+set -e
+
+for d in BUILD RPMS SOURCES SPECS SRPMS; do
+    mkdir -p buildroot/$d
+done
+
+rpmbuild -bb --buildroot $PWD/builddir -v --clean ./waptrepo.spec
