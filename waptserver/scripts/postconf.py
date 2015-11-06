5a2b700fa364d5ecbe0689db98ac4c7599a92b64
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-11-06 16:56:50 +0100

    Just run that damn selinux command

--- waptserver/scripts/postconf.py
+++ waptserver/scripts/postconf.py
@@ -165,10 +165,7 @@ def main():
 
     if type_redhat():
         if re.match('^SELinux status:.*enabled', subprocess.check_output('sestatus')):
-            postconf.msgbox("""You have SELinux enabled.
-		Either disable it or run the following command:
-		setsebool -P httpd_can_network_connect 1
-		""")
+            subprocess.check_call(['setsebool', '-P', 'httpd_can_network_connect', '1'])
 
     shutil.copyfile('/opt/wapt/waptserver/waptserver.ini.template','/opt/wapt/conf/waptserver.ini')
     os.symlink('/opt/wapt/conf/waptserver.ini','/opt/wapt/waptserver/waptserver.ini')
