c4ddf617a5335037b94c3eddec40d7ce31799c4a
htouvet <htouvet-github@tranquil.it>
2015-11-09 10:07:44 +0100

    add missing unit uvispackagewizard.pas

---
+++ waptconsole/uvispackagewizard.pas
@@ -0,0 +1,30 @@
+unit uVisPackageWizard;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ButtonPanel1: TButtonPanel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
