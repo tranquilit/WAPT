e4106b1f6fb5e9d8e49aef7409935cba39b0470c
Jérémie Courrèges-Anglas <jca@tranquil.it>
2015-11-10 14:16:15 +0100

    Missed in previous

--- waptserver/rpm/createrpm.py
+++ waptserver/rpm/createrpm.py
@@ -135,6 +135,7 @@ except Exception as e:
     exit(1)
 
 print >> sys.stderr, "adding symlink for wapt-serverpostconf"
+mkdir_p('builddir/usr/bin')
 os.symlink('/opt/wapt/waptserver/scripts/postconf.py', 'builddir/usr/bin/wapt-serverpostconf')
 
 print >> sys.stderr, "copying apache-related goo"
