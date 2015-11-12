0be929cda39f6836634ddf216f3b10617edce612
htouvet <htouvet-github@tranquil.it>
2015-11-12 12:51:12 +0100

    move sha utils to waptwinutils.pas

--- wapt-get/waptwinutils.pas
+++ wapt-get/waptwinutils.pas
@@ -77,14 +77,128 @@ function BasicRegistrationData: ISuperObject;
 
 function ReadRegEntry(strSubKey, strValueName: ansistring): ansistring;
 
+function WaptBaseDir: Utf8String;
 function WaptGetIniPath: string;
 function WaptIniReadString(Parameter, DefaultValue: string): string;
 function LocalWaptVersion: ansistring;
 
+function SHA1Hash(FilePath: ansistring): ansistring;
+function SHA256Hash(FilePath: ansistring): ansistring;
+
 implementation
 
 uses Variants, registry, sysconst, JwaIpHlpApi,
-  JwaIpTypes, JwaWinDNS, JwaWinsock2, tisinifiles, soutils;
+  JwaIpTypes, JwaWinDNS, JwaWinsock2, tisinifiles, soutils, DCPsha1,DCPsha256;
+
+
+function BinToStr(const Bin: array of byte): ansistring;
+const
+  HexSymbols = '0123456789ABCDEF';
+var
+  i: integer;
+begin
+  SetLength(Result, 2 * Length(Bin));
+  for i := 0 to Length(Bin) - 1 do
+  begin
+    Result[1 + 2 * i + 0] := HexSymbols[1 + Bin[i] shr 4];
+    Result[1 + 2 * i + 1] := HexSymbols[1 + Bin[i] and $0F];
+  end;
+end;
+
+
+function SHA1Hash(FilePath: ansistring): ansistring;
+var
+  Context: TDCP_sha1;
+  Buf: PByte;
+  BufSize, ReadSize, TotalSize: integer;
+  FileStream: TFileStream;
+  RawDigest: array[0..31] of byte;
+begin
+  Result := '';
+  FileStream := nil;
+  Buf := nil;
+  Context := nil;
+
+  TotalSize := 0;
+  Bufsize := 32 * 1024; // 32k
+
+  try
+    FileStream := TFileStream.Create(FilePath, fmOpenRead);
+    FileStream.Position := 0;
+    Buf := GetMem(BufSize);
+    Context := TDCP_sha1.Create(nil);
+    Context.Init;
+
+    while True do
+    begin
+      ReadSize := FileStream.Read(Buf^, BufSize);
+      if ReadSize <= 0 then
+        break;
+      Context.Update(Buf^, ReadSize);
+    end;
+
+    Context.Final(RawDigest);
+
+    Result := BinToStr(RawDigest);
+
+  finally
+    if FileStream <> nil then
+      FileStream.Free;
+    if Buf <> nil then
+      FreeMem(Buf);
+    if Context <> nil then
+      Context.Free;
+  end;
+end;
+
+
+
+function SHA256Hash(FilePath: ansistring): ansistring;
+var
+  Context: TDCP_sha256;
+  Buf: PByte;
+  BufSize, ReadSize, TotalSize: integer;
+  FileStream: TFileStream;
+  RawDigest: array[0..31] of byte;
+begin
+  Result := '';
+  FileStream := nil;
+  Buf := nil;
+  Context := nil;
+
+  TotalSize := 0;
+  Bufsize := 32 * 1024; // 32k
+
+  try
+    FileStream := TFileStream.Create(FilePath, fmOpenRead);
+    FileStream.Position := 0;
+    Buf := GetMem(BufSize);
+    Context := TDCP_sha256.Create(nil);
+    Context.Init;
+
+    while True do
+    begin
+      ReadSize := FileStream.Read(Buf^, BufSize);
+      if ReadSize <= 0 then
+        break;
+      Context.Update(Buf^, ReadSize);
+    end;
+
+    Context.Final(RawDigest);
+
+    Result := BinToStr(RawDigest);
+
+  finally
+    if FileStream <> nil then
+      FileStream.Free;
+    if Buf <> nil then
+      FreeMem(Buf);
+    if Context <> nil then
+      Context.Free;
+  end;
+end;
+
+
 
 
 Function IPV4ToInt(ipaddr:AnsiString):LongWord;
@@ -678,6 +792,20 @@ begin
     raise Exception.Create('Wrong key HKLM\' + strSubKey);
 end;
 
+function WaptBaseDir: Utf8String;
+begin
+  if FileExists('c:\wapt') then
+    Result := 'c:\wapt'
+  else
+  if FileExists(SysUtils.GetEnvironmentVariable('ProgramFiles(x86)') +
+    '\wapt') then
+    Result := SysUtils.GetEnvironmentVariable('ProgramFiles(x86)') + '\wapt'
+  else
+  if FileExists(SysUtils.GetEnvironmentVariable('ProgramFiles') + '\wapt') then
+    Result := SysUtils.GetEnvironmentVariable('ProgramFiles') + '\wapt'
+  else
+    Result := 'c:\wapt';
+end;
 
 function WaptGetIniPath: string;
 begin
--- waptdeploy/waptdeploy.lpr
+++ waptdeploy/waptdeploy.lpr
@@ -158,65 +158,6 @@ Connection: Keep-Alive}
     Result := '';
   end;
 
-  function BinToStr(const Bin: array of byte): ansistring;
-  const
-    HexSymbols = '0123456789ABCDEF';
-  var
-    i: integer;
-  begin
-    SetLength(Result, 2 * Length(Bin));
-    for i := 0 to Length(Bin) - 1 do
-    begin
-      Result[1 + 2 * i + 0] := HexSymbols[1 + Bin[i] shr 4];
-      Result[1 + 2 * i + 1] := HexSymbols[1 + Bin[i] and $0F];
-    end;
-  end;
-
-  function SHA256Hash(FilePath: ansistring): ansistring;
-  var
-    Context: TDCP_sha256;
-    Buf: PByte;
-    BufSize, ReadSize, TotalSize: integer;
-    FileStream: TFileStream;
-    RawDigest: array[0..31] of byte;
-  begin
-    Result := '';
-    FileStream := nil;
-    Buf := nil;
-    Context := nil;
-
-    TotalSize := 0;
-    Bufsize := 32 * 1024; // 32k
-
-    try
-      FileStream := TFileStream.Create(FilePath, fmOpenRead);
-      FileStream.Position := 0;
-      Buf := GetMem(BufSize);
-      Context := TDCP_sha256.Create(nil);
-      Context.Init;
-
-      while True do
-      begin
-        ReadSize := FileStream.Read(Buf^, BufSize);
-        if ReadSize <= 0 then
-          break;
-        Context.Update(Buf^, ReadSize);
-      end;
-
-      Context.Final(RawDigest);
-
-      Result := BinToStr(RawDigest);
-
-    finally
-      if FileStream <> nil then
-        FileStream.Free;
-      if Buf <> nil then
-        FreeMem(Buf);
-      if Context <> nil then
-        Context.Free;
-    end;
-  end;
-
 
   // Returns only non options arguments of command line (those not starting with -- or -)
   function CommandParams: TDynStringArray;
@@ -266,6 +207,7 @@ Connection: Keep-Alive}
       end;
     end;
   end;
+
 
 var
   tmpDir, waptsetupPath, localVersion, requiredVersion, getVersion: ansistring;
@@ -392,6 +334,9 @@ begin
       Writeln('Wapt agent path: ' + waptsetupPath);
       writeln('Wget new waptagent ' + waptsetupurl);
       wget(waptsetupurl, waptsetupPath,Nil,Nil,True,False);
+
+      if hashString='' then
+        hashString:=StrSplit(FileToString(WaptBaseDir+'\waptagent.sha256'),' ')[0];
 
       if (HashString <> '')then
       begin
