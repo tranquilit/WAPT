f060665dbaa425447ad2bbac5f1dd2d4a425e4e8
htouvet <htouvet-github@tranquil.it>
2015-11-24 15:12:44 +0100

    small typo

--- waptdeploy/uwaptdeployres.pas
+++ waptdeploy/uwaptdeployres.pas
@@ -17,8 +17,8 @@ resourcestring
   rsUsage1 = 'Usage : waptdeploy.exe --hash=<sha256hash> --minversion=<min_wapt_version>';
   rsUsage2 = '  Download waptagent.exe from WAPT repository and launch it if local version is obsolete (< %s or < parameter 1 or < --minversion parameter)';
   rsUsage3 = ' --force : install waptagent.exe even if version doesn''t match';
-  rsUsage4 = ' --minversion=1.2.3 : install waptagent.exe even if installed version is less than that';
-  rsUsage5 = ' --repo_url=http://wapt/wapt : location of repo where to lookup waptdeploy.version and to get waptagent.exe';
+  rsUsage4 = ' --minversion=1.2.3 : install waptagent.exe if installed version is less than that';
+  rsUsage5 = ' --repo_url=http://wapt/wapt : location of repository where to lookup waptdeploy.version and to get waptagent.exe';
   rsUsage6 = ' --waptsetupurl=http://wapt/wapt/waptagent.exe : location where to download setup exe. (default=<repo_url>/waptagent.exe';
   rsUsage7 = ' --tasks=autorunTray,installService,installredist2008,autoUpgradePolicy  : if given, pass this arguments to the /TASKS options of the waptagent installer. Default = installService,installredist2008,autoUpgradePolicy';
   rsUsage8 = ' --hash=<sha256hash> : check that downloaded waptagent.exe setup sha256 hash match this parameter.';
