848bf795bccd155bc9df68ff9ac22f680246af61
htouvet <htouvet-github@tranquil.it>
2015-11-25 12:49:32 +0100

    rotate waptservice log with nssm

--- waptservice/waptservice.py
+++ waptservice/waptservice.py
@@ -2209,7 +2209,10 @@ def install_service():
         "DisplayName" : "sz:WAPTService",
         "AppStdout" : r"expand_sz:{}".format(os.path.join(waptconfig.log_directory,'waptservice.log')),
         "Parameters\\AppStderr" : r"expand_sz:{}".format(os.path.join(waptconfig.log_directory,'waptservice.log')),
+        "Parameters\\AppStdout" : r"expand_sz:{}".format(os.path.join(waptconfig.log_directory,'waptservice.log')),
         "Parameters\\AppParameters" : r'expand_sz:"{}"'.format(os.path.abspath(__file__)),
+        "Parameters\\AppRotateFiles": 1,
+        "Parameters\\AppRotateBytes": 10*1024*1024,
         }
 
     root = setuphelpers.HKEY_LOCAL_MACHINE
