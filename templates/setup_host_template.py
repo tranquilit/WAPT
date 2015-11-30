6c6acf49946ba590bd09ba5d5ee85d88b4ae492a
htouvet <htouvet-github@tranquil.it>
2015-11-30 16:51:04 +0100

    remove host description rename in host package template..

--- templates/setup_host_template.py
+++ templates/setup_host_template.py
@@ -5,6 +5,4 @@ import sys
 uninstallkey = []
 
 def install():
-    if control.description:
-        print('Change local computer description to match package description')
-        print run_notfatal('echo "" | WMIC os set description="'+control.description.splitlines()[0].encode(sys.getfilesystemencoding())+'"')
+    pass
