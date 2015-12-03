c4db850bfd65b7528957871efb021cc6fd4fa11b
htouvet <htouvet-github@tranquil.it>
2015-12-03 17:48:39 +0100

    when a full waptagent.exe must be deployed with waptupgrade package, and waptexit is running, schedule the task for next system startup to avoid half corrupted install if system is forcibly shutted down.

--- waptupgrade/setup.py
+++ waptupgrade/setup.py
@@ -12,6 +12,69 @@ from win32com.taskscheduler import taskscheduler
 # registry key(s) where WAPT will find how to remove the application(s)
 uninstallkey = []
 
+import types,re
+class Version(object):
+    """Version object of form 0.0.0
+        can compare with respect to natural numbering and not alphabetical
+
+    >>> Version('0.10.2') > Version('0.2.5')
+    True
+    >>> Version('0.1.2') < Version('0.2.5')
+    True
+    >>> Version('0.1.2') == Version('0.1.2')
+    True
+    """
+
+    def __init__(self,version,members_count=None):
+        if version is None:
+            version = ''
+        assert isinstance(version,types.ModuleType) or isinstance(version,str) or isinstance(version,unicode) or isinstance(version,Version)
+        if isinstance(version,types.ModuleType):
+            self.versionstring = version.__version__
+        elif isinstance(version,Version):
+            self.versionstring = version.versionstring
+        else:
+            self.versionstring = version
+        self.members = [ v.strip() for v in self.versionstring.split('.')]
+        if members_count is not None:
+            if len(self.members)<members_count:
+                self.members.extend(['0'] * (members_count-len(self.members)))
+            else:
+                del self.members[members_count:]
+
+    def __cmp__(self,aversion):
+        def nat_cmp(a, b):
+            a = a or ''
+            b = b or ''
+
+            def convert(text):
+                if text.isdigit():
+                    return int(text)
+                else:
+                    return text.lower()
+
+            def alphanum_key(key):
+                return [convert(c) for c in re.split('([0-9]+)', key)]
+
+            return cmp(alphanum_key(a), alphanum_key(b))
+
+        if not isinstance(aversion,Version):
+            aversion = Version(aversion)
+        for i in range(0,min([len(self.members),len(aversion.members)])):
+            i1,i2  = self.members[i], aversion.members[i]
+            v = nat_cmp(i1,i2)
+            if v:
+                return v
+        return 0
+
+    def __str__(self):
+        return '.'.join(self.members)
+
+    def __repr__(self):
+        return "Version('{}')".format('.'.join(self.members))
+
+
+
 def update_sources():
     files = [
          'common.py',
@@ -236,10 +299,10 @@ def create_onetime_task(name,cmd,parameters, delay_minutes=2,max_runtime=10, ret
     return task
 
 
-def full_waptagent_install():
+def full_waptagent_install(at_startup=False):
     # get it from
-    waptagent_path = makepath(tempfile.tempdir,'waptagent.exe')
-    waptdeploy_path = makepath(tempfile.tempdir,'waptdeploy.exe')
+    waptagent_path = makepath(tempfile.gettempdir(),'waptagent.exe')
+    waptdeploy_path = makepath(tempfile.gettempdir(),'waptdeploy.exe')
     filecopyto(makepath('patchs','waptdeploy.exe'),waptdeploy_path)
 
     expected_sha256 = open('waptagent.sha256','r').read().splitlines()[0].split()[0]
@@ -248,22 +311,49 @@ def full_waptagent_install():
     if not isfile(waptagent_path) or sha256_for_file(waptagent_path) != expected_sha256:
         download_waptagent(waptagent_path,expected_sha256)
     #create_onetime_task('fullwaptupgrade',waptagent_path,'/VERYSILENT',delay_minutes=15)
-    # use embedded waptagent.exe, wait 15 minutes for other tasks to complete.
-    create_onetime_task('fullwaptupgrade',waptdeploy_path,'--hash=%s --waptsetupurl=%s --wait=15 --temporary'%(expected_sha256,waptagent_path),delay_minutes=1)
+
+    if at_startup or isrunning('waptexit.exe'):
+        cmd = "'%s' --hash=%s --waptsetupurl=%s --wait=15 --temporary" %(waptdeploy_path,expected_sha256,waptagent_path)
+        if not at_startup:
+            print('waptexit is running, scheduling a one time task at system startup with command %s'%cmd)
+        # task at system startup
+        try:
+            run('schtasks /Create /RU SYSTEM /SC ONSTART /TN waptupgradetmp /TR "%s" /F /V1 /Z' % cmd)
+        except:
+            # windows xp doesn't support one time startup task
+            run('schtasks /Create /RU SYSTEM /SC ONSTART /TN waptupgradetmp /TR "%s" /F' % cmd)
+    else:
+        # use embedded waptagent.exe, wait 15 minutes for other tasks to complete.
+        create_onetime_task('fullwaptupgrade',waptdeploy_path,'--hash=%s --waptsetupurl=%s --wait=15 --temporary'%(expected_sha256,waptagent_path),delay_minutes=1)
 
 
 def install():
     # if you want to modify the keys depending on environment (win32/win64... params..)
     import common
+    if installed_softwares(uninstallkey='WAPT Server_is1'):
+        error('Wapt server installed on this host. Aborting')
+
     installed_wapt = installed_softwares(uninstallkey='WAPT_is1')
     if installed_wapt:
-        wapt_version = Version(installed_wapt[0]['version'].replace('WAPT ',''))
+        installed_wapt_version = installed_wapt[0]['version'].replace('WAPT ','')
+        if '-' in installed_wapt_version:
+            (wapt_version,installed_packaging) = wapt_version.split('-')
+            installed_packaging = int(installed_packaging)
+        else:
+            installed_packaging = 0
     else:
-        error('WAPT current version can not be found in registry (key is "WAPT_is1"), aborting...')
-    if wapt_version > Version(control.version):
-        print('Your current wapt (%s) is more recent than the upgrade package (%s). Skipping...'%(wapt_version,control.version))
-    elif wapt_version.members[0:2] < Version(control.version).members[0:2]:
-        print('Your current wapt version (%s) is too old to be upgraded, a full waptagent.exe install will be planned for %s'%(wapt_version,time.ctime(time.time() + 1*60)))
+        print('WAPT current version can not be found in registry (key is "WAPT_is1") ...full upgrade')
+        installed_wapt_version = '0.0.0'
+        installed_packaging = 0
+
+    # get upgrade package informations
+    (package_wapt_version,package_packaging) = control.version.split('-')
+    package_packaging = int(package_packaging)
+
+    if Version(installed_wapt_version,4) > Version(package_wapt_version,4):
+        print('Your current wapt (%s) is more recent than the upgrade package (%s). Skipping...'%(installed_wapt_version,control.version))
+    elif Version(installed_wapt_version,4) < Version(package_wapt_version,4):
+        print('Your current wapt version (%s) is too old to be upgraded, a full waptagent.exe install will be planned for %s'%(installed_wapt_version,time.ctime(time.time() + 1*60)))
         full_waptagent_install()
     else:
         print(u'Partial upgrade of WAPT client')
