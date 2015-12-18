1d8a58fbc874c61a366313d5c08ffa4496575556
htouvet <htouvet-github@tranquil.it>
2015-12-18 08:43:34 +0100

    Remobe virtualtree from ScaleDPI

--- waptconsole/uscaledpi.pas
+++ waptconsole/uscaledpi.pas
@@ -15,7 +15,7 @@ function DoScaleY(Size: Integer; FromDPI: Integer): integer;
 
 implementation
 
-uses Classes, Graphics, VirtualTrees,sogrid,vte_json;
+uses Classes, Graphics, sogrid,vte_json;
 
 procedure HighDPI(FromDPI: Integer);
 var
@@ -179,42 +179,6 @@ begin
     end;
   end;
 
-
-  if Control is TVirtualDrawTree  then
-  begin
-    With Control as TVirtualDrawTree do
-    begin
-      DefaultNodeHeight := ScaleY(DefaultNodeHeight,FromDPI);
-      Header.MinHeight:=ScaleY(Header.MinHeight,FromDPI);;
-      Header.MaxHeight:=ScaleY(Header.MaxHeight,FromDPI);;
-      Header.Height:=ScaleY(Header.Height,FromDPI);;
-      Font.Height := 0;
-      for i := 0 to header.Columns.Count-1 do
-      begin
-        header.Columns[i].MinWidth:=ScaleX(header.Columns[i].MinWidth,FromDPI);
-        header.Columns[i].MaxWidth:=ScaleX(header.Columns[i].MaxWidth,FromDPI);
-        header.Columns[i].Width:=ScaleX(header.Columns[i].Width,FromDPI);
-      end;
-    end;
-  end;
-
-  if Control is TVirtualStringTree  then
-  begin
-    With Control as TVirtualStringTree do
-    begin
-      DefaultNodeHeight := ScaleY(DefaultNodeHeight,FromDPI);
-      Header.MinHeight:=ScaleY(Header.MinHeight,FromDPI);;
-      Header.MaxHeight:=ScaleY(Header.MaxHeight,FromDPI);;
-      Header.Height:=ScaleY(Header.Height,FromDPI);;
-      Font.Height := 0;
-      for i := 0 to header.Columns.Count-1 do
-      begin
-        header.Columns[i].MinWidth:=ScaleX(header.Columns[i].MinWidth,FromDPI);
-        header.Columns[i].MaxWidth:=ScaleX(header.Columns[i].MaxWidth,FromDPI);
-        header.Columns[i].Width:=ScaleX(header.Columns[i].Width,FromDPI);
-      end;
-    end;
-  end;
 
   if Control is TWinControl then begin
     WinControl:=TWinControl(Control);
