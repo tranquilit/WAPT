46a3f65c5ba7d08497a6cf62dd23fdfd80217ff0
htouvet <htouvet-github@tranquil.it>
2016-11-23 16:40:10 +0100

    avoid critical message when trying to map sid to user

--- setuphelpers.py
+++ setuphelpers.py
@@ -2605,7 +2605,7 @@ def get_user_from_sid(sid,controller=None):
         name, domain, type = win32security.LookupAccountSid (controller, pysid)
         return "%s\\%s" % (domain,name)
     except win32security.error as e:
-        logger.critical(u'Unable to GET username from SID %s : %s' % ("%s" % sid,e))
+        logger.debug(u'Unable to GET username from SID %s : %s, using profile directory instead' % ("%s" % sid,e))
         # try from directory
         return get_user_from_profpath(sid)
 
