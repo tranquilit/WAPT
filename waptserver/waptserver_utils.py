8480d4e5d54eef365bd259a08528948ca8d03a75
htouvet <htouvet-github@tranquil.it>
2016-12-19 17:11:59 +0100

    ditto

--- waptserver/waptserver_utils.py
+++ waptserver/waptserver_utils.py
@@ -86,7 +86,7 @@ def mkdir_p(path):
     import errno
     try:
         os.makedirs(path)
-    except OSError, exc:
+    except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
