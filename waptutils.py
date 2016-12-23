b4e95940fc010f2aa55c3862f70bf2360cfa86d7
htouvet <htouvet-github@tranquil.it>
2016-12-23 17:58:46 +0100

    try filesystemencoding decode before replacing chars in ensure_unicode

--- waptutils.py
+++ waptutils.py
@@ -320,9 +320,11 @@ def ensure_unicode(data):
     u'Exception: '
     """
     try:
-        if type(data) is types.UnicodeType:
+        if isinstance(data,types.UnicodeType):
             return data
-        if type(data) is types.StringType:
+        if isinstance(data,str):
+            return data.decode(sys.getfilesystemencoding(),'replace')
+        if isinstance(data,types.StringType):
             return unicode(data, 'utf8', 'replace')
         if isinstance(data,WindowsError):
             return u"%s : %s" % (data.args[0], data.args[1].decode(sys.getfilesystemencoding(),'replace'))
