a43ba40c788a32ec5e14277d2416af724c87f190
htouvet <htouvet-github@tranquil.it>
2017-02-09 19:48:16 +0100

    relative init of workdir removed virtualenv activate

--- init_workdir.bat
+++ init_workdir.bat
@@ -1,15 +1,13 @@
 
-virtualenv .
+virtualenv . --distribute
 xcopy /I /E /F /Y c:\python27\libs libs
 xcopy /I /E /F /Y c:\python27\DLLs DLLs
 xcopy /I /E /F /Y /EXCLUDE:libexcludes.txt c:\python27\lib lib
 
 virtualenv --relocatable .
-Scripts\activate.bat
-pip install --upgrade pip setuptools wheel virtualenv
-rem wget "https://downloads.sourceforge.net/project/pywin32/pywin32/Build%20220/pywin32-220.win32-py2.7.exe?r=&ts=1486553375&use_mirror=kent" --no-check-certificate
+Scripts\pip install --upgrade pip setuptools wheel virtualenv
 Scripts\easy_install.exe c:\binaries\pywin32-220.win32-py2.7.exe
-pip install -r requirements.txt
+Scripts\pip install -r requirements.txt
 
 copy /Y lib\site-packages\pywin32-220-py2.7-win32.egg\py*.dll .
 copy /Y c:\windows\SysWOW64\python27.dll .
