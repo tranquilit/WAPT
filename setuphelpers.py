3d854ee251a06f7ca1813d24f1b998fced7ff60c
htouvet <htouvet-github@tranquil.it>
2017-02-17 15:09:16 +0100

    1.3.9.2

--- setuphelpers.py
+++ setuphelpers.py
@@ -20,7 +20,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "1.3.9.0"
+__version__ = "1.3.9.2"
 
 __all__ = \
 ['CalledProcessError',
