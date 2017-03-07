e78433a7f4d01538a6e058eeab8eb7d64cb9ba43
htouvet <htouvet-github@tranquil.it>
2017-03-07 10:48:05 +0100

    change waptservice version

--- waptservice/waptservice.py
+++ waptservice/waptservice.py
@@ -19,7 +19,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "1.3.11"
+__version__ = "1.4.0"
 
 import time
 import sys
