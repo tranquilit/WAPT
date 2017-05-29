32f04eae01c8b379f505dd96e6327af2efd8b3a9
htouvet <htouvet-github@tranquil.it>
2017-05-29 12:02:10 +0200

    ditto

--- waptserver/rpm/build.sh
+++ waptserver/rpm/build.sh
@@ -5,4 +5,4 @@ set -ex
 mkdir -p BUILD RPMS
 rpmbuild -bb --buildroot $PWD/builddir -v --clean waptserver.spec
 rm -f tis-waptserver.rpm
-cp RPMS/*/tis-waptserver*.rpm .
+cp RPMS/*/tis-waptserver*.rpm tis-waptserver.rpm
