c6114ffb0b03dd989ec5c580b811919e7b867da0
htouvet <htouvet-github@tranquil.it>
2017-06-01 17:43:46 +0200

    Moved check_package_signature and unzip_package, list_corrupted_files  from Wapt.common to PackageEntry Don't allow to sign a Package awith a setup.py file with a setup.py file if no coe signing certificate can be found for the private key. -> added forbidden_files parameter to build_manifest -> sign_package raise an exception in this case

--- wapt-get.py
+++ wapt-get.py
@@ -20,7 +20,7 @@
 #    along with WAPT.  If not, see <http://www.gnu.org/licenses/>.
 #
 # -----------------------------------------------------------------------
-__version__ = "1.5.0.3"
+__version__ = "1.5.0.5"
 
 import codecs
 import getpass
