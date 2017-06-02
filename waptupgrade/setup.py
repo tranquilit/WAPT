a7537157cc881f073d2171d4de982348c589756c
htouvet <htouvet-github@tranquil.it>
2017-06-02 19:08:02 +0200

    removed partial upgrade in waptupgrade package

--- waptupgrade/setup.py
+++ waptupgrade/setup.py
@@ -74,79 +74,12 @@ class Version(object):
     def __repr__(self):
         return "Version('{}')".format('.'.join(self.members))
 
-
-
-def update_sources():
-    files = [
-         'waptutils.py',
-         'waptcrypto.py',
-         'common.py',
-         'setuphelpers.py',
-         'wapt-get.exe',
-         'waptdeploy.exe',
-         'wapt-get.exe.manifest',
-         'wapt-get.py',
-         'waptdevutils.py',
-         'waptpackage.py',
-         'windnsquery.py',
-         'waptwua.py',
-         'wapttray.exe',
-         'waptexit.exe',
-         'custom_zip.py',
-         'keyfinder.py',
-         'COPYING.txt',
-         'templates',
-         'waptconsole.exe',
-         'waptconsole.exe.manifest',
-         'waptservice',
-         'languages',
-         'revision.txt',
-         'version',
-         'vc_redist/icacls.exe',
-         'templates/setup_package_template_exe.py',
-         'templates/setup_package_template_msi.py',
-         'templates/setup_package_template_msu.py',
-         'templates/setup_package_template_dir.py',
-    ]
-
-    def ignore(src,names):
-        result = []
-        for name in names:
-            for pattern in ['*.pyc','*.exe']:
-                if glob.fnmatch.fnmatch(name,pattern):
-                    result.append(name)
-        return result
-
-    checkout_dir = os.getcwd()
-    # cleanup patchs dir
-    if os.path.exists(os.path.join(checkout_dir,'patchs')):
-        shutil.rmtree(os.path.join(checkout_dir,'patchs'))
-
-    os.makedirs(os.path.join(checkout_dir,'patchs'))
-    for f in files:
-        fn = os.path.join(WAPT.wapt_base_dir,f)
-        target_fn = os.path.join(checkout_dir,'patchs',f)
-        if os.path.isfile(fn):
-            if not os.path.exists(os.path.dirname(target_fn)):
-                os.makedirs(os.path.dirname(target_fn))
-            filecopyto(fn,target_fn)
-        elif os.path.isdir(fn):
-            copytree2(
-                src=fn,
-                dst=target_fn,
-                onreplace = default_overwrite,
-                ignore=ignore)
-    return True
-
 def update_control(entry):
     """Update package control file before build-upload"""
-    if update_sources():
-        waptget = get_file_properties(makepath('patchs','wapt-get.exe'))
-        rev = open(makepath('patchs','version')).read().strip()
-        entry.package = '%s-waptupgrade' % WAPT.config.get('global','default_package_prefix')
-        entry.version = '%s-%s' % (waptget['FileVersion'],rev)
-    else:
-        print(u'Keeping current control data %s (%s)'%(control.package,control.version))
+    waptget = get_file_properties(makepath('patchs','wapt-get.exe'))
+    rev = open(makepath('patchs','version')).read().strip()
+    entry.package = '%s-waptupgrade' % WAPT.config.get('global','default_package_prefix')
+    entry.version = '%s-%s' % (waptget['FileVersion'],rev)
 
 def update_registry_version(version):
     # updatethe registry
@@ -155,76 +88,6 @@ def update_registry_version(version):
         reg_setvalue(waptis,"DisplayName","WAPT %s" % version)
         reg_setvalue(waptis,"DisplayVersion","%s" % version)
         reg_setvalue(waptis,"InstallDate",currentdate())
-
-
-
-def copytree2(src, dst, ignore=None,onreplace=default_skip,oncopy=default_oncopy,enable_replace_at_reboot=True,onerror=None):
-    """Copy src directory to dst directory. dst is created if it doesn't exists
-        src can be relative to installation temporary dir
-        oncopy is called for each file copy. if False is returned, copy is skipped
-        onreplace is called when a file will be overwritten.
-    """
-    logger.debug('Copy tree from "%s" to "%s"' % (ensure_unicode(src),ensure_unicode(dst)))
-    # path relative to temp directory...
-    tempdir = os.getcwd()
-    if not os.path.isdir(src) and os.path.isdir(os.path.join(tempdir,src)):
-        src = os.path.join(tempdir,src)
-
-    names = os.listdir(src)
-    if callable(ignore) and ignore is not None:
-        ignored_names = ignore(src, names)
-    else:
-        ignored_names = set()
-
-    if not os.path.isdir(dst):
-        if oncopy('create directory',src,dst):
-            os.makedirs(dst)
-    errors = []
-    for name in names:
-        if name in ignored_names:
-            continue
-        srcname = os.path.join(src, name)
-        dstname = os.path.join(dst, name)
-        try:
-            if os.path.isdir(srcname):
-                if oncopy('directory',srcname,dstname):
-                    copytree2(srcname, dstname, ignore = ignore,onreplace=onreplace,oncopy=oncopy)
-            else:
-                if os.path.isfile(dstname):
-                    if onreplace(srcname,dstname) and oncopy('overwrites',srcname,dstname):
-                        os.unlink(dstname)
-                        shutil.copy2(srcname, dstname)
-                else:
-                    if oncopy('copy',srcname,dstname):
-                        shutil.copy2(srcname, dstname)
-
-        except (IOError, os.error) as why:
-            #print(u'IO Error copying from "%s" to "%s" : %s' % (ensure_unicode(src),ensure_unicode(dst),ensure_unicode(why)))
-            if onerror is not None and callable(onerror):
-                try:
-                    onerror(srcname,dstname,why)
-                except Exception as e:
-                    errors.append((srcname,dstname,ensure_unicode(e)))
-            else:
-                errors.append((srcname,dstname,ensure_unicode(why)))
-
-        # catch the Error from the recursive copytree so that we can
-        # continue with other files
-        except shutil.Error as err:
-            logger.critical(u'shutil Error copying from "%s" to "%s" : %s' % (ensure_unicode(srcname),ensure_unicode(dstname),ensure_unicode(err)))
-            errors.extend(err.args[0])
-    try:
-        shutil.copystat(src, dst)
-    except OSError, why:
-        if WindowsError is not None and isinstance(why, WindowsError):
-            # Copying file access times may fail on Windows
-            pass
-        else:
-            print(u'Error copying stats from "%s" to "%s" : %s' % (ensure_unicode(src),ensure_unicode(dst),ensure_unicode(why)))
-            errors.append((src, dst, str(why)))
-    if errors:
-        raise shutil.Error, errors
-
 
 
 def sha1_for_file(fname, block_size=2**20):
@@ -345,21 +208,6 @@ def install():
     if installed_softwares('WAPT Server_is1'):
         error('Wapt server installed on this host. Aborting')
 
-    """
-    installed_wapt = installed_softwares(uninstallkey='WAPT_is1')
-    if installed_wapt:
-        wapt_version = installed_wapt[0]['version'].replace('WAPT ','')
-        if '-' in wapt_version:
-            (installed_wapt_version,installed_packaging) = wapt_version.split('-')
-            installed_packaging = int(installed_packaging)
-        else:
-            installed_wapt_version = wapt_version
-            installed_packaging = 0
-    else:
-        print('WAPT current version can not be found in registry (key is "WAPT_is1") ...full upgrade')
-        installed_wapt_version = '0.0.0'
-        installed_packaging = 0
-    """
     status = WAPT.wapt_status()
     installed_wapt_version = status['wapt-exe-version']
 
@@ -369,59 +217,9 @@ def install():
 
     if Version(installed_wapt_version,3) > Version(package_wapt_version,3):
         print('Your current wapt (%s) is more recent than the upgrade package (%s). Skipping...'%(installed_wapt_version,control.version))
-    elif Version(installed_wapt_version,3) < Version(package_wapt_version,3):
-        print('Your current wapt version (%s) is too old to be upgraded, a full waptagent.exe install will be planned for %s'%(installed_wapt_version,time.ctime(time.time() + 1*60)))
-        full_waptagent_install(str(Version(package_wapt_version,4)))
     else:
-        print(u'Partial upgrade of WAPT client')
-        killalltasks('wapttray.exe')
-        killalltasks('waptconsole.exe')
-
-        def onerror(srcname,dstname,e):
-            print u"Error %s %s %s" %(srcname,dstname,ensure_unicode(e))
-            if e[0] == 5:   # locked
-                filecopyto(srcname,dstname+'.pending')
-                replace_at_next_reboot(None, dstname)
-            else:
-                raise e
-
-        def check_exe_version(src,dst):
-            if os.path.splitext(dst)[1] in ('.exe','.dll'):
-                try:
-                    ov = get_file_properties(dst)['FileVersion']
-                    nv = get_file_properties(src)['FileVersion']
-                    return Version(ov)<Version(nv)
-                except:
-                    return True
-            else:
-                return True
-
-        copytree2('patchs',WAPT.wapt_base_dir,
-            onreplace = check_exe_version,
-            onerror = onerror)
-
-        update_registry_version(control.version)
-
-        # restart of service can not be done by service...
-        if service_installed('waptservice') and service_is_running('waptservice'):
-            import requests,json
-            try:
-                res = json.loads(requests.get('http://127.0.0.1:8088/waptservicerestart.json').text)
-            except:
-                tmp_bat = tempfile.NamedTemporaryFile(prefix='waptrestart',suffix='.cmd',mode='wt',delete=False)
-                tmp_bat.write('net stop waptservice\n')
-                tmp_bat.write('net start waptservice\n')
-                tmp_bat.write('del "%s"\n'%tmp_bat.name)
-                tmp_bat.close()
-                create_onetime_task('waptrestart',tmp_bat.name)
-        # fix ACL on wapt folder : remove inheritance
-        run([makepath(WAPT.wapt_base_dir,'vc_redist','icacls.exe'),WAPT.wapt_base_dir,'/inheritance:r'])
-        print(u'Upgrade done')
-
-    #remove winshell in root waptdir
-    for fn in glob.glob(makepath(WAPT.wapt_base_dir,'winshell.py*')):
-        print(u'Removing old %s' % fn)
-        remove_file(fn)
+        print('Setting up upgrade from wapt version %s to %s. waptagent install planned for %s'%(installed_wapt_version,package_wapt_version,time.ctime(time.time() + 1*60)))
+        full_waptagent_install(str(Version(package_wapt_version,4)))
 
 if __name__ == '__main__':
     pass
