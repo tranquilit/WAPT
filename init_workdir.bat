9cd17109f030ca5c57001f61bfd57942fa77deb5
Denis Cardon <dcardon@tranquil.it>
2017-06-19 18:52:01 +0200

    add .exe fileextension for buildbot

--- init_workdir.bat
+++ init_workdir.bat
@@ -7,19 +7,19 @@ REM  ##########################################"
 set PYTHON_PATH=c:\python27
 
 git clean -xfd
-
-%PYTHON_PATH%\Scripts\pip install virtualenv
-%PYTHON_PATH%\Scripts\virtualenv . 
+%PYTHON_PATH%\Scripts\pip.exe install -U pip distribute 
+%PYTHON_PATH%\Scripts\pip.exe install virtualenv
+%PYTHON_PATH%\Scripts\virtualenv.exe . 
 xcopy /I /E /F /Y c:\python27\libs libs
 xcopy /I /E /F /Y c:\python27\DLLs DLLs
 xcopy /I /E /F /Y /EXCLUDE:libexcludes.txt c:\python27\lib lib
 
-Scripts\virtualenv --relocatable .
-Scripts\pip install --upgrade pip setuptools wheel virtualenv six
+Scripts\virtualenv.exe --relocatable .
+Scripts\pip.exe install --upgrade pip distribute wheel virtualenv six
 REM  pywin32 is not available as binary wheel from standard pip download, it is 
 REM  currently integrated into the git tree and installed directly from that file
 Scripts\easy_install.exe utils\pywin32-220.win32-py2.7.exe
-Scripts\pip install --require-hashes  -r requirements.txt
+Scripts\pip.exe install --require-hashes  -r requirements.txt
 
 copy /Y lib\site-packages\pywin32-220-py2.7-win32.egg\py*.dll .
 copy /Y c:\windows\SysWOW64\python27.dll .
