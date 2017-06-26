12e914c730086d8a856886c150e16141ef13febd
Denis Cardon <dcardon@tranquil.it>
2017-06-26 11:10:23 +0200

    rpm build fix

--- waptserver/rpm/build.sh
+++ waptserver/rpm/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 set -ex
-
+rm -Rf BUILD RPMS BUILDROOT
 mkdir -p BUILD RPMS
 VERSION=$(python get_version.py ../waptserver.py)
 echo $VERSION
 rpmbuild -bb --define "_version $VERSION" --buildroot $PWD/builddir -v --clean waptserver.spec
-rm -f tis-waptserver.rpm
+rm -f tis-waptserver*.rpm
 cp RPMS/*/tis-waptserver*.rpm .
 # temporary addition for builbot
 ln -s tis-waptserver*.rpm tis-waptserver.rpm
