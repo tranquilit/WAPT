20bf6ad30c1749df9671e8a35439effb190013e7
Denis Cardon <dcardon@tranquil.it>
2017-07-03 10:21:07 +0200

    add pre-commit hooks with tortoisegit

--- waptsetup/create_setup.py
+++ waptsetup/create_setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 # Name:        create_setup.py
 # Purpose:
@@ -19,19 +20,18 @@ import getpass
 import glob
 import win32api
 
-"""
-pour le fichier autobuild.ini, la syntaxe est la suivante:
-[global]
-ssh_username = root
-ssh_private_key = c:\private\srvwapt_priv.key
-ssh_hostname = srvwapt
-ssh_log_file = c:\tranquilit\paramiko.log
-ksigntool_path=C:\Program Files\kSign\kSignCMD.exe
-private_key=c:\private\certificat.p12
-key_password=xxxxxxxxx
-ksign=C:\Program Files\kSign\kSignCMD.exe /f C:\private\certificat.p12 /p xxxxxxxx $p
-
-"""
+# """
+# pour le fichier autobuild.ini, la syntaxe est la suivante:
+# [global]
+##ssh_username = root
+# ssh_private_key = c:\private\srvwapt_priv.key
+##ssh_hostname = srvwapt
+# ssh_log_file = c:\tranquilit\paramiko.log
+# ksigntool_path=C:\Program Files\kSign\kSignCMD.exe
+# private_key=c:\private\certificat.p12
+# key_password=xxxxxxxxx
+# ksign=C:\Program Files\kSign\kSignCMD.exe /f C:\private\certificat.p12 /p xxxxxxxx $p
+# """
 
 
 def programfiles32():
@@ -42,15 +42,16 @@ def programfiles32():
         return os.environ['PROGRAMFILES']
 
 
-def ssl_server_trust_prompt( trust_dict ):
+def ssl_server_trust_prompt(trust_dict):
     return True, trust_dict['failures'], True
 
-def get_required_param(param_name,section='global'):
+
+def get_required_param(param_name, section='global'):
     global config
     if config.has_option(section, param_name):
         return config.get(section, param_name)
     else:
-        raise Exception ("missing parameter %s in section %s config file",(param_name,section))
+        raise Exception('missing parameter %s in section %s config file', (param_name, section))
 
 
 def update_waptupgrade(checkout_dir):
@@ -58,32 +59,31 @@ def update_waptupgrade(checkout_dir):
     sys.path.append('c:/wapt')
     from common import Wapt
     wapt = Wapt()
-    wapt.build_upload(os.path.join(checkout_dir,'waptupgrade'))
+    wapt.build_upload(os.path.join(checkout_dir, 'waptupgrade'))
 
 
-def sign_all_exe(ksigntool_path,private_key,key_password,checkout_dir):
-    for file_to_sign in glob.glob(checkout_dir + "\\*.exe"):
+def sign_all_exe(ksigntool_path, private_key, key_password, checkout_dir):
+    for file_to_sign in glob.glob(checkout_dir + '\\*.exe'):
         print file_to_sign
         cmd = """ "%(ksigntool_path)s" \
         /d "Binary part of Wapt-get software management suite" \
         /du http://dev.tranquil.it \
         /f %(private_key)s  \
         /p %(key_password)s \
-        "%(file_to_sign)s" """  % {
-                        'ksigntool_path':ksigntool_path,
-                        'private_key':private_key,
-                        'key_password':key_password,
-                        'file_to_sign':file_to_sign,
-                        }
+        "%(file_to_sign)s" """ % {
+            'ksigntool_path': ksigntool_path,
+            'private_key': private_key,
+            'key_password': key_password,
+            'file_to_sign': file_to_sign,
+        }
 
         print cmd
         print subprocess.check_output(cmd, shell=True)
 ######################
 
 
-config_file = "c:\\private\\autobuild.ini"
+config_file = 'c:\\private\\autobuild.ini'
 config = ConfigParser.RawConfigParser()
-
 
 
 if os.path.exists(config_file):
@@ -97,26 +97,25 @@ if config.has_section('global'):
     #ssh_hostname = get_required_param('ssh_hostname')
     #ssh_log_file = get_required_param('ssh_log_file')
 
-    if config.has_option('global','ksigntool_path'):
-        ksigntool_path =get_required_param('ksigntool_path')
+    if config.has_option('global', 'ksigntool_path'):
+        ksigntool_path = get_required_param('ksigntool_path')
     else:
-        ksigntool_path = os.path.join(programfiles32(),'ksign','kSignCMD.exe')
-
+        ksigntool_path = os.path.join(programfiles32(), 'ksign', 'kSignCMD.exe')
 
     private_key = get_required_param('private_key')
     private_key_p12 = get_required_param('private_key_p12')
 else:
-    raise Exception ('missing [global] section')
+    raise Exception('missing [global] section')
 
 
 key_password = getpass.getpass('Signing key password :')
 print '\n'
 
-if os.path.exists(ksigntool_path)==False:
-    print "FATAL: ksigntool_path %s does not exists" % ksigntool_path
+if os.path.exists(ksigntool_path) == False:
+    print 'FATAL: ksigntool_path %s does not exists' % ksigntool_path
     sys.exit(1)
-if os.path.exists(private_key)==False:
-    print "FATAL: private_key file %s does not exists" % private_key
+if os.path.exists(private_key) == False:
+    print 'FATAL: private_key file %s does not exists' % private_key
     sys.exit(1)
 
 # if there is space in the name, we have to protect it with quote
@@ -124,45 +123,45 @@ if os.path.exists(private_key)==False:
 if ' ' in ksigntool_path:
     ksigntool_path_protected = win32api.GetShortPathName(ksigntool_path)
 else:
-    ksigntool_path_protected=ksigntool_path
+    ksigntool_path_protected = ksigntool_path
 
 if ' ' in private_key:
-    private_key_protected= win32api.GetShortPathName(private_key_p12)
+    private_key_protected = win32api.GetShortPathName(private_key_p12)
 else:
     private_key_protected = private_key_p12
 
 #ksigntool_path_protected = "C:\\progra~2\\kSign\\kSignCMD.exe"
 ksign = """%(ksigntool_path)s /f %(private_key)s /p %(key_password)s $p""" % {
-            'ksigntool_path':ksigntool_path_protected,
-            'private_key':private_key_protected,
-            'key_password':key_password}
+    'ksigntool_path': ksigntool_path_protected,
+    'private_key': private_key_protected,
+    'key_password': key_password}
 
 print ksign
 
-###CHECKOUT
+# CHECKOUT
 checkout_dir = os.path.join('C:\\', 'tranquilit', 'wapt')
 os.chdir(checkout_dir)
-#print subprocess.check_output('git checkout -f --')
+# print subprocess.check_output('git checkout -f --')
 
-### .EXE Signature
-sign_all_exe(ksigntool_path,private_key_p12,key_password,checkout_dir)
+# .EXE Signature
+sign_all_exe(ksigntool_path, private_key_p12, key_password, checkout_dir)
 
-### update WAPT upgrade package
+# update WAPT upgrade package
 update_waptupgrade(checkout_dir)
 
-issc_binary = os.path.join(programfiles32(),'Inno Setup 5','ISCC.exe')
+issc_binary = os.path.join(programfiles32(), 'Inno Setup 5', 'ISCC.exe')
 
-for buildtype in ('waptserver','waptsetup','waptstarter'):
-    print "building %s.exe" % (buildtype,)
+for buildtype in ('waptserver', 'waptsetup', 'waptstarter'):
+    print 'building %s.exe' % (buildtype,)
 
-    issfile = os.path.join(checkout_dir,'waptsetup','%s.iss'%buildtype )
-    print "running waptsetup for iss file : %s " % issfile
+    issfile = os.path.join(checkout_dir, 'waptsetup', '%s.iss' % buildtype)
+    print 'running waptsetup for iss file : %s ' % issfile
     cmd = '"%(issc_binary)s" /s"kSign=%(ksign)s" /f%(buildtype)s %(issfile)s' % {
-                    'issc_binary':issc_binary,
-                    'ksign':ksign,
-                    'buildtype':buildtype,
-                    'issfile':issfile
-                    }
-    print "ligne de commande : %s " % cmd
+        'issc_binary': issc_binary,
+        'ksign': ksign,
+        'buildtype': buildtype,
+        'issfile': issfile
+    }
+    print 'ligne de commande : %s ' % cmd
     print subprocess.check_output(cmd)
-                            #,'' % (buildtype,),"%s"%issfile])
+    #,'' % (buildtype,),"%s"%issfile])
