12e3279f65824ae9150acffdcb9ba3fc0f9fdf4b
htouvet <htouvet-github@tranquil.it>
2017-09-26 10:33:28 +0200

    ditto

--- waptserver/rpm/build.sh
+++ waptserver/rpm/build.sh
@@ -4,8 +4,7 @@ set -ex
 rm -Rf BUILD RPMS BUILDROOT
 mkdir -p BUILD RPMS
 VERSION=$(python get_version.py ../waptserver.py)
-echo $VERSION
-rpmbuild -bb --define "_version $VERSION" --buildroot $PWD/builddir -v --clean waptserver.spec
+rpmbuild -bb --define "_version $VERSION" --buildroot $PWD/builddir -v --clean waptserver.spec 1>&2
 rm -f tis-waptserver*.rpm
 cp RPMS/*/tis-waptserver*.rpm .
 echo tis-waptserver*.rpm
