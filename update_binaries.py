6cf72d7a1290cadeacbeede557359818e5dab84b
htouvet <htouvet-github@tranquil.it>
2017-10-13 19:10:00 +0200

    get nssm and zmq binaries

--- update_binaries.py
+++ update_binaries.py
@@ -40,6 +40,19 @@ print('Python PATH: %s' % sys.path)
 from setuphelpers import *
 import tempfile
 
+print('Get and unzip nssm')
+nssm_zip = wget('https://nssm.cc/ci/nssm-2.24-101-g897c7ad.zip',resume=True,md5='63175d3830b8a5cfd254353c4f561e5c')
+nssm_files = unzip(nssm_zip,filenames=['*/win*/nssm.exe'])
+for f in nssm_files:
+    new_name = makepath(wapt_base_dir,'waptservice',* f.split(os.path.sep)[-2:])
+    if not os.path.isdir(os.path.dirname(new_name)):
+        os.makedirs(os.path.dirname(new_name))
+    if os.path.isfile(new_name):
+        os.unlink(new_name)
+    os.renames(f,new_name)
+print nssm_files
+
+
 print('Get Postgresql zip')
 pgsql_zip = wget('https://get.enterprisedb.com/postgresql/postgresql-9.4.14-1-windows-x64-binaries.zip',resume=True,md5='b69d2e6135a0061dc5ecd968f3d0a31e')
 if os.path.isdir(makepath(wapt_base_dir,'waptserver','pgsql')):
@@ -51,7 +64,7 @@ nginx_zip = wget('https://nginx.org/download/nginx-1.13.5.zip',resume=True,md5='
 if os.path.isdir(makepath(wapt_base_dir,'waptserver','nginx')):
     shutil.rmtree(makepath(wapt_base_dir,'waptserver','nginx'))
 nginx_files = unzip(nginx_zip,target=makepath(wapt_base_dir,'waptserver'))
-os.rename(makepath(wapt_base_dir,'waptserver','nginx-1.13.5'),makepath(wapt_base_dir,'waptserver','nginx'))
+os.renames(makepath(wapt_base_dir,'waptserver','nginx-1.13.5'),makepath(wapt_base_dir,'waptserver','nginx'))
 
 print('Get innosetup compiler setup and extract files to waptsetup')
 innosetup_install = wget('http://www.jrsoftware.org/download.php/is-unicode.exe',resume=True,md5='42b9c2fcfdd96b79aeef49029ce776d4')
@@ -65,6 +78,16 @@ iscfiles_path = makepath(os.path.dirname(innosetup_install),'iscc','app')
 for fn in ['Default.isl', 'isbunzip.dll', 'isbzip.dll', 'ISCC.exe', 'ISCmplr.dll', 'islzma.dll', 'islzma32.exe', 'islzma64.exe', 'ISPP.dll', 'ISPPBuiltins.iss', 'isscint.dll', 'isunzlib.dll', 'iszlib.dll', 'license.txt', 'Setup.e32', 'SetupLdr.e32', 'WizModernImage-IS.bmp', 'WizModernImage.bmp', 'WizModernSmallImage-IS.bmp', 'WizModernSmallImage.bmp']:
     filecopyto(makepath(iscfiles_path,fn),makepath(wapt_base_dir,'waptsetup','innosetup'))
 
+
+print('Get and unzip libzmq.dll')
+p7zip = makepath(programfiles,'7-Zip','7z.exe')
+zmq_exe = wget('http://miru.hk/archive/ZeroMQ-4.0.4~miru1.0-x86.exe',resume=True,md5='699b63085408cd7bfcde5d3d62077f4e')
+run([p7zip,'e',zmq_exe,'*/libzmq-v90-mt-4_0_4.dll','-o'+wapt_base_dir])
+os.renames(makepath(wapt_base_dir,'libzmq-v90-mt-4_0_4.dll'),makepath(wapt_base_dir,'libzmq.dll'))
+
+print('Get DMIDecode')
+
+
 print('Get OpenSSL binaries from Fulgan')
 ssl_zip = wget('https://indy.fulgan.com/SSL/openssl-1.0.2l-i386-win32.zip',resume=True,md5='f1901d936f73d57a9efcef9b028e1621')
 ssl_file = unzip(ssl_zip,target=makepath(wapt_base_dir),filenames=['ssleay32.dll','openssl.exe','libeay32.dll'])
@@ -74,4 +97,5 @@ print('Python ldap wheel windows')
 python_ldap = wget('https://pypi.python.org/packages/55/8b/7e9b4f4f5c3b4c98416b10ba02f682e8e23d34c20fe8e56b9d09f4667e02/python_ldap-2.4.44-cp27-cp27m-win32.whl',resume=True,md5='21db70f804fe06d941a2e36f907358cf')
 print('Install ldap wheel')
 print(run(['pip','install',python_ldap,'--target',site_packages,'--upgrade']))
+
 
