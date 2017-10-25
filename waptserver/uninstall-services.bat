e8d6fbd6441219e88b0e6de8dd0bbd7eb4512e31
Denis Cardon <dcardon@tranquil.it>
2017-10-25 19:25:55 +0200

    fix service name for uninstall

--- waptserver/uninstall-services.bat
+++ waptserver/uninstall-services.bat
@@ -1,6 +1,6 @@
 net stop waptserver
 sc delete waptserver
-net stop nginx /yes
-sc delete nginx
-net stop pgsql /yes
-sc delete pgsql
+net stop waptnginx /yes
+sc delete waptnginx
+net stop waptpostgresql /yes
+sc delete waptpostgresql
