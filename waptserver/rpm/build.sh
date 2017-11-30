741d6e520afd7e253850b032252302c8c3a53847
Denis Cardon <dcardon@tranquil.it>
2017-11-30 16:34:52 +0100

    rpm build chain : change directory cleanup code for performance

--- waptserver/rpm/build.sh
+++ waptserver/rpm/build.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 set -ex
-rm -Rf BUILD RPMS BUILDROOT
+if [ -d BUILD ]; then
+    echo "removing BUILD directory"
+    find BUILD -delete 
+fi
+if [ -d RPMS ]; then
+    echo "remove RPMS build directory"
+    find RPMS -delete
+fi
+if [ -d BUILDROOT ]; then
+    echo "remove BUILDROOT build directory"
+    find BUILDROOT -delete
+fi 
+
 mkdir -p BUILD RPMS
 VERSION=$(python get_version.py ../waptserver_config.py)
 rpmbuild -bb --define "_version $VERSION" --buildroot $PWD/builddir -v --clean waptserver.spec 1>&2
