19f17f6d6557f4570133160fa9188ad1d3898e01
htouvet <htouvet-github@tranquil.it>
2018-02-07 13:20:02 +0100

    batch file to start pyscripter without system-wide python27 installation

--- devwapt.bat
+++ devwapt.bat
@@ -1,3 +1,4 @@
 SET VIRTUAL_ENV=%cd%
 SET PYTHONHOME=%VIRTUAL_ENV%
-START wapt.psproj
+start "Wapt Pycripter" /D "%cd%" "C:\Program Files (x86)\PyScripter\PyScripter.exe" -N --python27 --PYTHONDLLPATH "%cd%"  --project "%cd%\wapt.psproj"'
+
