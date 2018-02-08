834a6068fd2bf2474ea5b93f4cd942170345620f
htouvet <htouvet-github@tranquil.it>
2018-02-08 14:59:51 +0100

    small utility script to launch pyscripter in wapt dev environment.

---
+++ pyscripter.bat
@@ -0,0 +1,7 @@
+pushd %~dp0
+mklink %CD%\python.exe %CD%\Scripts\python.exe
+set VIRTUAL_ENV=%CD%
+SET PYTHONHOME=%VIRTUAL_ENV%
+start "Pycripter" /D "%CD%" "C:\Program Files (x86)\PyScripter\PyScripter.exe" -N --python27 --PYTHONDLLPATH "%cd%"  %1'
+popd
+pause
