15eebb1624e927b576e44b2160adf36eb517fd0c
htouvet <htouvet-github@tranquil.it>
2018-02-22 19:02:24 +0100

    fix debug script runwaptserver.sh

--- runwaptserver.sh
+++ runwaptserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo -u wapt PYTHONPATH=/opt/wapt PYTHONHOME=/opt/wapt /opt/wapt/waptserver/waptserver.py $@
+sudo -u wapt PYTHONPATH=/opt/wapt PYTHONHOME=/opt/wapt /opt/wapt/bin/python /opt/wapt/waptserver/waptserver.py $@
 
