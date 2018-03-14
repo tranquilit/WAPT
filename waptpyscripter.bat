229ee43608b7b562f0dd5983f96e961c3e655d9b
htouvet <htouvet-github@tranquil.it>
2018-03-14 16:46:15 +0100

    small typo

--- waptpyscripter.bat
+++ waptpyscripter.bat
@@ -2,5 +2,5 @@ pushd %~dp0
 mklink "%CD%\python.exe" "%CD%\Scripts\python.exe"
 set VIRTUAL_ENV=%CD%
 SET PYTHONHOME=%VIRTUAL_ENV%
-start "Pycripter" /D "%CD%" "%ProgramFiles(x86)%\PyScripter\PyScripter.exe" -N --python27 --PYTHONDLLPATH "%cd%" %*
+start "PyScripter" /D "%CD%" "%ProgramFiles(x86)%\PyScripter\PyScripter.exe" -N --python27 --PYTHONDLLPATH "%cd%" %*
 popd
