59608a49cd2c36cf4f028653a8914b59175678d5
htouvet <htouvet-github@tranquil.it>
2018-03-19 17:58:16 +0100

    add sign to create_setup_simple.py

--- waptsetup/create_setup_simple.py
+++ waptsetup/create_setup_simple.py
@@ -19,12 +19,30 @@ __doc__ = """\
 Build installer
 """
 
+def sign_exe(exe_path,p12path,p12password):
+    KSIGN = os.path.join(setuphelpers.programfiles32,'kSign','kSignCMD.exe')
+
+    for attempt in [1, 2, 3]:
+        try:
+            print "Signing attempt #" + str(attempt)
+            setuphelpers.run(r'"%s" /f "%s" /p"%s" "%s"' % (KSIGN,p12path,p12password,exe_path),return_stderr=False)
+            break
+        except subprocess.CalledProcessError as cpe:
+            cpe.cmd =  cpe.cmd.replace(p12password, '********')
+            cpe.output = cpe.output.replace(p12password, '********')
+            print "Got CalledProcessError from subprocess.check_output: %s" % str(cpe)
+        except Exception as e:
+            print "Got an exception from subprocess.check_output"
+            raise
+
+
 def main():
     parser=OptionParser(usage=__doc__)
     parser.add_option("-l","--iscc-binary", dest="iscc_binary", default=os.path.join(os.path.dirname(__file__),'innosetup','ISCC.exe'), help="Path to ISCC compiler (default: %default)")
     parser.add_option("-v","--wapt-version", dest="waptversion", default=waptutils.__version__, help="Wapt edition to build (community, enterprise...).  (default: %default)")
     parser.add_option("-e","--wapt-edition", dest="waptedition", default='community', help="Wapt edition to build (community, enterprise...).  (default: %default)")
-    parser.add_option("-c","--compress", action='store_true', type='boolean', dest="compress", default=True, help="Compress with UPX.  (default: %default)")
+    parser.add_option("-k","--sign-key", dest="sign_key_path", help="Sign with this  key.  (default: %default)")
+    parser.add_option("-w","--sign-key-pwd-path", dest="sign_key_pwd_path", help="Path to password file. (default: %default)")
     (options,args) = parser.parse_args()
 
     if len(args) != 1:
@@ -50,6 +68,9 @@ def main():
             'waptedition':options.waptedition.lower()
             }
         subprocess.check_call(cmd)
+
+        if options.sign_key_path:
+            sign_exe(exe_fn,options.sign_key_path,open(options.sign_key_pwd_path,'rb').read())
 
 
 if __name__ == "__main__":
