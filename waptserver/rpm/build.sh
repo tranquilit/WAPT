f455a56ea2e3c67a0ac1ed6e451383d8504c6cb4
htouvet <htouvet-github@tranquil.it>
2018-03-20 12:09:15 +0100

    Add enterprise in package name for rpm

--- waptserver/rpm/build.sh
+++ waptserver/rpm/build.sh
@@ -16,8 +16,15 @@ fi
 
 mkdir -p BUILD RPMS
 rm -rf $PWD/builddir
+
+if [ "$WAPTEDITION" = "enterprise" ]; then
+    LICENCE="Commercial license"
+else
+    LICENCE="GPL"
+fi
+
 VERSION=$(python get_version.py ../waptserver_config.py)
-QA_SKIP_BUILD_ROOT=1 rpmbuild -bb --define "_version $VERSION" --buildroot $PWD/builddir -v waptserver.spec 1>&2
+QA_SKIP_BUILD_ROOT=1 rpmbuild -bb --define "_version $VERSION"  --define "waptedition $WAPTEDITION"  --define "licence $LICENCE" --buildroot $PWD/builddir -v waptserver.spec 1>&2
 #rm -rf $PWD/builddir
 rm -f tis-waptserver*.rpm
 cp RPMS/*/tis-waptserver*.rpm .
