a71f6f8bb3a1e5349e5970725316249ea22e795e
htouvet <htouvet-github@tranquil.it>
2018-07-03 20:50:51 +0200

    some dev build scripts

--- build_deb.sh
+++ build_deb.sh
@@ -5,8 +5,32 @@ git pull
 git submodule update
 git reset --hard
 git clean -fx -d
+
+export REV=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
+
 export WAPTEDITION=community
+
 cd waptserver/deb && fakeroot python createdeb.py
-export REV=git --
 rsync -aP *.deb  buildbot:/home/tisadmin/public_html/wapt-1.5.1.24/$WAPTEDITION/
-cd ..
+cd ../../waptsetup/deb/
+wget http://buildbot.ad.tranquil.it/~tisadmin/wapt-1.5.1.24/$WAPTEDITION/waptdeploy.exe -O waptdeploy.exe
+wget http://buildbot.ad.tranquil.it/~tisadmin/wapt-1.5.1.24/$WAPTEDITION/waptsetup.exe -O waptsetup-tis.exe
+fakeroot python createdeb.py
+rsync -aP *.deb  buildbot:/home/tisadmin/public_html/wapt-1.5.1.24/$WAPTEDITION/
+
+cd /home/tisadmin/tmp/buildbot/basedir/wapt-build-debian-packages/build || cd /home/tisadmin/tmp/buildbot/basedir/wapt-build-debian9-packages/build
+git fetch
+git pull
+git submodule update
+git reset --hard
+git clean -fx -d
+
+export WAPTEDITION=enterprise
+cd waptserver/deb && fakeroot python createdeb.py
+rsync -aP *.deb  buildbot:/home/tisadmin/public_html/wapt-1.5.1.24/$WAPTEDITION/
+cd ../../waptsetup/deb/
+wget http://buildbot.ad.tranquil.it/~tisadmin/wapt-1.5.1.24/$WAPTEDITION/waptdeploy.exe -O waptdeploy.exe
+wget http://buildbot.ad.tranquil.it/~tisadmin/wapt-1.5.1.24/$WAPTEDITION/waptsetup.exe -O waptsetup-tis.exe
+fakeroot python createdeb.py
+rsync -aP *.deb  buildbot:/home/tisadmin/public_html/wapt-1.5.1.24/$WAPTEDITION/
+
