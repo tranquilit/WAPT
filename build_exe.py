a982d694df115715da9831e2d58d8ca340d5bb3a
htouvet <htouvet-github@tranquil.it>
2018-07-03 17:37:51 +0200

    build_exe for bothe community and enterprise

--- build_exe.py
+++ build_exe.py
@@ -1,16 +1,32 @@
 # -*- coding: UTF-8 -*-
 from setuphelpers import *
+import sys
 import os
+
 
 LAZBUILD = r'waptpython.exe lazbuild.py -p C:\\typhon32-config -k C:\Users\buildbot\Documents\tranquilit2.p12 -w C:\Users\buildbot\Documents\tmpkeypwd'
 ISCCBUILD = r'waptpython.exe waptsetup\create_setup_simple.py -k C:\Users\buildbot\Documents\tranquilit2.p12 -w C:\Users\buildbot\Documents\tmpkeypwd'
 
-for lpi in ["wapt-get\\waptget.lpi","wapt-get\\waptguihelper.lpi","waptdeploy\\waptdeploy.lpi","wapttray\\wapttray.lpi","waptconsole\\waptconsole.lpi","waptexit\\waptexit.lpi","waptserver\\postconf\\waptserverpostconf.lpi"]:
-    if not isfile(os.path.split(lpi)[1]+'.exe'):
-        print(lpi)
-        print(run(LAZBUILD+ " -e enterprise "+ lpi ))
+def compile_exes(edition='enterprise',force=False):
+    for lpi in ["wapt-get\\waptget.lpi","wapt-get\\waptguihelper.lpi","waptdeploy\\waptdeploy.lpi","wapttray\\wapttray.lpi","waptconsole\\waptconsole.lpi","waptexit\\waptexit.lpi","waptserver\\postconf\\waptserverpostconf.lpi"]:
+        if force or not isfile(os.path.split(lpi)[1]+'.exe'):
+            print('Compiling %s...' % lpi)
+            print(run(LAZBUILD+ r' -e {edition} {lpi}'.format(**locals())))
 
-run(ISCCBUILD +r" -e enterprise --sign-exe-filenames=waptservice\win32\nssm.exe,waptservice\win64\nssm.exe waptsetup\waptstarter")
-run(ISCCBUILD +r" -e enterprise --sign-exe-filenames=waptservice\win32\nssm.exe,waptservice\win64\nssm.exe waptsetup\waptsetup")
-run(ISCCBUILD +r" -e enterprise --sign-exe-filenames=waptservice\win32\nssm.exe,waptservice\win64\nssm.exe waptsetup\waptserversetup")
+def compile_setups(edition='enterprise'):
+    for iscc in ('waptstarter','waptsetup','waptserversetup'):
+        run(ISCCBUILD + r" -e {edition} --sign-exe-filenames=waptservice\win32\nssm.exe,waptservice\win64\nssm.exe waptsetup\{iscc}".format(**locals()))
+
+os.chdir(os.path.dirname(__file__))
+if len(sys.argv) == 1:
+    editions = ['enterprise','community']
+else:
+    editions = sys.argv[1:]
+ 
+for edition in sys.argv[1:]:
+    print('Edition: %s' % edition)
+    compile_exes(edition)
+    compile_setups(edition)
+
+
 
