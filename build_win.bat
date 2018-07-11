baf99fa8084e2cf46fa2598359589828c724e235
htouvet <htouvet-github@tranquil.it>
2018-07-11 18:17:39 +0200

    standalone temp build scripts for 1.5.1.25

--- build_win.bat
+++ build_win.bat
@@ -1,7 +1,7 @@
 REM build_win
-init_workdir.bat
+call init_workdir.bat
 waptpython build_exe.py community
-"c:\Program Files\PuTTY\pscp.exe" *.exe waptsetup\*.exe tisadmin@buildbot:/home/tisadmin/public_html/wapt-1.5.1.24/community/
+"c:\Program Files\PuTTY\pscp.exe" *.exe waptsetup\*.exe tisadmin@buildbot:/home/tisadmin/public_html/wapt-1.5.1.25/community/
 
 waptpython build_exe.py enterprise
-"c:\Program Files\PuTTY\pscp.exe" *.exe waptsetup\*.exe tisadmin@buildbot:/home/tisadmin/public_html/wapt-1.5.1.24/enterprise/
+"c:\Program Files\PuTTY\pscp.exe" *.exe waptsetup\*.exe tisadmin@buildbot:/home/tisadmin/public_html/wapt-1.5.1.25/enterprise/
