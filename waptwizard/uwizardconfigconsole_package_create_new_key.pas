d5b86dc00fd370f2e6daa864fff6a6b66c4a5e7f
Frederic Morvan <fmorvan@tranquil.it>
2018-07-25 18:16:38 +0200

    wizard : [console] when creating a new key, behaviour is to prevent key deletion if exist

--- waptwizard/uwizardconfigconsole_package_create_new_key.pas
+++ waptwizard/uwizardconfigconsole_package_create_new_key.pas
@@ -161,11 +161,10 @@ end;
 
 function TWizardConfigConsole_PackageCreateNewKey.validate_package_signing(): integer;
 type
-  TStep = ( sFieldsNotEmpty, sPasswordsEquals, sKeyIsAlphanum, sKeyExist, sKeyDestination, sCreatePrivateKey, sExistingKeyCheckPassword, sCheckExistingCertificate, sCopyCertToSSLDirectory, sFinish );
+  TStep = ( sFieldsNotEmpty, sPasswordsEquals, sKeyIsAlphanum, sKeyExist, sKeyDestination, sCreatePrivateKey, sCopyCertToSSLDirectory, sFinish );
   TStepSet = set of TStep;
 const
   valide_stepset_1 : TStepSet = [ sFieldsNotEmpty, sPasswordsEquals, sKeyIsAlphanum, sKeyDestination, sCreatePrivateKey, sCopyCertToSSLDirectory, sFinish ];
-  valide_stepset_2 : TStepSet = [ sFieldsNotEmpty, sPasswordsEquals, sKeyIsAlphanum, sExistingKeyCheckPassword, sCheckExistingCertificate, sFinish ];
 var
   s                 : String;
   msg               : String;
@@ -197,9 +196,6 @@ begin
   begin
 
     if completed_steps >= valide_stepset_1 then
-      break;
-
-    if completed_steps >= valide_stepset_2 then
       break;
 
     Include( completed_steps, step );
@@ -241,12 +237,14 @@ begin
       // Check if key exist
       sKeyExist:
         begin
+          msg :=        'A key with this exist .' + #13#10;
+          msg := msg +  'Choose a new name or delete it manually';
           s := fs_path_concat( ed_private_key_directory.Text, pem );
-          if FileExists( s ) then
-            step := sExistingKeyCheckPassword
-          else
-            step := sKeyDestination;;
+          if not wizard_validate_fs_file_not_exist( self.m_wizard, @s[1], 'Preventing overwriting key file', @msg[1], self.ed_private_key_name ) then
+            exit(-1);
+          step := sKeyDestination;;
         end;
+
 
       // Private key destination directory exist and writable or parent dir is directory writable
       sKeyDestination :
@@ -298,54 +296,6 @@ begin
         FreeMemAndNil( params );
         step := sCopyCertToSSLDirectory;
       end;
-
-      // Validate check supplied password for existing key
-      sExistingKeyCheckPassword :
-        begin
-          m_wizard.SetValidationDescription('Key exist, checking supplied password can decrypt' );
-          r := crypto_check_key_password(b, s, ed_private_key_password_1.Text );
-          if r <> 0 then
-          begin
-            m_wizard.show_validation_error( ed_private_key_password_1, 'A key with this name exist but an error has occured when checking password.' + #13#10 + 'Move or delete key manually to continue.' );
-            exit(-1);
-          end;
-
-          if not b then
-          begin
-            m_wizard.show_validation_error( ed_private_key_password_1, 'A key with this name exist but supplied password is wrong.' + #13#10 + 'Check your password or move or delete key manually to continue.' );
-            exit(-1);
-          end;
-          step := sCheckExistingCertificate;
-        end;
-
-      // Validate  certificate ...
-      sCheckExistingCertificate :
-        begin
-          m_wizard.SetValidationDescription( 'Validing existing certificate' );
-          s := fs_path_concat( 'ssl', crt );
-          // A cert not exist in ssl dir ?
-          if FileExists(s) then
-          begin
-            step := sFinish;
-            Continue;
-          end;
-
-          // ... and not in private key dir ?
-          s := fs_path_concat( ed_private_key_directory.Text, crt );
-          if FileExists(s) then
-          begin
-            step := sCopyCertToSSLDirectory;
-            Continue;
-          end;
-
-          msg :=       'The certificate %s corresponding' + #13#10;
-          msg := msg + 'to this key cannot be find. You can either' + #13#10;
-          msg := msg + 'recreate the key or place the certificate' + #13#10;
-          msg := msg + 'in the private key directory';
-          msg := Format( msg, [crt] );
-          m_wizard.show_validation_error( ed_private_key_directory, s );
-          exit(-1);
-        end;
 
       // Copy certificate to authorized certificates directory
       sCopyCertToSSLDirectory :
