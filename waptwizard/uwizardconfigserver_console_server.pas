0213ddc547abfc9595de3938e580fc11340084c7
Frederic Morvan <fmorvan@tranquil.it>
2018-07-27 10:42:22 +0200

    wizard : [server] add custom server url choice

--- waptwizard/uwizardconfigserver_console_server.pas
+++ waptwizard/uwizardconfigserver_console_server.pas
@@ -7,15 +7,21 @@ interface
 uses
   uwizard,
   uwizardstepframe,
-  Classes, SysUtils, FileUtil, Forms, Controls, ExtCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, ExtCtrls, StdCtrls;
 
 type
 
   { TWizardConfigserver_Console_Server }
 
   TWizardConfigserver_Console_Server = class( TWizardStepFrame )
+    ed_custom_server_url: TEdit;
+    lbl_custom_server_url: TLabel;
+    p_custom_server_url: TPanel;
     rg_server_url: TRadioGroup;
+    procedure rg_server_urlSelectionChanged(Sender: TObject);
   private
+
+    function is_custom_server_url_selected() : boolean;
 
   public
 
@@ -27,8 +33,10 @@ type
 implementation
 
 uses
+  dialogs,
   uwapt_ini,
   IniFiles,
+  uwizardvalidattion,
   uwizardconfigserver_data,
   tiscommon,
   uwizardutil;
@@ -37,8 +45,24 @@ uses
 
 { TWizardConfigserver_Console_Server }
 
+
+procedure TWizardConfigserver_Console_Server.rg_server_urlSelectionChanged( Sender: TObject);
+begin
+  self.p_custom_server_url.Visible := is_custom_server_url_selected();
+end;
+
+function TWizardConfigserver_Console_Server.is_custom_server_url_selected(): boolean;
+begin
+  result := (self.rg_server_url.Items.Count-1) = self.rg_server_url.ItemIndex;
+end;
+
 procedure TWizardConfigserver_Console_Server.clear();
 begin
+  self.p_custom_server_url.Caption := '';
+  self.rg_server_url.ItemIndex:= -1;
+  self.rg_server_url.Items.Clear;
+  self.ed_custom_server_url.Clear;
+  self.p_custom_server_url.Visible := false;
 end;
 
 procedure TWizardConfigserver_Console_Server.wizard_show();
@@ -49,6 +73,7 @@ var
   ini : TIniFile;
   r   : integer;
   s   : String;
+  b_ini_wapt_server : boolean;
 begin
 
 
@@ -61,8 +86,12 @@ begin
     ini := TIniFile.Create( s );
     try
       s := ini.ReadString( INI_GLOBAL, INI_WAPT_SERVER, '' );
-      if Length(s) > 0 then
+      b_ini_wapt_server := Length(s) > 0;
+      if b_ini_wapt_server then
+      begin
         h := s;
+        self.ed_custom_server_url.Text := s;
+      end;
     finally
       FreeAndNil(ini);
     end;
@@ -92,6 +121,11 @@ begin
   end;
   sl.Free;
 
+  // Server url custom
+  self.rg_server_url.Items.AddObject('Custom url', nil );
+  if b_ini_wapt_server then
+    self.rg_server_url.ItemIndex := i;
+
 
   self.m_wizard.WizardButtonPanel.NextButton.SetFocus;
 
@@ -101,20 +135,35 @@ procedure TWizardConfigserver_Console_Server.wizard_next(var bCanNext: boolean);
 var
   s : String;
   data : PWizardConfigServerData;
+  c : TControl;
 begin
 
   bCanNext := false;
 
   data := self.m_wizard.data();
 
-  // server_url
   if self.rg_server_url.ItemIndex = -1 then
   begin
     m_wizard.show_validation_error( self.rg_server_url, 'You must a valid server url' );
     exit;
   end;
 
-  s := self.rg_server_url.Items[ self.rg_server_url.ItemIndex ];
+  // custom ?
+  if is_custom_server_url_selected() then
+  begin
+    s := self.ed_custom_server_url.Text;
+    c := self.ed_custom_server_url;
+  end
+  else
+  begin
+    s := self.rg_server_url.Items[ self.rg_server_url.ItemIndex ];
+    c := self.rg_server_url;
+  end;
+
+  if not wizard_validate_waptserver_ping( self.m_wizard, s, c ) then
+    exit;
+
+
   data^.wapt_server := s;
   data^.repo_url    := s + '/wapt' ;
   data^.server_certificate := s  + '.crt';
