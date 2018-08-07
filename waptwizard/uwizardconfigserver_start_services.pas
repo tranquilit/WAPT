d6fdb717a8fe191a2e5030723d5157f4360c74e0
Frederic Morvan <fmorvan@tranquil.it>
2018-08-07 10:10:31 +0200

    Added a progress to restart service wizard page

--- waptwizard/uwizardconfigserver_start_services.pas
+++ waptwizard/uwizardconfigserver_start_services.pas
@@ -7,19 +7,18 @@ interface
 uses
   uwizard,
   uwizardstepframe,
-  Classes, SysUtils, FileUtil, Forms, Controls, StdCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, StdCtrls, ExtCtrls, ComCtrls;
 
 type
 
   { TWizardConfigServer_StartServices }
 
   TWizardConfigServer_StartServices = class(TWizardStepFrame)
-    ImageList1: TImageList;
+    progress: TProgressBar;
   private
 
   public
 
-    procedure wizard_load(w: TWizard); override; final;
     procedure wizard_show(); override; final;
     procedure wizard_next(var bCanNext: boolean); override; final;
 
@@ -29,7 +28,6 @@ implementation
 
 uses
   dialogs,
-  ExtCtrls,
   uwizardconfigserver_data,
   uwizardvalidattion,
   uwizardutil;
@@ -38,68 +36,57 @@ uses
 
 {$R *.lfm}
 
-const
-  IMG_SUCCEED : integer = 0;
-  IMG_FAILED  : integer = 1;
-
 
 { TWizardConfigServer_StartServices }
-
-procedure TWizardConfigServer_StartServices.wizard_load(w: TWizard);
-var
-  i : integer;
-  lbl : TLabel;
-  img : TImage;
-begin
-  inherited wizard_load(w);
-
-  for i := 0 to Length(WAPT_SERVICES) - 1 do
-  begin
-    lbl := TLabel.Create( self );
-    lbl.Caption:= WAPT_SERVICES[i];
-    lbl.Align:= alTop;
-    lbl.Parent := self;
-
-    img := Timage.Create( self );
-    img.Align := alTop;
-    self.ImageList1.GetBitmap( IMG_SUCCEED, img.Picture.Bitmap );
-    img.Parent := self;
-  end;
-
-end;
 
 procedure TWizardConfigServer_StartServices.wizard_show();
 begin
   inherited wizard_show();
+
+  self.progress.Position := 0;
+
   if m_show_count = 1 then
     self.m_wizard.click_next_async();
+
 end;
 
 procedure TWizardConfigServer_StartServices.wizard_next(var bCanNext: boolean);
+const
+  GRACEFULL_TIME_MS : integer = 1 * 1000;
 var
   data : PWizardConfigServerData;
+  i : integer;
 begin
   bCanNext := false;
   data := m_wizard.data();
 
+  self.progress.Max := Length(WAPT_SERVICES);
+  if data^.has_found_waptservice then
+    self.progress.Max := self.progress.Max + 2;
 
   // Write setting
   data_write_ini_waptserver( m_wizard.data(), m_wizard );
 
-  // Restart server
-  if not wizard_validate_waptserver_start_services( m_wizard, nil ) then
-    exit;
+  // Restart services
+  for i := 0 to Length(WAPT_SERVICES) - 1 do
+  begin
+    if not wizard_validate_service_start( self.m_wizard, nil, WAPT_SERVICES[i] ) then
+      exit;
+    self.progress.Position := self.progress.Position + 1;
+  end;
 
   // Restart local agent
   if data^.has_found_waptservice then
   begin
-    Sleep( 1 * 1000 );
+    Sleep( GRACEFULL_TIME_MS );
     self.m_wizard.SetValidationDescription( 'Register local machine');
     wapt_register();
+    self.progress.Position := self.progress.Position + 1;
 
-    Sleep( 1 * 1000 );
+    Sleep( GRACEFULL_TIME_MS );
     self.m_wizard.SetValidationDescription( 'Restarting local agent');
     wapt_service_restart();
+    self.progress.Position := self.progress.Position + 1;
 
     self.m_wizard.ClearValidationDescription();
   end;
