792d597c06b62f6604bb06f1cdae30f318604dca
Frederic Morvan <fmorvan@tranquil.it>
2018-08-20 15:04:13 +0200

    Removed some console context irrelevant code

--- waptconsolepostconf/uvisconsolepostconf.pas
+++ waptconsolepostconf/uvisconsolepostconf.pas
@@ -7,7 +7,7 @@ interface
 uses
   PythonEngine,
   Classes, SysUtils, FileUtil, LazFileUtils, LazUTF8, Forms, Controls, Graphics,
-  Dialogs, ComCtrls, StdCtrls, ExtCtrls, Buttons, ActnList, htmlview, Readhtml,
+  Dialogs, ComCtrls, StdCtrls, ExtCtrls, Buttons, ActnList, htmlview,
   IdHTTP, IdComponent, uvisLoading, DefaultTranslator, LCLTranslator, LCLProc,
   EditBtn, waptconsolepostconfres;
 
@@ -108,8 +108,15 @@ uses
   uutil,
   uvalidation,
   udefault,
-  LCLIntf, Windows, waptcommon, waptwinutils, UScaleDPI, tisinifiles,
-  superobject, tiscommon, tisstrings, IniFiles,DCPsha256,dcpcrypt2,DCPbase64,Math;
+  LCLIntf,
+  Windows,
+  waptcommon,
+  waptwinutils,
+  UScaleDPI,
+  tisinifiles,
+  superobject,
+  tiscommon,
+  IniFiles;
 
 {$R *.lfm}
 
@@ -159,21 +166,6 @@ begin
     Application.ProcessMessages;
   end;
 end;
-
-function GetString(const Index: integer) : string;
-var
-  buffer : array[0..8191] of char;
-  ls : integer;
-begin
-  Result := '';
-  ls := LoadString(hInstance,
-                   Index,
-                   buffer,
-                   sizeof(buffer));
-  if ls <> 0 then Result := buffer;
-end;
-
-
 
 procedure TVisWAPTConsolePostConf.PagesControlChange(Sender: TObject);
 const
@@ -870,130 +862,6 @@ begin
   actPrevious.Enabled:=(PagesControl.ActivePageIndex>0) and (PagesControl.ActivePageIndex<=PagesControl.PageCount-1);
 end;
 
-function runwapt(cmd:String):String;
-begin
-  StrReplace(cmd,'{app}',WaptBaseDir,[rfReplaceAll]);
-  result := Run(cmd);
-end;
-
-
-function RPad(x: string; c: Char; s: Integer): string;
-var
-  i: Integer;
-begin
-  Result := x;
-  if Length(x) < s then
-    for i := 1 to s-Length(x) do
-      Result := Result + c;
-end;
-
-function XorBlock(s, x: ansistring): ansistring; inline;
-var
-  i: Integer;
-begin
-  SetLength(Result, Length(s));
-  for i := 1 to Length(s) do
-    Result[i] := Char(Byte(s[i]) xor Byte(x[i]));
-end;
-
-function CalcDigest(text: string; dig: TDCP_hashclass): string;
-var
-  x: TDCP_hash;
-begin
-  x := dig.Create(nil);
-  try
-    x.Init;
-    x.UpdateStr(text);
-    SetLength(Result, x.GetHashSize div 8);
-    x.Final(Result[1]);
-  finally
-    x.Free;
-  end;
-end;
-
-function CalcHMAC(message, pgPackagePrivateKey: string; hash: TDCP_hashclass): string;
-const
-  blocksize = 64;
-begin
-  // Definition RFC 2104
-  if Length(pgPackagePrivateKey) > blocksize then
-    pgPackagePrivateKey := CalcDigest(pgPackagePrivateKey, hash);
-  pgPackagePrivateKey := RPad(pgPackagePrivateKey, #0, blocksize);
-
-  Result := CalcDigest(XorBlock(pgPackagePrivateKey, RPad('', #$36, blocksize)) + message, hash);
-  Result := CalcDigest(XorBlock(pgPackagePrivateKey, RPad('', #$5c, blocksize)) + result, hash);
-end;
-
-function PBKDF1(pass, salt: ansistring; count: Integer; hash: TDCP_hashclass): ansistring;
-var
-  i: Integer;
-begin
-  Result := pass+salt;
-  for i := 0 to count-1 do
-    Result := CalcDigest(Result, hash);
-end;
-
-function PBKDF2(pass, salt: ansistring; count, kLen: Integer; hash: TDCP_hashclass): ansistring;
-
-  function IntX(i: Integer): ansistring; inline;
-  begin
-    Result := Char(i shr 24) + Char(i shr 16) + Char(i shr 8) + Char(i);
-  end;
-
-var
-  D, I, J: Integer;
-  T, F, U: ansistring;
-begin
-  T := '';
-  D := Ceil(kLen / (hash.GetHashSize div 8));
-  for i := 1 to D do
-  begin
-    F := CalcHMAC(salt + IntX(i), pass, hash);
-    U := F;
-    for j := 2 to count do
-    begin
-      U := CalcHMAC(U, pass, hash);
-      F := XorBlock(F, U);
-    end;
-    T := T + F;
-  end;
-  Result := '$pbkdf2-'+LowerCase(hash.GetAlgorithm)+'$'+IntToStr(count)+'$'+DCPbase64.Base64EncodeStr(salt)+'$'+DCPbase64.Base64EncodeStr(Copy(T, 1, kLen));
-end;
-
-
-function MakeRandomString(const ALength: Integer;
-                          const ACharSequence: String = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'): String;
-var
-  C1, sequence_length: Integer;
-begin
-  sequence_length := Length(ACharSequence);
-  SetLength(result, ALength);
-
-  for C1 := 1 to ALength do
-    result[C1] := ACharSequence[Random(sequence_length) + 1];
-end;
-
-function DigestToStr(Digest: array of byte): string;
-var
-  i: Integer;
-begin
-  Result := '';
-  for i := 0 to Length(Digest) -1 do
-    Result := Result + LowerCase(IntToHex(Digest[i], 2));
-end;
-
-function GetStringHash(Source: string): string;
-var
-  Hash: TDCP_sha256;
-  Digest: array[0..31] of Byte;
-begin
-  Hash := TDCP_sha256.Create(nil);
-  Hash.Init;
-  Hash.UpdateStr(Source);
-  Hash.Final(Digest);
-  Hash.Free;
-  Result := DigestToStr(Digest);
-end;
 
 
 procedure TVisWAPTConsolePostConf.ButCancelClick(Sender: TObject);
@@ -1006,17 +874,6 @@ procedure TVisWAPTConsolePostConf.ActManualExecute(Sender: TObject);
 begin
   ActManual.Checked := not ActManual.Checked;
 end;
-
-function MakeIdent(st:String):String;
-var
-  i:integer;
-begin
-  result :='';
-  for i := 1 to length(st) do
-    if CharIsValidIdentifierLetter(st[i]) then
-      result := Result+st[i];
-end;
-
 
 end.
 
