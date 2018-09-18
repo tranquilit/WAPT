2b4033c572a8fcc86b626f769fa12f259f1181f4
htouvet <htouvet-github@tranquil.it>
2018-09-18 14:29:14 +0200

    inc version to 1.7.0

--- waptserver/config.py
+++ waptserver/config.py
@@ -21,7 +21,7 @@
 #
 # -----------------------------------------------------------------------
 from __future__ import absolute_import
-__version__ = "1.6.2.5"
+__version__ = "1.7.0.0"
 
 import os
 import sys
