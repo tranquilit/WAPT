a5669f2e15b534b15ae6cd8880003233489ed6a2
htouvet <htouvet-github@tranquil.it>
2019-07-09 14:21:01 +0200

    waptexit : don't cancel tasks on CloseQuery

--- waptexit/uwaptexit.pas
+++ waptexit/uwaptexit.pas
@@ -365,12 +365,6 @@ begin
       CanClose:= AllowCancelUpgrade and ((Running=Nil) or (MessageDlg(rsConfirmCancelTask,Format(rsConfirmCancelRunningTask,[Running.S['description']]),
           mtConfirmation, [mbYes, mbNo, mbCancel],0) = mrYes));
 
-      if CanClose then
-      begin
-        if Running<>Nil then
-          WAPTLocalJsonGet('cancel_running_task.json');
-        WAPTLocalJsonGet('cancel_all_tasks.json')
-      end
     end
     else
       CanClose := True;
