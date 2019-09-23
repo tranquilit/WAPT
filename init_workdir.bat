4578f1c2a1b174ea66a53a4b6da15c851aff9136
htouvet <htouvet-github@tranquil.it>
2019-09-23 20:08:56 +0200

    missing r''

--- init_workdir.bat
+++ init_workdir.bat
@@ -10,10 +10,10 @@ REM uncomment and modify the following lines if you need proxy for internet acce
 REM set http_proxy=http://srvproxy:8080
 REM set https_proxy=http://srvproxy:8080
 
-git -C %~dp0 clean -xfd 
+git -C %~dp0 clean -xfd
 %PYTHON_PATH%\python.exe -m pip install -U pip setuptools
 %PYTHON_PATH%\Scripts\pip.exe install virtualenv
-%PYTHON_PATH%\Scripts\virtualenv.exe  --no-site-packages --always-copy %~dp0 
+%PYTHON_PATH%\Scripts\virtualenv.exe  --no-site-packages --always-copy %~dp0
 xcopy /I /E /F /Y c:\python27\libs %~dp0\libs
 xcopy /I /E /F /Y c:\python27\DLLs %~dp0\DLLs
 xcopy /I /E /F /Y /EXCLUDE:libexcludes.txt c:\python27\lib %~dp0\lib
@@ -21,7 +21,7 @@ xcopy /I /E /F /Y /EXCLUDE:libexcludes.txt c:\python27\lib %~dp0\lib
 %~dp0\Scripts\python  -m pip install -U pip setuptools wheel virtualenv six requests==2.19.1 psutil==3.4.2
 
 REM get  pywin32-220.win32Sc            -py2.7.exe from internet
-python -c "from waptutils import wget; from subprocess import check_output; pywin32=wget('https://github.com/mhammond/pywin32/releases/download/b223/pywin32-223.win32-py2.7.exe',resume=True,cache_dir='c:\\binaries',md5='366d181c39169d3b0c0e1d25f781d1d6'); print check_output('%~dp0\Scripts\easy_install.exe ""%%s""' %% pywin32,shell=True)"
+python -c "from waptutils import wget; from subprocess import check_output; pywin32=wget('https://github.com/mhammond/pywin32/releases/download/b223/pywin32-223.win32-py2.7.exe',resume=True,cache_dir='c:\\binaries',md5='366d181c39169d3b0c0e1d25f781d1d6'); print check_output(r'%~dp0\Scripts\easy_install.exe ""%%s""' %% pywin32,shell=True)"
 %0\..\Scripts\pip.exe install -r %~dp0\requirements.txt -r %~dp0\requirements-windows.txt
 
 rem copy /Y %0\..\lib\site-packages\pywin32-220-py2.7-win32.egg\py*.dll %0\..\
