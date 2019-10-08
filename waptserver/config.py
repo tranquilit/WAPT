a9e3640399a75c38e8a228eee235c3c8b94857d2
eblaudy <eblaudy@ad.tranquil.it>
2019-10-08 16:41:42 +0200

    Fix for version

--- waptserver/config.py
+++ waptserver/config.py
@@ -21,7 +21,7 @@
 #
 # -----------------------------------------------------------------------
 from __future__ import absolute_import
-__version__ = "1.7.5"
+__version__ = "1.7.6"
 
 import os
 import sys
