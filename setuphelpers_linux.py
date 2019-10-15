7a74ea62a80f90c70cb263cf4b364f227e2607e3
eblaudy <eblaudy@ad.tranquil.it>
2019-10-15 09:38:56 +0200

    Some fixes for innosetup and setuphelpers_linux.py

--- setuphelpers_linux.py
+++ setuphelpers_linux.py
@@ -250,8 +250,15 @@ def installed_softwares(keywords='',uninstallkey=None,name=None):
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return [s.getsockname()[0]]
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def get_loggedinusers():
     suser = psutil.users()
