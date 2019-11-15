677465aaaec0021dcbf197af3415cc04e4097121
eblaudy <eblaudy@tranquil.it>
2019-11-15 18:11:23 +0100

    Merge some work from python 3.6

--- init_workdir.bat
+++ init_workdir.bat
@@ -13,21 +13,18 @@ git -C %~dp0 clean -xfd
 %PYTHON_PATH%\python.exe -m pip install -U pip setuptools
 %PYTHON_PATH%\Scripts\pip.exe install virtualenv
 %PYTHON_PATH%\Scripts\virtualenv.exe  --no-site-packages --always-copy %~dp0
-xcopy /I /E /F /Y c:\python27\libs %~dp0\libs
-xcopy /I /E /F /Y c:\python27\DLLs %~dp0\DLLs
-xcopy /I /E /F /Y /EXCLUDE:%~dp0\libexcludes.txt c:\python27\lib %~dp0\lib
+xcopy /I /E /F /Y %PYTHON_PATH%\libs %~dp0\libs
+xcopy /I /E /F /Y %PYTHON_PATH%\DLLs %~dp0\DLLs
+xcopy /I /E /F /Y /EXCLUDE:%~dp0\libexcludes.txt %PYTHON_PATH%\lib %~dp0\lib
 
-%~dp0\Scripts\python  -m pip install -U pip setuptools wheel virtualenv six requests==2.19.1 psutil==3.4.2
+%~dp0\Scripts\python -m pip install -U pip setuptools wheel
 
 REM get  pywin32-220.win32Sc            -py2.7.exe from internet
 python -c "from urllib import urlretrieve; from subprocess import check_output; pywin32=urlretrieve('https://github.com/mhammond/pywin32/releases/download/b223/pywin32-223.win32-py2.7.exe');print(pywin32[0]); print(check_output(r'%~dp0\Scripts\easy_install.exe %%s' %% (pywin32[0]),shell=True));"
 %~dp0\Scripts\pip.exe install -r %~dp0\requirements.txt -r %~dp0\requirements-windows.txt
 
-rem copy /Y %0\..\lib\site-packages\pywin32-220-py2.7-win32.egg\py*.dll %0\..\
 copy /Y c:\windows\SysWOW64\python27.dll %~dp0\
-rem copy /Y c:\windows\SysWOW64\pythoncom27.dll %0\..\
-rem /Y c:\windows\SysWOW64\pythoncomloader27.dll %0\..\
-rem /Y c:\windows\SysWOW64\pywintypes27.dll %0\..\
+
 
 copy /Y %~dp0\Scripts\python.exe %~dp0\waptpython.exe
 copy /Y %~dp0\Scripts\pythonw.exe %~dp0\waptpythonw.exe
