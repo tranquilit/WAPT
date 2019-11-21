51b890d122fdd2bb4669b8e0af1e1646c2e11a4c
olarose <olarose@ad.tranquil.it>
2019-11-21 16:34:36 +0100

    Fixed proxy issue

--- pywininstall.py
+++ pywininstall.py
@@ -1,5 +1,6 @@
 import os
 import urllib
+import urllib2
 import hashlib
 
 hash_current_version = 'd059126a23a6d55b5198b271bd29ddc1'
@@ -11,22 +12,34 @@ binaries_cache = os.path.abspath(os.path.join(wapt_base_dir,'..','binaries_cache
 
 exe_location = os.path.join(binaries_cache,'pywin_install.exe')
 
+
+proxy = urllib2.ProxyHandler(urllib.getproxies())
+
+opener = urllib2.build_opener(proxy)
+
+urllib2.install_opener(opener)
+
+def download_file(url,location):
+    with open(location,'wb') as f:
+        f.write(urllib2.urlopen(url).read())
+        f.close()
+
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
-    
+
 def download_exe(url,hash,location):
-    download_location = urllib.urlretrieve(url,location)
-    if md5(download_location[0]) == hash:
+    download_file(url,location)
+    if md5(location) == hash:
         print('Good MD5 sum for %s' % (url))
-        return download_location[0]
+        return location
     else:
-        os.remove(download_location[0])
-        raise('Bad MD5 sum for %s' % (url))
-        
+        os.remove(location)
+        raise Exception('Bad MD5 sum for %s' % (url))
+
 
 if not(os.path.isdir(binaries_cache)):
     os.mkdir(binaries_cache)
