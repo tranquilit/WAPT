f1b94eba4faefc4fb0997adddad8e966f284913d
eblaudy <eblaudy@tranquil.it>
2020-03-11 13:25:22 +0100

    Fix for editor_for_packages in waptconsole Add parameters in templates/vscode_launch.json

--- waptconsole/uvisimportpackage.pas
+++ waptconsole/uvisimportpackage.pas
@@ -739,7 +739,8 @@ begin
           ));
         { TODO : Remove use of WAPT instance, use waptpackage.PackageEntry instead }
         dmpython.WAPT.add_pyscripter_project(sourceDir);
-        DMPython.common.wapt_sources_edit(sourceDir);
+        dmpython.WAPT.add_vscode_projectt(sourceDir);
+        DMPython.common.wapt_sources_edit(wapt_sources_dir:=sourceDir,editor_for_packages:=dmpython.WAPT.editor_for_packages);
       end;
     finally
       Free;
