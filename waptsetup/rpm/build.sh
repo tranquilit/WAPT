c8501c145857ed99158071f2b104c2fc0903746e
eblaudy <eblaudy@tranquil.it>
2020-04-29 16:32:49 +0200

    * fix for waptsetup/rpm/build.sh

--- waptsetup/rpm/build.sh
+++ waptsetup/rpm/build.sh
@@ -2,6 +2,10 @@
 set -ex
 VERSION=$(python2 get_version.py ../../waptutils.py)
 
+if [[ -d RPMS ]]
+	rm -rf RPMS
+fi
+
 mkdir -p BUILD RPMS SPECS
 mkdir -p builddir
 
@@ -24,6 +28,5 @@ else
 	rpmbuild -bb -v --clean --buildroot $PWD/builddir --define "_version $VERSION"    ./waptsetup.spec 1>&2
 fi
 
-rm -f tis-waptsetup.rpm
 cp ./RPMS/noarch/tis-waptsetup*.rpm  .
 echo tis-waptsetup*.rpm
