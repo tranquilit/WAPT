3a0dfd82c5e5c4dd07e317b99c517b971373a9d6
eblaudy <eblaudy@tranquil.it>
2020-04-29 14:57:19 +0200

    * waptsetup/deb/createdeb.py remove last builddir

--- waptsetup/deb/createdeb.py
+++ waptsetup/deb/createdeb.py
@@ -249,6 +249,8 @@ template_vars = {
 render_control = template_control.render(template_vars)
 render_postinst = template_postinst.render(template_vars)
 
+if os.path.exists(BDIR):
+    shutil.rmtree(BDIR)
 os.makedirs(os.path.join(BDIR,'DEBIAN'))
 
 with open(os.path.join(BDIR,'DEBIAN','control'),'w') as f_control:
