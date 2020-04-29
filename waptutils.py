c43d326f0fff21afaebd422744861f43ac204a39
eblaudy <eblaudy@tranquil.it>
2020-04-29 16:42:32 +0200

    * update version to 1.8.2

--- waptutils.py
+++ waptutils.py
@@ -21,7 +21,7 @@
 #
 # -----------------------------------------------------------------------
 from __future__ import absolute_import
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 
 import os
 import sys
