718841ce99c8ef8f5bf24b052fa30aba49c7d3de
olarose <olarose@ad.tranquil.it>
2020-06-02 16:44:08 +0200

    Base for WaptMessage (temp name)

---
+++ waptmessage/uviswaptmessage.pas
@@ -0,0 +1,32 @@
+unit uviswaptmessage;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Label1: TLabel;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
