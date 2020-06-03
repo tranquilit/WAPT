88716cd781c93d79eab6f19ef5a285c81e555a97
olarose <olarose@ad.tranquil.it>
2020-06-03 12:05:00 +0200

    Added a help message, option to read a file's content, and icon

--- waptmessage/uviswaptmessage.pas
+++ waptmessage/uviswaptmessage.pas
@@ -5,7 +5,8 @@ unit uviswaptmessage;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  uWaptMessageRes;
 
 type
 
@@ -14,6 +15,7 @@ type
   TMsgForm = class(TForm)
     Label1: TLabel;
     procedure FormShow(Sender: TObject);
+    procedure DisplayFileContent(fileName: String);
   private
 
   public
@@ -29,12 +31,45 @@ implementation
 
 { TMsgForm }
 
+procedure WriteHelp;
+begin
+  WriteLn(rsHelp);
+  Halt();
+end;
+
+procedure TMsgForm.DisplayFileContent(fileName: String);
+var
+  msgFile: TextFile;
+  fileContent: String;
+begin
+  try
+    AssignFile(msgFile, fileName);
+    Reset(msgFile);
+    Read(msgFile, fileContent);
+    Label1.Caption := fileContent;
+  except
+    writeln(stderr,'Could not find, open or read file');
+    Halt(1);
+  end;
+end;
+
 procedure TMsgForm.FormShow(Sender: TObject);
 begin
-  // TODO : -h
-  // TODO -f file to read content
+  if Application.HasOption('h', 'help') then
+  begin
+    WriteHelp();
+    Halt();
+  end;
+
+  // No flags : print message
   if ParamCount = 1 then
+  begin
      Label1.Caption := ParamStr(0);
+  end;
+
+  // -f flag : message is file content
+  if Application.HasOption('f') then
+    DisplayFileContent(Application.GetOptionValue('f'));
 end;
 
 end.
